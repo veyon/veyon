6ddede861781c4f00d507316d4ae2a0503525fef
Tobias Doerffel <tobias.doerffel@gmail.com>
2007-02-10 23:20:11 +0000

    added/updated Spanish localization
    git-svn-id: https://italc.svn.sourceforge.net/svnroot/italc@98 d6290f70-d918-0410-89a7-c2321b9b1c3d

---
+++ ica/resources/es.ts
@@ -0,0 +1,167 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="es_ES">
+<context>
+    <name>QApplication</name>
+    <message>
+        <location filename="../src/ica_main.cpp" line="246"/>
+        <source>iTALC Client on %1:%2</source>
+        <translation>Cliente iTALC en %1:%2</translation>
+    </message>
+    <message>
+        <location filename="../src/system_service.cpp" line="261"/>
+        <source>Unable to register service &apos;%1&apos;.</source>
+        <translation>No se puede registrar el servicio &apos;%1&apos;.</translation>
+    </message>
+    <message>
+        <location filename="../src/system_service.cpp" line="289"/>
+        <source>The Service Control Manager could not be contacted (do you have the neccessary rights?!) - the service &apos;%1&apos; was not registered.</source>
+        <translation>El gestor de control de servicios no puedo ser contactado (¿tiene privilegios suficientes?) - el servicio &apos;%1&apos; no se ha registrado.</translation>
+    </message>
+    <message>
+        <location filename="../src/system_service.cpp" line="320"/>
+        <source>The service &apos;%1&apos; is already registered.</source>
+        <translation>El servicio &apos;%1&apos; ya está registrado.</translation>
+    </message>
+    <message>
+        <location filename="../src/system_service.cpp" line="328"/>
+        <source>The service &apos;%1&apos; could not be registered.</source>
+        <translation>El servicio &apos;%1&apos; no se pudo registrar.</translation>
+    </message>
+    <message>
+        <location filename="../src/system_service.cpp" line="346"/>
+        <source>The service &apos;%1&apos; was successfully registered. It will automatically be run the next time you start this computer.</source>
+        <translation>El servicio &apos;%1&apos; se ha registrado correctamente. Se ejecutará la próxima vez que inicie su equipo.</translation>
+    </message>
+    <message>
+        <location filename="../src/system_service.cpp" line="584"/>
+        <source>The service &apos;%1&apos; could not be stopped.</source>
+        <translation>El servicio &apos;%1&apos; no se pudo parar.</translation>
+    </message>
+    <message>
+        <location filename="../src/system_service.cpp" line="409"/>
+        <source>The service &apos;%1&apos; has been unregistered.</source>
+        <translation>El servicio &apos;%1&apos; se ha des-registrado.</translation>
+    </message>
+    <message>
+        <location filename="../src/system_service.cpp" line="421"/>
+        <source>The service &apos;%1&apos; isn&apos;t registered and therefore can&apos;t be unregistered.</source>
+        <translation>El servicio &apos;%1&apos; no está registrado por lo que no se puede des-egistrar.</translation>
+    </message>
+    <message>
+        <location filename="../src/system_service.cpp" line="430"/>
+        <source>The service &apos;%1&apos; could not be unregistered.</source>
+        <translation>El servicio &apos;%1&apos; no se pudo des-registrar.</translation>
+    </message>
+    <message>
+        <location filename="../src/system_service.cpp" line="596"/>
+        <source>The service &apos;%1&apos; could not be found.</source>
+        <translation>El servicio &apos;%1&apos; no se ha encontrado.</translation>
+    </message>
+    <message>
+        <location filename="../src/system_service.cpp" line="457"/>
+        <source>The Service Control Manager could not be contacted (do you have the neccessary rights?!) - the service &apos;%1&apos; was not unregistered.</source>
+        <translation>El gestor de control de servicios no puedo ser contactado (¿tiene privilegios suficientes?) - el servicio &apos;%1&apos; no se ha des-registrado.</translation>
+    </message>
+    <message>
+        <location filename="../src/system_service.cpp" line="506"/>
+        <source>The service &apos;%1&apos; could not be started.</source>
+        <translation>El servicio &apos;%1&apos; no se pudo arrancar.</translation>
+    </message>
+    <message>
+        <location filename="../src/system_service.cpp" line="534"/>
+        <source>The Service Control Manager could not be contacted (do you have the neccessary rights?!) - the service &apos;%1&apos; was not started.</source>
+        <translation>El gestor de control de servicios no puedo ser contactado (¿tiene privilegios suficientes?) - el servicio &apos;%1&apos; no se ha arrancado.</translation>
+    </message>
+    <message>
+        <location filename="../src/system_service.cpp" line="612"/>
+        <source>The Service Control Manager could not be contacted (do you have the neccessary rights?!) - the service &apos;%1&apos; was not stopped.</source>
+        <translation>El gestor de control de servicios no puedo ser contactado (¿tiene privilegios suficientes?) - el servicio &apos;%1&apos; no se ha parado.</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../common/src/local_system.cpp" line="510"/>
+        <source>UPL (note for translators: the first three characters of this string are the accellerators (underlined characters) of the three input-fields in logon-dialog of windows - you only need to &quot;translate&quot; these characters, this note can be ommited)</source>
+        <translation>BKA</translation>
+    </message>
+    <message>
+        <location filename="../../common/src/local_system.cpp" line="722"/>
+        <source>iTALC-snapshots</source>
+        <translation>Capturas-iTALC</translation>
+    </message>
+</context>
+<context>
+    <name>demoClient</name>
+    <message>
+        <location filename="../src/demo_client.cpp" line="42"/>
+        <source>iTALC Demo</source>
+        <translation>iTALC Demo</translation>
+    </message>
+</context>
+<context>
+    <name>isdServer</name>
+    <message>
+        <location filename="../src/isd_server.cpp" line="769"/>
+        <source>Message from teacher</source>
+        <translation>Mensaje del profesor</translation>
+    </message>
+    <message>
+        <location filename="../src/isd_server.cpp" line="535"/>
+        <source>Somebody at host %1 tries to access your screen. Do you want to grant him/her access?</source>
+        <translation>Alguien en el equipo %1 intenta acceder a su pantalla. ¿Quiere permitir el acceso?</translation>
+    </message>
+    <message>
+        <location filename="../src/isd_server.cpp" line="553"/>
+        <source>Never for this session</source>
+        <translation>Nunca para esta sesión</translation>
+    </message>
+    <message>
+        <location filename="../src/isd_server.cpp" line="548"/>
+        <source>Always for this session</source>
+        <translation>Siempre para esta sesión</translation>
+    </message>
+    <message>
+        <location filename="../src/isd_server.cpp" line="532"/>
+        <source>Confirm access</source>
+        <translation>Confirmar acceso</translation>
+    </message>
+</context>
+<context>
+    <name>messageBox</name>
+    <message>
+        <location filename="../../common/src/messagebox.cpp" line="71"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+</context>
+<context>
+    <name>remoteControlWidget</name>
+    <message>
+        <location filename="../src/remote_control_widget.cpp" line="368"/>
+        <source>iTALC remote control (host %1)</source>
+        <translation>Control remoto iTALC (equipo %1)</translation>
+    </message>
+</context>
+<context>
+    <name>remoteControlWidgetToolBar</name>
+    <message>
+        <location filename="../src/remote_control_widget.cpp" line="53"/>
+        <source>Fullscreen/window</source>
+        <translation>Pantalla completa/Ventana</translation>
+    </message>
+    <message>
+        <location filename="../src/remote_control_widget.cpp" line="55"/>
+        <source>Quit</source>
+        <translation>Salir</translation>
+    </message>
+</context>
+<context>
+    <name>vncView</name>
+    <message>
+        <location filename="../src/vncview.cpp" line="54"/>
+        <source>Establishing connection to %1 ...</source>
+        <translation>Estableciendo conexión con %1 ...</translation>
+    </message>
+</context>
+</TS>
