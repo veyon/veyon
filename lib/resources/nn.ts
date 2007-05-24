07d8fcb18d2bbd90b15f869c58804cc527676901
Tobias Doerffel <tobias.doerffel@gmail.com>
2007-05-24 21:17:23 +0000

    added Norwegian localization
    git-svn-id: https://italc.svn.sourceforge.net/svnroot/italc@182 d6290f70-d918-0410-89a7-c2321b9b1c3d

---
+++ lib/resources/nn.ts
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="nn">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/local_system.cpp" line="51"/>
+        <source>UPL (note for translators: the first three characters of this string are the accellerators (underlined characters) of the three input-fields in logon-dialog of windows - please keep this note as otherwise there are strange errors concerning logon-feature)</source>
+        <translation>UPL</translation>
+    </message>
+    <message>
+        <location filename="../src/local_system.cpp" line="621"/>
+        <source>iTALC-snapshots</source>
+        <translation type="obsolete">iTALC-Skjermskot</translation>
+    </message>
+</context>
+<context>
+    <name>ivsConnection</name>
+    <message>
+        <location filename="../src/ivs_connection.cpp" line="309"/>
+        <source>Snapshot</source>
+        <translation>Skjermskot</translation>
+    </message>
+    <message>
+        <location filename="../src/ivs_connection.cpp" line="311"/>
+        <source>Could not make a snapshot as directory %1 doesn&apos;t exist and couldn&apos;t be created.</source>
+        <translation type="obsolete">Klarte ikkje lage skjermskot då katalogen %1 verken finnes eller kunne opprettast.</translation>
+    </message>
+    <message>
+        <location filename="../src/ivs_connection.cpp" line="311"/>
+        <source>Could not take a snapshot as directory %1 doesn&apos;t exist and couldn&apos;t be created.</source>
+        <translation>Klarte ikkje lage skjermskot då katalogen %1 verken finnes eller kunne opprettast.</translation>
+    </message>
+</context>
+<context>
+    <name>localSystem::QObject</name>
+    <message>
+        <location filename="../src/local_system.cpp" line="721"/>
+        <source>iTALC-snapshots</source>
+        <translation>iTALC-Skjermskotdd</translation>
+    </message>
+</context>
+<context>
+    <name>messageBox</name>
+    <message>
+        <location filename="../src/messagebox.cpp" line="75"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+</context>
+<context>
+    <name>vncView</name>
+    <message>
+        <location filename="../src/vncview.cpp" line="55"/>
+        <source>Establishing connection to %1 ...</source>
+        <translation>Kobler til %1 ...</translation>
+    </message>
+</context>
+</TS>
