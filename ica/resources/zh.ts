50473a7fd4492cc6570b45498af4b516f16da03c
Tobias Doerffel <tobias.doerffel@gmail.com>
2007-10-11 20:06:24 +0000

    added Chinese localization-files
    git-svn-id: https://italc.svn.sourceforge.net/svnroot/italc/branches/STABLE_1-0@234 d6290f70-d918-0410-89a7-c2321b9b1c3d

---
+++ ica/resources/zh.ts
@@ -0,0 +1,104 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="zh_CN">
+<context>
+    <name>QApplication</name>
+    <message>
+        <source>iTALC Client on %1:%2</source>
+        <translation>iTALC 客户端在 %1:%2</translation>
+    </message>
+    <message>
+        <source>Unable to register service &apos;%1&apos;.</source>
+        <translation>不能注册服务 &apos;%1&apos;。</translation>
+    </message>
+    <message>
+        <source>The Service Control Manager could not be contacted (do you have the neccessary rights?!) - the service &apos;%1&apos; was not registered.</source>
+        <translation>无法连接 Service Control Manager（您是否拥有所需的权限？）－服务 &apos;%1&apos; 未被注册。</translation>
+    </message>
+    <message>
+        <source>The service &apos;%1&apos; is already registered.</source>
+        <translation>服务 &apos;%1&apos; 已经注册。</translation>
+    </message>
+    <message>
+        <source>The service &apos;%1&apos; could not be registered.</source>
+        <translation>服务 &apos;%1&apos; 未能注册。</translation>
+    </message>
+    <message>
+        <source>The service &apos;%1&apos; was successfully registered.</source>
+        <translation>服务 &apos;%1&apos; 注册成功。</translation>
+    </message>
+    <message>
+        <source>The service &apos;%1&apos; could not be stopped.</source>
+        <translation>服务 &apos;%1&apos; 不能停止。</translation>
+    </message>
+    <message>
+        <source>The service &apos;%1&apos; has been unregistered.</source>
+        <translation>服务 &apos;%1&apos; 已取消注册。</translation>
+    </message>
+    <message>
+        <source>The service &apos;%1&apos; isn&apos;t registered and therefore can&apos;t be unregistered.</source>
+        <translation>服务 &apos;%1&apos; 未注册，因此不能被取消注册。</translation>
+    </message>
+    <message>
+        <source>The service &apos;%1&apos; could not be unregistered.</source>
+        <translation>服务 &apos;%1&apos; 不能被取消注册。</translation>
+    </message>
+    <message>
+        <source>The service &apos;%1&apos; could not be found.</source>
+        <translation>服务 &apos;%1&apos; 未发现。</translation>
+    </message>
+    <message>
+        <source>The Service Control Manager could not be contacted (do you have the neccessary rights?!) - the service &apos;%1&apos; was not unregistered.</source>
+        <translation>无法连接 Service Control Manager（您是否拥有所需的权限？）－服务 &apos;%1&apos; 未能被取消注册。</translation>
+    </message>
+    <message>
+        <source>The Service Control Manager could not be contacted (do you have the neccessary rights?!) - the service &apos;%1&apos; was not stopped.</source>
+        <translation>无法连接 Service Control Manager（您是否拥有所需的权限？）－服务 &apos;%1&apos; 不能停止。</translation>
+    </message>
+</context>
+<context>
+    <name>demoClient</name>
+    <message>
+        <source>iTALC Demo</source>
+        <translation>iTALC 演示</translation>
+    </message>
+</context>
+<context>
+    <name>isdServer</name>
+    <message>
+        <source>ISD-server error</source>
+        <translation>ISD-服务器 错误</translation>
+    </message>
+    <message>
+        <source>The ISD-server could not be started because port %1 is already in use. Please make sure that no other application is using this port and try again.</source>
+        <translation>ISD-服务器 不能启动，因为端口 %1 已经被占用。请确认没有其他程序在使用该端口并重试。</translation>
+    </message>
+    <message>
+        <source>Confirm access</source>
+        <translation>访问确认</translation>
+    </message>
+    <message>
+        <source>Somebody at host %1 tries to access your screen. Do you want to grant him/her access?</source>
+        <translation>在主机 %1 上有人试图访问你的屏幕。你同意他／她访问吗?</translation>
+    </message>
+    <message>
+        <source>Never for this session</source>
+        <translation>从不使用这个会话</translation>
+    </message>
+    <message>
+        <source>Always for this session</source>
+        <translation>一直使用这个会话</translation>
+    </message>
+    <message>
+        <source>Message from teacher</source>
+        <translation>来自教师的消息</translation>
+    </message>
+    <message>
+        <source>Authentication error</source>
+        <translation>验证错误</translation>
+    </message>
+    <message>
+        <source>Somebody (IP: %1) tried to access this computer but could not authenticate itself successfully!</source>
+        <translation>有人（IP: %1 ）试图访问这台计算机但未能通过验证!</translation>
+    </message>
+</context>
+</TS>
