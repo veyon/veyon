249640effc68a455c564c72dc472a736023ce954
Tobias Doerffel <tobias.doerffel@gmail.com>
2008-03-03 20:55:38 +0000

    release preparations
    git-svn-id: https://italc.svn.sourceforge.net/svnroot/italc/branches/STABLE_1-0@292 d6290f70-d918-0410-89a7-c2321b9b1c3d

--- lib/resources/de.ts
+++ lib/resources/de.ts
@@ -71,7 +71,7 @@
         <translation>BKA (note for translators: the first three characters of this string are the accellerators (underlined characters) of the three input-fields in logon-dialog of windows - please keep this note as otherwise there are strange errors concerning logon-feature)</translation>
     </message>
     <message>
-        <location filename="../src/local_system.cpp" line="704"/>
+        <location filename="../src/local_system.cpp" line="631"/>
         <source>iTALC-snapshots</source>
         <translation>iTALC-Schnappschuesse</translation>
     </message>
@@ -79,7 +79,7 @@
 <context>
     <name>ivsConnection</name>
     <message>
-        <location filename="../src/ivs_connection.cpp" line="307"/>
+        <location filename="../src/ivs_connection.cpp" line="308"/>
         <source>Snapshot</source>
         <translation>Schnappschuss</translation>
     </message>
@@ -89,7 +89,7 @@
         <translation type="obsolete">Konnte keinen Schnappschuss erstellen, da das Verzeichnis %1 nicht existiert und nicht erstellt werden konnte.</translation>
     </message>
     <message>
-        <location filename="../src/ivs_connection.cpp" line="309"/>
+        <location filename="../src/ivs_connection.cpp" line="310"/>
         <source>Could not take a snapshot as directory %1 doesn&apos;t exist and couldn&apos;t be created.</source>
         <translation>Konnte keinen Schnappschuss erstellen, da das Verzeichnis %1 nicht existiert und nicht erstellt werden konnte.</translation>
     </message>
@@ -113,7 +113,7 @@
 <context>
     <name>messageBox</name>
     <message>
-        <location filename="../src/messagebox.cpp" line="75"/>
+        <location filename="../src/messagebox.cpp" line="76"/>
         <source>OK</source>
         <translation>OK</translation>
     </message>
@@ -121,7 +121,7 @@
 <context>
     <name>vncView</name>
     <message>
-        <location filename="../src/vncview.cpp" line="55"/>
+        <location filename="../src/vncview.cpp" line="61"/>
         <source>Establishing connection to %1 ...</source>
         <translation>Verbindung zu %1 wird hergestellt ...</translation>
     </message>
