f4c1d0fbacefa8f325de93513b482d7fc4d17b93
Tobias Doerffel <tobias.doerffel@gmail.com>
2008-03-19 11:08:07 +0000

    added Finnish localization-files by Andrei Krueger <anc/at/iki/dot/fi>
    git-svn-id: https://italc.svn.sourceforge.net/svnroot/italc/branches/STABLE_1-0@304 d6290f70-d918-0410-89a7-c2321b9b1c3d

---
+++ lib/resources/fi.ts
@@ -0,0 +1,53 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="fi_FI">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/local_system.cpp" line="51"/>
+        <source>UPL (note for translators: the first three characters of this string are the accellerators (underlined characters) of the three input-fields in logon-dialog of windows - please keep this note as otherwise there are strange errors concerning logon-feature)</source>
+        <translation>KST (note for translators: the first three characters of this string are the accellerators (underlined characters) of the three input-fields in logon-dialog of windows - please keep this note as otherwise there are strange errors concerning logon-feature)</translation>
+    </message>
+    <message>
+        <location filename="../src/local_system.cpp" line="631"/>
+        <source>iTALC-snapshots</source>
+        <translation>iTALC-kuvaruutukopiot</translation>
+    </message>
+</context>
+<context>
+    <name>ivsConnection</name>
+    <message>
+        <location filename="../src/ivs_connection.cpp" line="308"/>
+        <source>Snapshot</source>
+        <translation>Kuvaruutukopio</translation>
+    </message>
+    <message>
+        <location filename="../src/ivs_connection.cpp" line="310"/>
+        <source>Could not take a snapshot as directory %1 doesn&apos;t exist and couldn&apos;t be created.</source>
+        <translation>Kuvaruutukopion ottaminen epäonnistui, koska hakemistoa %1 ei ole olemassa eikä sitä pystytty luomaan.</translation>
+    </message>
+</context>
+<context>
+    <name>lockWidget</name>
+    <message>
+        <location filename="../src/lock_widget.cpp" line="57"/>
+        <source>screen lock</source>
+        <translation>Lukitse</translation>
+    </message>
+</context>
+<context>
+    <name>messageBox</name>
+    <message>
+        <location filename="../src/messagebox.cpp" line="76"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+</context>
+<context>
+    <name>vncView</name>
+    <message>
+        <location filename="../src/vncview.cpp" line="61"/>
+        <source>Establishing connection to %1 ...</source>
+        <translation>Luodaan yhteyttä %1 ...</translation>
+    </message>
+</context>
+</TS>
