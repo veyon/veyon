e6f9eae11e86d131b714202bd8c5e59563cf74c3
Tobias Doerffel <tobias.doerffel@gmail.com>
2009-07-07 20:53:27 +0200

    Added Turkish localization files
    Added Turkish localization files - thanks to İbrahim Paşa Minisker.
    
    Signed-off-by: Tobias Doerffel <tobias.doerffel@gmail.com>
    (cherry picked from commit 796e2515457e6ecad0e30f915d9bc3ae09833d61)

---
+++ lib/resources/tr.ts
@@ -0,0 +1,53 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/local_system.cpp" line="55"/>
+        <source>UPL (note for translators: the first three characters of this string are the accellerators (underlined characters) of the three input-fields in logon-dialog of windows - please keep this note as otherwise there are strange errors concerning logon-feature)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/local_system.cpp" line="662"/>
+        <source>iTALC-snapshots</source>
+        <translation>iTALC-ekran görüntüsü</translation>
+    </message>
+</context>
+<context>
+    <name>ivsConnection</name>
+    <message>
+        <location filename="../src/ivs_connection.cpp" line="308"/>
+        <source>Snapshot</source>
+        <translation>Ekran görüntüsü</translation>
+    </message>
+    <message>
+        <location filename="../src/ivs_connection.cpp" line="310"/>
+        <source>Could not take a snapshot as directory %1 doesn&apos;t exist and couldn&apos;t be created.</source>
+        <translation>%1 dizin varolmadığından ekran görüntüsü oluşturulamadı</translation>
+    </message>
+</context>
+<context>
+    <name>lockWidget</name>
+    <message>
+        <location filename="../src/lock_widget.cpp" line="69"/>
+        <source>screen lock</source>
+        <translation>Ekranı kilitle</translation>
+    </message>
+</context>
+<context>
+    <name>messageBox</name>
+    <message>
+        <location filename="../src/messagebox.cpp" line="76"/>
+        <source>OK</source>
+        <translation>TAMAM</translation>
+    </message>
+</context>
+<context>
+    <name>vncView</name>
+    <message>
+        <location filename="../src/vncview.cpp" line="61"/>
+        <source>Establishing connection to %1 ...</source>
+        <translation>Bağlantı kuruluyor %1...</translation>
+    </message>
+</context>
+</TS>
