60b62f5aafd94d5e3ca63765be09a4ea93542f0d
Christian Beier <dontmind@freeshell.org>
2010-07-11 15:31:07 +0200

    Fix MinGW32 checking for IPv6.
    Signed-off-by: Christian Beier <dontmind@freeshell.org>
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    (cherry picked from commit 79f0f1374cabb3d252e9eda0d13fc21857991b2e)

--- ica/x11/libvncclient/sockets.c
+++ ica/x11/libvncclient/sockets.c
@@ -37,6 +37,10 @@
 #define read(sock,buf,len) recv(sock,buf,len,0)
 #define write(sock,buf,len) send(sock,buf,len,0)
 #define socklen_t int
+#ifdef LIBVNCSERVER_HAVE_WS2TCPIP_H
+#undef socklen_t
+#include <ws2tcpip.h>
+#endif
 #else
 #include <sys/socket.h>
 #include <netinet/in.h>
