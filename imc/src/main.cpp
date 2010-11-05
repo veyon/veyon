02dc3f57b67c809b2e2b80292705e9201843d7b9
Tobias Doerffel <tobias.doerffel@gmail.com>
2010-11-05 11:20:29 +0100

    IMC: added source code skeleton
    The IMC is now integrated into the build system and is now built as a
    stand alone application.

---
+++ imc/src/main.cpp
@@ -0,0 +1,78 @@
+/*
+ * main.cpp - main file for iTALC Management Console
+ *
+ * Copyright (c) 2010 Tobias Doerffel <tobydox/at/users/dot/sf/dot/net>
+ *
+ * This file is part of iTALC - http://italc.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#include <QtCore/QLocale>
+#include <QtCore/QTranslator>
+#include <QtGui/QApplication>
+
+#include "MainWindow.h"
+#include "LocalSystem.h"
+#include "Logger.h"
+
+
+
+// good old main-function... initializes and starts application
+int main( int argc, char **argv )
+{
+	QApplication app( argc, argv );
+
+	LocalSystem::initialize( NULL );
+
+	Logger l( "ItalcManagementConsole" );
+
+	app.connect( &app, SIGNAL( lastWindowClosed() ), SLOT( quit() ) );
+
+	const QString loc = QLocale::system().name().left( 2 );
+
+	foreach( const QString & qm, QStringList()
+												<< loc + "-core"
+												<< loc
+												<< "qt_" + loc )
+	{
+		QTranslator * tr = new QTranslator( &app );
+		tr->load( QString( ":/resources/%1.qm" ).arg( qm ) );
+		app.installTranslator( tr );
+	}
+
+
+	// parse arguments
+	QStringListIterator argIt( QCoreApplication::arguments() );
+	argIt.next();
+
+	while( argc > 1 && argIt.hasNext() )
+	{
+		const QString & a = argIt.next();
+	}
+
+	// now create the main window
+	MainWindow mainWindow;
+
+	mainWindow.show();
+
+	ilog( Info, "App.Exec" );
+
+	// let's rock
+	return app.exec();
+}
+
