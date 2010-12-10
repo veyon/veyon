veyon/veyon

commit df7ac8c6082518955afaa916f490d9d434f2a788

ICA/WindowsService: fixed too server restart on XP
On user logoff, we restart ICA in order to always run it within the
currently active session. On WinXP, the WM_QUERYENDSESSION was event
sent to the restarted server which then terminated itself immediatly.
Fixed this by periodically checking whether server is still running
and if not, start it.

Closes #3122846.

ica/src/WindowsService.cpp
@@ -84,6 +84,17 @@ public:
 		}
 	}
 
+	bool isRunning() const
+	{
+		if( m_subProcessHandle &&
+			WaitForSingleObject( m_subProcessHandle, 5000 ) == WAIT_TIMEOUT )
+		{
+			return true;
+		}
+
+		return false;
+	}
+
 
 private:
 	HANDLE m_subProcessHandle;
@@ -772,10 +783,12 @@ void WindowsService::monitorSessions()
 				SetEvent( hShutdownEvent );
 				italcProcess.stop();
 			}
-			if( sessionId != SESSION_INVALID || sessionChanged )
+			if( sessionId != SESSION_INVALID || sessionChanged ||
+					!italcProcess.isRunning() )
 			{
 				italcProcess.start( sessionId );
 			}
+
 			oldSessionId = sessionId;
 		}
 	}

commit de0d441d90cd368ad7b41d4ded2c4b5f2173624a

ICA/WindowsService: always check whether server process is still running
Commit df7ac8c6082518955afaa916f490d9d434f2a788 introduced some wrong
logic which made the WindowsService class only check whether the server
process is still running if the session changes. However this should be
done always.

ica/src/WindowsService.cpp
@@ -785,12 +785,16 @@ void WindowsService::monitorSessions()
 				SetEvent( hShutdownEvent );
 				italcProcess.stop();
 			}
-			if( sessionId != SESSION_INVALID || sessionChanged ||
-					!italcProcess.isRunning() )
+			if( sessionId != SESSION_INVALID || sessionChanged )
 			{
 				italcProcess.start( sessionId );
 			}
 
+			oldSessionId = sessionId;
+		}
+		else if( italcProcess.isRunning() == false )
+		{
+			italcProcess.start( sessionId );
 			oldSessionId = sessionId;
 		}
 	}