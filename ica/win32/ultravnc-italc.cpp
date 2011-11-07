veyon/veyon

commit 0662bd48839306a871a4188ae27d970bd598ff88

ICA/VncServer/Win32: declare secure string functions
Instead of replacing the secure string functions with macros, declare
them properly as they can be imported from MSVCRT.

ica/win32/winvnc/winvnc/stdhdrs.h
@@ -77,10 +77,13 @@ extern VNCLog vnclog;
 // Log everything, including internal table setup, etc.
 #define LL_ALL		10
 
-#define strcat_s(dst,num,src) strncat(dst,src,num)
-#define strncat_s(a,b,c,d) strncat(a,c,b)
-#define strcpy_s(dst,num,src)	strncpy(dst,src,num)
-#define strncpy_s(dst,num,src,x)	strncpy(dst,src,num)
+extern "C"
+{
+errno_t strcat_s(char* dst, size_t size, const char* src);
+errno_t strncat_s(char* dst, size_t size, const char* src, size_t n);
+errno_t strcpy_s(char* dst, size_t size, const char* src);
+errno_t strncpy_s(char* dst, size_t size, const char* src, size_t n);
+} ;
 
 #define KEEPALIVE_HEADROOM 1
 #define KEEPALIVE_INTERVAL 5

commit 25e2581fa4f93b8845176a1c8f20961756dd6d34

ICA/VncServer/Win32: manually implement secure string functions
Commit 0662bd48839306a871a4188ae27d970bd598ff88 broke things for various
platforms as the secure string functions are implemented in different
DLL files in different Windows versions. Therefore added manual
implementations from the WINE source.

ica/win32/ultravnc-italc.cpp
@@ -110,3 +110,168 @@ BOOL ultravnc_italc_ask_permission( const char *username, const char *host )
 }
 
 
+
+/*
+ * MSVCRT string functions
+ *
+ * Copyright 1996,1998 Marcus Meissner
+ * Copyright 1996 Jukka Iivonen
+ * Copyright 1997,2000 Uwe Bonnes
+ * Copyright 2000 Jon Griffiths
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+
+#define CHECK_PMT(x) (x)
+#define SIZE_T_MAX	((size_t)-1)
+
+
+errno_t strcat_s( char* dst, size_t elem, const char* src )
+{
+    size_t i, j;
+    if(!dst) return EINVAL;
+    if(elem == 0) return EINVAL;
+    if(!src)
+    {
+        dst[0] = '\0';
+        return EINVAL;
+    }
+
+    for(i = 0; i < elem; i++)
+    {
+        if(dst[i] == '\0')
+        {
+            for(j = 0; (j + i) < elem; j++)
+            {
+                if((dst[j + i] = src[j]) == '\0') return 0;
+            }
+        }
+    }
+    /* Set the first element to 0, not the first element after the skipped part */
+    dst[0] = '\0';
+    return ERANGE;
+}
+
+errno_t strncat_s( char* dst, size_t elem, const char* src, size_t count )
+{
+    size_t i, j;
+    if(!CHECK_PMT(dst != 0) || !CHECK_PMT(elem != 0))
+        return EINVAL;
+    if(!CHECK_PMT(src != 0))
+    {
+        dst[0] = '\0';
+        return EINVAL;
+    }
+
+    for(i = 0; i < elem; i++)
+    {
+        if(dst[i] == '\0')
+        {
+            for(j = 0; (j + i) < elem; j++)
+            {
+                if(count == SIZE_T_MAX && j + i == elem - 1)
+                {
+                    dst[j + i] = '\0';
+                    return STRUNCATE;
+                }
+                if(j == count || (dst[j + i] = src[j]) == '\0')
+                {
+                    dst[j + i] = '\0';
+                    return 0;
+                }
+            }
+        }
+    }
+    /* Set the first element to 0, not the first element after the skipped part */
+    dst[0] = '\0';
+    return ERANGE;
+}
+
+
+errno_t strcpy_s(char* dst, size_t elem, const char* src)
+{
+    size_t i;
+    if(!elem) return EINVAL;
+    if(!dst) return EINVAL;
+    if(!src)
+    {
+        dst[0] = '\0';
+        return EINVAL;
+    }
+
+    for(i = 0; i < elem; i++)
+    {
+        if((dst[i] = src[i]) == '\0') return 0;
+    }
+    dst[0] = '\0';
+    return ERANGE;
+}
+
+
+
+/*
+ * msvcrt.dll heap functions
+ *
+ * Copyright 2000 Jon Griffiths
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ *
+ * Note: Win32 heap operations are MT safe. We only lock the new
+ *       handler and non atomic heap operations
+ */
+
+
+errno_t strncpy_s(char* dst, size_t numberOfElements, const char* src, size_t count)
+{
+    size_t i, end;
+
+    if(!count)
+        return 0;
+
+    if (!CHECK_PMT(dst != NULL) || !CHECK_PMT(src != NULL) || !CHECK_PMT(numberOfElements != 0)) {
+        *_errno() = EINVAL;
+        return EINVAL;
+    }
+
+    if(count!=SIZE_T_MAX && count<numberOfElements)
+        end = count;
+    else
+        end = numberOfElements-1;
+
+    for(i=0; i<end && src[i]; i++)
+        dst[i] = src[i];
+
+    if(!src[i] || end==count || count==SIZE_T_MAX) {
+        dst[i] = '\0';
+        return 0;
+    }
+
+    dst[0] = '\0';
+    *_errno() = EINVAL;
+    return EINVAL;
+}

ica/win32/winvnc/winvnc/stdhdrs.h
@@ -77,13 +77,10 @@ extern VNCLog vnclog;
 // Log everything, including internal table setup, etc.
 #define LL_ALL		10
 
-extern "C"
-{
 errno_t strcat_s(char* dst, size_t size, const char* src);
 errno_t strncat_s(char* dst, size_t size, const char* src, size_t n);
 errno_t strcpy_s(char* dst, size_t size, const char* src);
 errno_t strncpy_s(char* dst, size_t size, const char* src, size_t n);
-} ;
 
 #define KEEPALIVE_HEADROOM 1
 #define KEEPALIVE_INTERVAL 5