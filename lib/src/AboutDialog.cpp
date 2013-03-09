acc78358a48183caca923c0a7f27e58a386da1ef
Tobias Doerffel <tobias.doerffel@gmail.com>
2013-03-09 17:09:16 +0100

    AboutDialog: update for year 2013

--- lib/src/AboutDialog.cpp
+++ lib/src/AboutDialog.cpp
@@ -1,7 +1,7 @@
 /*
  * AboutDialog.cpp - implementation of AboutDialog
  *
- * Copyright (c) 2011 Tobias Doerffel <tobydox/at/users/dot/sf/dot/net>
+ * Copyright (c) 2011-2013 Tobias Doerffel <tobydox/at/users/dot/sf/dot/net>
  *
  * This file is part of iTALC - http://italc.sourceforge.net
  *
