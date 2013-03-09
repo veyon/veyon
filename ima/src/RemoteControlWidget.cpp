9a633ba4e42231f38e44fe98c39c24ccb088c1c8
Tobias Doerffel <tobias.doerffel@gmail.com>
2013-03-09 16:44:14 +0100

    IMA/RemoteControlWidget: send user update request earlier
    Thanks to Silvio <silvio13/at/gmail/dot/com> for the hint.

--- ima/src/RemoteControlWidget.cpp
+++ ima/src/RemoteControlWidget.cpp
@@ -1,7 +1,7 @@
 /*
  *  RemoteControlWidget.cpp - widget containing a VNC-view and controls for it
  *
- *  Copyright (c) 2006-2011 Tobias Doerffel <tobydox/at/users/dot/sf/dot/net>
+ *  Copyright (c) 2006-2013 Tobias Doerffel <tobydox/at/users/dot/sf/dot/net>
  *
  *  This file is part of iTALC - http://italc.sourceforge.net
  *
@@ -257,6 +257,8 @@ void RemoteControlWidgetToolBar::startConnection()
 
 void RemoteControlWidgetToolBar::connectionEstablished()
 {
+	m_parent->m_coreConnection->sendGetUserInformationRequest();
+
 	m_connecting = false;
 	m_iconStateTimeLine.stop();
 	QTimer::singleShot( 3000, this, SLOT( disappear() ) );
