veyon/veyon

commit 193d342595a79de4e3fff0c1cee7302bbad3680a

ICA/libvncclient: do not redefine EWOULDBLOCK
Recent versions of MinGW runtime already define the EWOULDBLOCK symbol.

ica/x11/libvncclient/sockets.c
@@ -38,7 +38,9 @@
 #ifdef WIN32
 #undef SOCKET
 #include <winsock2.h>
+#ifndef EWOULDBLOCK
 #define EWOULDBLOCK WSAEWOULDBLOCK
+#endif
 #define close closesocket
 #define read(sock,buf,len) recv(sock,buf,len,0)
 #define write(sock,buf,len) send(sock,buf,len,0)

commit c87178299af2402615db3cbab31fb94e46680d5a

ICA/libvncclient: always redefine EWOULDBLOCK
Commit 193d342595a79de4e3fff0c1cee7302bbad3680a broke libvncclient's
socket layer completely by using the EWOULDBLOCK error code from errno.h
instead of the corresponding WSA error code.

ica/x11/libvncclient/sockets.c
@@ -38,9 +38,11 @@
 #ifdef WIN32
 #undef SOCKET
 #include <winsock2.h>
-#ifndef EWOULDBLOCK
-#define EWOULDBLOCK WSAEWOULDBLOCK
+/* make sure to use the WSA error codes instead of the POSIX ones defined in errno.h */
+#ifdef EWOULDBLOCK
+#undef EWOULDBLOCK
 #endif
+#define EWOULDBLOCK WSAEWOULDBLOCK
 #define close closesocket
 #define read(sock,buf,len) recv(sock,buf,len,0)
 #define write(sock,buf,len) send(sock,buf,len,0)