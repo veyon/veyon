14ec752f8596400fe4b1b696d58800fc318144a9
Tobias Doerffel <tobias.doerffel@gmail.com>
2016-04-25 21:56:38 +0200

    SocketDevice: added missing header for Qt5 compatibility

--- lib/include/SocketDevice.h
+++ lib/include/SocketDevice.h
@@ -1,7 +1,7 @@
 /*
  * SocketDevice.h - SocketDevice abstraction
  *
- * Copyright (c) 2010 Tobias Doerffel <tobydox/at/users/dot/sf/dot/net>
+ * Copyright (c) 2010-2016 Tobias Doerffel <tobydox/at/users/dot/sf/dot/net>
  *
  * This file is part of iTALC - http://italc.sourceforge.net
  *
@@ -25,6 +25,7 @@
 #ifndef SOCKET_DEVICE_H
 #define SOCKET_DEVICE_H
 
+#include <QtCore/QDataStream>
 #include <QtCore/QIODevice>
 #include <QtCore/QString>
 #include <QtCore/QVariant>
