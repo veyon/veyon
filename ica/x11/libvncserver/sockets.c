veyon/veyon

commit b71f01bf693aa162226751aca26c972089d051a5

ICA/libvncserver: do not redefine EWOULDBLOCK and ETIMEDOUT
Recent versions of MinGW runtime already define the EWOULDBLOCK and
ETIMEDOUT symbols.

ica/x11/libvncserver/sockets.c
@@ -102,8 +102,12 @@ int deny_severity=LOG_WARNING;
 #pragma warning (disable: 4018 4761)
 #endif
 #define read(sock,buf,len) recv(sock,buf,len,0)
+#ifndef EWOULDBLOCK
 #define EWOULDBLOCK WSAEWOULDBLOCK
+#endif
+#ifndef ETIMEDOUT
 #define ETIMEDOUT WSAETIMEDOUT
+#endif
 #define write(sock,buf,len) send(sock,buf,len,0)
 #else
 #define closesocket close

commit ec3e5afe3194fcd99b498b73c59ad15c44ea4bcf

libvncserver/sockets: always redefine EWOULDBLOCK and ETIMEDOUT on Win32
Commit b71f01bf693aa162226751aca26c972089d051a5 introduced a severe
regression which completely broke socket read and write functions for
libvncserver on Win32. The symbols EWOULDBLOCK and ETIMEDOUT always have
to correspond to the WSA error codes on Win32 in order to make the code
in rfbReadExactTimeout() and rfbWriteExact() work properly

This finally fixes the broken demo mode. Thanks to the Univention GmbH
for sponsoring the debugging of this issue.

ica/x11/libvncserver/sockets.c
@@ -105,12 +105,14 @@ int deny_severity=LOG_WARNING;
 #pragma warning (disable: 4018 4761)
 #endif
 #define read(sock,buf,len) recv(sock,buf,len,0)
-#ifndef EWOULDBLOCK
+#ifdef EWOULDBLOCK
+#undef EWOULDBLOCK
+#endif
 #define EWOULDBLOCK WSAEWOULDBLOCK
+#ifdef ETIMEDOUT
+#undef ETIMEDOUT
 #endif
-#ifndef ETIMEDOUT
 #define ETIMEDOUT WSAETIMEDOUT
-#endif
 #define write(sock,buf,len) send(sock,buf,len,0)
 #else
 #define closesocket close