d4074c3aaebaed5338a9b20e64176f1b99cae630
Tobias Doerffel <tobias.doerffel@gmail.com>
2016-07-25 22:46:33 +0200

    ItalcCoreServer: only report failed authentication attempts once per host
    Closes #31.

--- ica/src/ItalcCoreServer.cpp
+++ ica/src/ItalcCoreServer.cpp
@@ -47,6 +47,7 @@ ItalcCoreServer * ItalcCoreServer::_this = NULL;
 ItalcCoreServer::ItalcCoreServer() :
 	QObject(),
 	m_allowedIPs(),
+	m_failedAuthHosts(),
 	m_slaveManager()
 {
 	Q_ASSERT( _this == NULL );
@@ -342,11 +343,14 @@ bool ItalcCoreServer::authSecTypeItalc( socketDispatcher sd, void *user )
 
 void ItalcCoreServer::errorMsgAuth( const QString &ip )
 {
-	_this->m_slaveManager.systemTrayMessage(
-			tr( "Authentication error" ),
-			tr( "Somebody (IP: %1) tried to access this computer "
-					"but could not authenticate itself "
-					"successfully!" ).arg( ip ) );
+	if( _this->m_failedAuthHosts.contains( ip ) == false )
+	{
+		_this->m_failedAuthHosts += ip;
+		_this->m_slaveManager.systemTrayMessage(
+					tr( "Authentication error" ),
+					tr( "Somebody (IP: %1) tried to access this computer "
+						"but could not authenticate successfully!" ).arg( ip ) );
+	}
 }
 
 
