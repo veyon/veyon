12d0be011e7db94fc3b37b207ecc024263ba5a68
Tobias Doerffel <tobias.doerffel@gmail.com>
2016-11-25 18:59:32 +0100

    IMC/PageAccessControl: tie configuration and UI together via ConfigurationPage

--- imc/src/PageAccessControl.cpp
+++ imc/src/PageAccessControl.cpp
@@ -1,8 +1,36 @@
+/*
+ * PageAccessControl.cpp - implementation of the access control page in IMC
+ *
+ * Copyright (c) 2016 Tobias Doerffel <tobydox/at/users/dot/sf/dot/net>
+ *
+ * This file is part of iTALC - http://italc.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
 #include "PageAccessControl.h"
+#include "ItalcCore.h"
+#include "ItalcConfiguration.h"
+#include "Configuration/UiMapping.h"
+
 #include "ui_PageAccessControl.h"
 
-PageAccessControl::PageAccessControl(QWidget *parent) :
-	QWidget(parent),
+PageAccessControl::PageAccessControl() :
+	ConfigurationPage(),
 	ui(new Ui::PageAccessControl)
 {
 	ui->setupUi(this);
@@ -13,6 +41,20 @@ PageAccessControl::PageAccessControl(QWidget *parent) :
 PageAccessControl::~PageAccessControl()
 {
 	delete ui;
+}
+
+
+
+void PageAccessControl::resetWidgets()
+{
+	FOREACH_ITALC_ACCESS_CONTROL_CONFIG_PROPERTY(INIT_WIDGET_FROM_PROPERTY);
+}
+
+
+
+void PageAccessControl::connectWidgetsToProperties()
+{
+	FOREACH_ITALC_ACCESS_CONTROL_CONFIG_PROPERTY(CONNECT_WIDGET_TO_PROPERTY)
 }
 
 
--- imc/src/PageAccessControl.h
+++ imc/src/PageAccessControl.h
@@ -1,19 +1,46 @@
-#ifndef PAGEACCESSCONTROL_H
-#define PAGEACCESSCONTROL_H
+/*
+ * PageAccessControl.h - header for the PageAccessControl class
+ *
+ * Copyright (c) 2016 Tobias Doerffel <tobydox/at/users/dot/sf/dot/net>
+ *
+ * This file is part of iTALC - http://italc.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
 
-#include <QWidget>
+#ifndef PAGE_ACCESS_CONTROL_H
+#define PAGE_ACCESS_CONTROL_H
+
+#include "ConfigurationPage.h"
 
 namespace Ui {
 class PageAccessControl;
 }
 
-class PageAccessControl : public QWidget
+class PageAccessControl : public ConfigurationPage
 {
 	Q_OBJECT
 
 public:
-	explicit PageAccessControl(QWidget *parent = 0);
+	PageAccessControl();
 	~PageAccessControl();
+
+	virtual void resetWidgets();
+	virtual void connectWidgetsToProperties();
 
 private slots:
 	void addGroup();
@@ -28,4 +55,4 @@ private:
 	Ui::PageAccessControl *ui;
 };
 
-#endif // PAGEACCESSCONTROL_H
+#endif // PAGE_ACCESS_CONTROL_H
