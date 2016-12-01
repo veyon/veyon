b71ffbcdb27bacd27fe3a0b9c8958b316e4ce80a
Tobias Doerffel <tobias.doerffel@gmail.com>
2016-12-01 19:22:40 +0100

    ItalcCore: add new class AccessControlRule
    An AccessControlRule represents a rule which defines under which
    conditions to take which action if a computer access is requested.
    The class provides functions for serializing and deserializing the rule
    to/from a (base64-encoded) string.

---
+++ lib/include/AccessControlRule.h
@@ -0,0 +1,157 @@
+/*
+ * AccessControlRule.h - declaration of class AccessControlRule
+ *
+ * Copyright (c) 2016 Tobias Doerffel <tobydox/at/users/dot/sf/dot/net>
+ *
+ * This file is part of iTALC - http://italc.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#ifndef ACCESS_CONTROL_RULE_H
+#define ACCESS_CONTROL_RULE_H
+
+#include <QObject>
+#include <QVariant>
+#include <QMetaEnum>
+
+class AccessControlRule : public QObject
+{
+	Q_GADGET
+public:
+	typedef enum Actions
+	{
+		ActionNone,
+		ActionAllow,
+		ActionDeny,
+		ActionCount
+	} Action;
+
+	Q_ENUM(Action)
+
+
+	typedef enum Entities
+	{
+		EntityNone,
+		EntityAccessingUser,
+		EntityAccessingComputer,
+		EntityLocalUser,
+		EntityLocalComputer,
+		EntityCount
+	} Entity;
+
+	Q_ENUM(Entity)
+
+
+	enum Condition
+	{
+		ConditionNone,
+		ConditionMemberOfGroup,
+		ConditionMemberOfComputerPool,
+		ConditionGroupsInCommon,
+		ConditionComputerPoolsInCommon,
+		ConditionCount
+	} ;
+
+	Q_ENUM(Condition)
+
+
+	typedef QVariant ConditionArgument;
+	typedef QMap<Condition, ConditionArgument> Conditions;
+
+
+	AccessControlRule();
+	AccessControlRule( const AccessControlRule& other );
+	AccessControlRule( const QString& encodedData );
+
+	const QString& name() const
+	{
+		return m_name;
+	}
+
+	void setName( const QString& name )
+	{
+		m_name = name;
+	}
+
+	const QString& description() const
+	{
+		return m_description;
+	}
+
+	void setDescription( const QString& description )
+	{
+		m_description = description;
+	}
+
+	Action action() const
+	{
+		return m_action;
+	}
+
+	void setAction( Action action )
+	{
+		m_action = action;
+	}
+
+	Entity entity() const
+	{
+		return m_entity;
+	}
+
+	void setEntity( Entity entity )
+	{
+		m_entity = entity;
+	}
+
+	bool hasCondition( Condition condition ) const
+	{
+		return m_conditions.contains( condition );
+	}
+
+	const Conditions conditions() const
+	{
+		return m_conditions;
+	}
+
+	ConditionArgument conditionArgument( Condition condition )
+	{
+		return m_conditions.value( condition );
+	}
+
+	void clearCondition( Condition condition )
+	{
+		m_conditions.remove( condition );
+	}
+
+	void setCondition( Condition condition, ConditionArgument conditionArgument )
+	{
+		m_conditions[condition] = conditionArgument;
+	}
+
+	QString encode() const;
+
+private:
+	QString m_name;
+	QString m_description;
+	Action m_action;
+	Entity m_entity;
+	Conditions m_conditions;
+
+} ;
+
+#endif
---
+++ lib/src/AccessControlRule.cpp
@@ -0,0 +1,106 @@
+/*
+ * AccessControlRule.cpp - implementation of the AccessControlRule class
+ *
+ * Copyright (c) 2016 Tobias Doerffel <tobydox/at/users/dot/sf/dot/net>
+ *
+ * This file is part of iTALC - http://italc.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#include <QDataStream>
+
+#include "AccessControlRule.h"
+
+AccessControlRule::AccessControlRule() :
+	m_name(),
+	m_description(),
+	m_action( ActionNone ),
+	m_entity( EntityNone ),
+	m_conditions()
+{
+
+}
+
+
+
+AccessControlRule::AccessControlRule(const AccessControlRule &other) :
+	m_name( other.name() ),
+	m_description( other.description() ),
+	m_action( other.action() ),
+	m_entity( other.entity() ),
+	m_conditions( other.conditions() )
+{
+
+}
+
+
+
+AccessControlRule::AccessControlRule(const QString &encodedData) :
+	m_name(),
+	m_description(),
+	m_action( ActionNone ),
+	m_entity( EntityNone ),
+	m_conditions()
+{
+	QByteArray buffer = QByteArray::fromBase64( encodedData.toUtf8() );
+	QDataStream ds( buffer );
+
+	qint8 action;
+	qint8 entity;
+	qint8 conditionCount;
+
+	ds >> m_name;
+	ds >> m_description;
+	ds >> action;
+	ds >> entity;
+	ds >> conditionCount;
+
+	for( int i = 0; i < conditionCount; ++i )
+	{
+		qint8 condition;
+		QVariant conditionArgument;
+		ds >> condition;
+		ds >> conditionArgument;
+		m_conditions[static_cast<Condition>(condition)] = conditionArgument;
+	}
+
+	m_action = static_cast<Action>( action );
+	m_entity = static_cast<Entity>( entity );
+}
+
+
+
+QString AccessControlRule::encode() const
+{
+	QByteArray buffer;
+	QDataStream ds( &buffer, QIODevice::ReadWrite );
+
+	ds << m_name;
+	ds << m_description;
+	ds << static_cast<qint8>( m_action );
+	ds << static_cast<qint8>( m_entity );
+	ds << static_cast<qint8>( m_conditions.count() );
+
+	for( auto condition : m_conditions.keys() )
+	{
+		ds << static_cast<qint8>( condition );
+		ds << m_conditions[condition];
+	}
+
+	return QString::fromUtf8( buffer.toBase64() );
+}
