6fbeaf3400a09e2ea2c42e6c04a41f60568a64fa
Tobias Doerffel <tobias.doerffel@gmail.com>
2016-12-05 17:33:29 +0100

    Core: ItalcVncConnection: return copy of QImage
    In order to provide thread-safe access to the framebuffer image, we have
    to return a deep copy in ItalcVncConnection::image() as the underlying
    memory could be deleted concurrently due to framebuffer size changes.

--- lib/src/ItalcVncConnection.cpp
+++ lib/src/ItalcVncConnection.cpp
@@ -467,7 +467,7 @@ const QImage ItalcVncConnection::image( int x, int y, int w, int h ) const
 
 	if( w == 0 || h == 0 ) // full image requested
 	{
-		return m_image;
+		return m_image.copy();
 	}
 	return m_image.copy( x, y, w, h );
 }
