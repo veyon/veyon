bf9f42b7376496926748569efd8d9e46ef30d1e2
Tobias Doerffel <tobias.doerffel@gmail.com>
2016-12-06 19:29:06 +0100

    Travis: install qttools5-dev

--- .travis/linux..install.sh
+++ .travis/linux..install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-PACKAGES="cmake qtbase5-dev qtbase5-dev-tools xorg-dev libxtst-dev libjpeg-dev zlib1g-dev libssl-dev libpam0g-dev"
+PACKAGES="cmake qtbase5-dev qtbase5-dev-tools qttools5-dev xorg-dev libxtst-dev libjpeg-dev zlib1g-dev libssl-dev libpam0g-dev"
 
 sudo apt-get install -y $PACKAGES
