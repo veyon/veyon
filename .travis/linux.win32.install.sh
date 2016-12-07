230623ba2a3930f6e91c45af66594087fe73eb94
Tobias Doerffel <tobias.doerffel@gmail.com>
2016-12-07 09:55:05 +0100

    Travis: add tofrodos build dependency

--- .travis/linux.win32.install.sh
+++ .travis/linux.win32.install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PACKAGES="nsis g++-mingw-w64 qt5base-mingw-w64 qt5tools-mingw-w64
+PACKAGES="nsis tofrodos g++-mingw-w64 qt5base-mingw-w64 qt5tools-mingw-w64
 	openssl-mingw-w64 libz-mingw-w64-dev libpng-mingw-w64 libjpeg-mingw-w64"
 
 sudo apt-get install -y --force-yes $PACKAGES
