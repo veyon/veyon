166537bb8a22dcc1412e4288b81b5e02c7ede2c7
Tobias Doerffel <tobias.doerffel@gmail.com>
2016-12-08 16:34:28 +0100

    Travis: install packages from Ubuntu Xenial for Linux build

--- .travis/linux..install.sh
+++ .travis/linux..install.sh
@@ -2,4 +2,4 @@
 
 PACKAGES="cmake qtbase5-dev qtbase5-dev-tools qttools5-dev qttools5-dev-tools xorg-dev libxtst-dev libjpeg-dev zlib1g-dev libssl-dev libpam0g-dev libldap2-dev"
 
-sudo apt-get install -y $PACKAGES
+sudo apt-get install -y --force-yes $PACKAGES
