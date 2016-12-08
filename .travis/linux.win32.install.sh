3265660f3aa561eb841a5cad9b3e3814f250ac3f
Tobias Doerffel <tobias.doerffel@gmail.com>
2016-12-08 19:44:42 +0100

    Travis: update make and cmake

--- .travis/linux.win32.install.sh
+++ .travis/linux.win32.install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PACKAGES="nsis tofrodos g++-mingw-w64 qt5base-mingw-w64 qt5tools-mingw-w64
+PACKAGES="cmake make nsis tofrodos g++-mingw-w64 qt5base-mingw-w64 qt5tools-mingw-w64
 	openssl-mingw-w64 libz-mingw-w64-dev libpng-mingw-w64 libjpeg-mingw-w64
 	openldap-mingw-w64"
 
