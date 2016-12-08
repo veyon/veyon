a69e424ea69c64f0593f8a20b32e9c3eeeb61536
Tobias Doerffel <tobias.doerffel@gmail.com>
2016-12-08 16:28:25 +0100

    Travis: install OpenLDAP library and development files

--- .travis/linux.win32.install.sh
+++ .travis/linux.win32.install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 PACKAGES="nsis tofrodos g++-mingw-w64 qt5base-mingw-w64 qt5tools-mingw-w64
-	openssl-mingw-w64 libz-mingw-w64-dev libpng-mingw-w64 libjpeg-mingw-w64"
+	openssl-mingw-w64 libz-mingw-w64-dev libpng-mingw-w64 libjpeg-mingw-w64
+	openldap-mingw-w64"
 
 sudo apt-get install -y --force-yes $PACKAGES
