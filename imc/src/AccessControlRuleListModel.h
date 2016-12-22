bdd06c2e637df7d7c728f0d3483b726ff975e9b4
Tobias Doerffel <tobias.doerffel@gmail.com>
2016-12-22 16:39:25 +0100

    IMC: add data model class AccessControlRuleListModel
    The data model provides access control rules for usage in a list view or
    similiar.

---
+++ imc/src/AccessControlRuleListModel.h
@@ -0,0 +1,49 @@
+/*
+ * AccessControlRuleListModel.h - data model for access control rules
+ *
+ * Copyright (c) 2016 Tobias Doerffel <tobydox/at/users/dot/sf/dot/net>
+ *
+ * This file is part of iTALC - http://italc.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#ifndef ACCESS_CONTROL_RULE_LIST_MODEL_H
+#define ACCESS_CONTROL_RULE_LIST_MODEL_H
+
+#include <QAbstractListModel>
+
+#include "AccessControlRule.h"
+
+class AccessControlRuleListModel : public QAbstractListModel
+{
+	Q_OBJECT
+public:
+	explicit AccessControlRuleListModel(QObject *parent = 0);
+
+	void reload();
+
+	int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+
+	QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+
+private:
+	QList<AccessControlRule> m_accessControlRules;
+
+};
+
+#endif // ACCESS_CONTROL_RULE_LIST_MODEL_H
