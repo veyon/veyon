37b7532085f373754fb7fd2d847b60eb7091c62a
Tobias Doerffel <tobias.doerffel@gmail.com>
2016-12-27 14:43:02 +0100

    Travis: initial import for iTALC 2

---
+++ .travis/linux..install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+PACKAGES="cmake libqt4-dev libqt4-dev-bin qt4-dev-tools xorg-dev libxtst-dev libjpeg-dev zlib1g-dev libssl-dev libpam0g-dev"
+
+sudo apt-get install -y $PACKAGES
