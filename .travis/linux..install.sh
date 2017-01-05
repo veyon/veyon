635e01ae701ea5bbcbf2d8a2f8d55a22effafe6b
Tobias Doerffel <tobias.doerffel@gmail.com>
2017-01-05 19:09:40 +0100

    Travis: fixed package name

--- .travis/linux..install.sh
+++ .travis/linux..install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-PACKAGES="binutils gcc g++ cmake qtbase5-dev qtbase5-dev-tools qttools5-dev qttools5-dev-tools xorg-dev libxtst-dev libjpeg-dev zlib1g-dev libssl-dev libpam0g-dev libldap2-dev libzo2-dev"
+PACKAGES="binutils gcc g++ cmake qtbase5-dev qtbase5-dev-tools qttools5-dev qttools5-dev-tools xorg-dev libxtst-dev libjpeg-dev zlib1g-dev libssl-dev libpam0g-dev libldap2-dev liblzo2-dev"
 
 sudo apt-get install -y --force-yes $PACKAGES
