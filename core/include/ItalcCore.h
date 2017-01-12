cccfa670c084e72fbb8b13ea3148e0f4b0b6b853
Tobias Doerffel <tobias.doerffel@gmail.com>
2017-01-12 15:50:40 +0100

    Core: always include italcconfig.h

--- core/include/ItalcCore.h
+++ core/include/ItalcCore.h
@@ -1,7 +1,7 @@
 /*
  * ItalcCore.h - definitions for iTALC Core
  *
- * Copyright (c) 2006-2016 Tobias Doerffel <tobydox/at/users/dot/sf/dot/net>
+ * Copyright (c) 2006-2017 Tobias Doerffel <tobydox/at/users/dot/sf/dot/net>
  *
  * This file is part of iTALC - http://italc.sourceforge.net
  *
@@ -24,6 +24,8 @@
 
 #ifndef ITALC_CORE_H
 #define ITALC_CORE_H
+
+#include <italcconfig.h>
 
 #include <QtCore/QPair>
 #include <QtCore/QString>
