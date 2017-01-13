14776364bb1b3d9833475d02bb665e174b46f187
Tobias Doerffel <tobias.doerffel@gmail.com>
2017-01-13 20:49:07 +0100

    Core: AccessControlRule: add ActionAskForPermission

--- core/include/AccessControlRule.h
+++ core/include/AccessControlRule.h
@@ -1,7 +1,7 @@
 /*
  * AccessControlRule.h - declaration of class AccessControlRule
  *
- * Copyright (c) 2016 Tobias Doerffel <tobydox/at/users/dot/sf/dot/net>
+ * Copyright (c) 2016-2017 Tobias Doerffel <tobydox/at/users/dot/sf/dot/net>
  *
  * This file is part of iTALC - http://italc.sourceforge.net
  *
@@ -37,6 +37,7 @@ public:
 		ActionNone,
 		ActionAllow,
 		ActionDeny,
+		ActionAskForPermission,
 		ActionCount
 	} Action;
 
