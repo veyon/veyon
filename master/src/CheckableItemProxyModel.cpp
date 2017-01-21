16a7d319b808909e51f71d34c6fc3e25ac84a4aa
Tobias Doerffel <tobias.doerffel@gmail.com>
2017-01-21 22:33:07 +0100

    Master: add initial implementation of CheckableItemProxyModel

---
+++ master/src/CheckableItemProxyModel.cpp
@@ -0,0 +1,116 @@
+/*
+ * CheckableItemProxyModel.cpp - proxy model for overlaying checked property
+ *
+ * Copyright (c) 2017 Tobias Doerffel <tobydox/at/users.sourceforge.net>
+ *
+ * This file is part of iTALC - http://italc.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#include "CheckableItemProxyModel.h"
+
+CheckableItemProxyModel::CheckableItemProxyModel(QObject *parent) :
+	QIdentityProxyModel(parent),
+	m_callDepth( 0 )
+{
+}
+
+
+
+Qt::ItemFlags CheckableItemProxyModel::flags(const QModelIndex &index) const
+{
+	if( index.isValid() == false )
+	{
+		return QIdentityProxyModel::flags( index );
+	}
+
+	return QIdentityProxyModel::flags( index ) | Qt::ItemIsUserCheckable;
+}
+
+
+
+QVariant CheckableItemProxyModel::data(const QModelIndex &index, int role) const
+{
+	if( !index.isValid() )
+	{
+		return QVariant();
+	}
+
+	if( role == Qt::CheckStateRole )
+	{
+		return m_checkStates.value( index );
+	}
+
+	return QIdentityProxyModel::data(index, role);
+}
+
+
+
+bool CheckableItemProxyModel::setData(const QModelIndex &index, const QVariant &value, int role)
+{
+	if( role != Qt::CheckStateRole )
+	{
+		return QIdentityProxyModel::setData( index, value, role );
+	}
+
+	if( m_callDepth > 1 )
+	{
+		return false;
+	}
+
+	++m_callDepth;
+
+	Qt::CheckState checkState = value.value<Qt::CheckState>();
+
+	m_checkStates[index] = checkState;
+
+	int childrenCount = rowCount( index );
+
+	if( childrenCount > 0 )
+	{
+		for( int i = 0; i < childrenCount; ++i )
+		{
+			setData( this->index( i, 0, index ), value, role );
+		}
+
+		emit dataChanged( this->index( 0, 0, index ), this->index( childrenCount, 0, index ), QVector<int>( role ) );
+	}
+	else if( index.parent().isValid() )
+	{
+		childrenCount = rowCount( index.parent() );
+
+		Qt::CheckState parentCheckState = checkState;
+
+		for( int i = 0; i < childrenCount; ++i )
+		{
+			if( data( this->index( i, 0, index.parent() ), role ).value<Qt::CheckState>() != parentCheckState )
+			{
+				parentCheckState = Qt::PartiallyChecked;
+				break;
+			}
+		}
+
+		setData( index.parent(), parentCheckState, role );
+
+		emit dataChanged( index.parent(), index.parent(), QVector<int>( role ) );
+	}
+
+	--m_callDepth;
+
+	return true;
+}
---
+++ master/src/CheckableItemProxyModel.h
@@ -0,0 +1,48 @@
+/*
+ * CheckableItemProxyModel.h - proxy model for overlaying checked property
+ *
+ * Copyright (c) 2017 Tobias Doerffel <tobydox/at/users.sourceforge.net>
+ *
+ * This file is part of iTALC - http://italc.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#ifndef CHECKABLE_ITEM_PROXY_MODEL_H
+#define CHECKABLE_ITEM_PROXY_MODEL_H
+
+#include <QIdentityProxyModel>
+
+class CheckableItemProxyModel : public QIdentityProxyModel
+{
+	Q_OBJECT
+public:
+	CheckableItemProxyModel(QObject *parent = 0);
+
+	Qt::ItemFlags flags(const QModelIndex &index) const override;
+
+	QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+
+	bool setData(const QModelIndex &index, const QVariant &value, int role) override;
+
+private:
+	QMap<QModelIndex, Qt::CheckState> m_checkStates;
+	int m_callDepth;
+
+};
+
+#endif // CHECKABLE_ITEM_PROXY_MODEL_H
