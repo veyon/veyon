44edcff4ba0d920ed66413082e5bbee5d0b0fe5a
Tobias Doerffel <tobias.doerffel@gmail.com>
2017-02-06 09:53:18 +0100

    Core: NetworkObjectDirectory: implement constructor outside of header

--- core/src/NetworkObjectDirectory.cpp
+++ core/src/NetworkObjectDirectory.cpp
@@ -1,5 +1,5 @@
 /*
- * NetworkObjectDirectory.h - base class for network object directory implementations
+ * NetworkObjectDirectory.cpp - base class for network object directory implementations
  *
  * Copyright (c) 2017 Tobias Doerffel <tobydox/at/users.sourceforge.net>
  *
@@ -22,35 +22,10 @@
  *
  */
 
-#ifndef NETWORK_OBJECT_DIRECTORY_H
-#define NETWORK_OBJECT_DIRECTORY_H
+#include "NetworkObjectDirectory.h"
 
-#include <QObject>
-
-#include "NetworkObject.h"
-
-class NetworkObjectDirectory : public QObject
+NetworkObjectDirectory::NetworkObjectDirectory( QObject* parent ) :
+	QObject( parent )
 {
-	Q_OBJECT
-public:
-	typedef enum Backends
-	{
-		ConfigurationBackend,
-		LdapBackend,
-		TestBackend,
-		BackendCount
-	} Backend;
+}
 
-	NetworkObjectDirectory( QObject* parent );
-
-	virtual QList<NetworkObject> objects( const NetworkObject& parent ) = 0;
-
-signals:
-	void objectsAboutToBeInserted( const NetworkObject& parent, int index, int count );
-	void objectsInserted();
-	void objectsAboutToBeRemoved( const NetworkObject& parent, int index, int count );
-	void objectsRemoved();
-
-};
-
-#endif // NETWORK_OBJECT_DIRECTORY_H
