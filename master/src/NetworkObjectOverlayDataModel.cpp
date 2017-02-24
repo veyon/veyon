2035cfaff5c6db794bf03c66d203fcdb84d517c3
Tobias Doerffel <tobias.doerffel@gmail.com>
2017-02-24 18:35:07 +0100

    Master: NetworkObjectOverlayDataModel: add model for adding overlay data to a NetworkObjectModel

---
+++ master/src/NetworkObjectOverlayDataModel.cpp
@@ -0,0 +1,94 @@
+/*
+ * NetworkObjectOverlayDataModel.cpp - overlay model for NetworkObjectModel to provide extra data
+ *
+ * Copyright (c) 2017 Tobias Doerffel <tobydox/at/users.sourceforge.net>
+ *
+ * This file is part of iTALC - http://italc.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#include "NetworkObjectOverlayDataModel.h"
+#include "NetworkObjectModel.h"
+
+
+NetworkObjectOverlayDataModel::NetworkObjectOverlayDataModel( int overlayDataColumn,
+															  int overlayDataRole,
+															  const QVariant& overlayDataColumnHeaderData,
+															  QObject *parent ) :
+	QIdentityProxyModel( parent ),
+	m_overlayDataColumn( overlayDataColumn ),
+	m_overlayDataRole( overlayDataRole ),
+	m_overlayDataColumnHeaderData( overlayDataColumnHeaderData )
+{
+}
+
+
+
+int NetworkObjectOverlayDataModel::columnCount( const QModelIndex& parent ) const
+{
+	return qMax<int>( QIdentityProxyModel::columnCount( parent ), m_overlayDataColumn + 1 );
+}
+
+
+
+QVariant NetworkObjectOverlayDataModel::headerData( int section, Qt::Orientation orientation, int role ) const
+{
+	if( section == m_overlayDataColumn && role == Qt::DisplayRole && orientation == Qt::Horizontal )
+	{
+		return m_overlayDataColumnHeaderData;
+	}
+
+	return QIdentityProxyModel::headerData( section, orientation, role );
+}
+
+
+
+QVariant NetworkObjectOverlayDataModel::data( const QModelIndex& index, int role ) const
+{
+	if( index.column() != m_overlayDataColumn || role != m_overlayDataRole )
+	{
+		return QIdentityProxyModel::data( index, role );
+	}
+
+	NetworkObject::Uid networkObjectUid = data( index, NetworkObjectModel::NetworkObjectUidRole ).value<NetworkObject::Uid>();
+
+	if( networkObjectUid.isNull() == false && m_overlayData.contains( networkObjectUid ) )
+	{
+		return m_overlayData[networkObjectUid];
+	}
+
+	return QVariant();
+}
+
+
+
+bool NetworkObjectOverlayDataModel::setData( const QModelIndex& index, const QVariant& value, int role )
+{
+	if( index.column() != m_overlayDataColumn || role != m_overlayDataRole )
+	{
+		return QIdentityProxyModel::setData( index, value, role );
+	}
+
+	NetworkObject::Uid networkObjectUid = data( index, NetworkObjectModel::NetworkObjectUidRole ).value<NetworkObject::Uid>();
+
+	m_overlayData[networkObjectUid] = value;
+
+	emit dataChanged( index, index, QVector<int>( { m_overlayDataRole } ) );
+
+	return true;
+}
---
+++ master/src/NetworkObjectOverlayDataModel.h
@@ -0,0 +1,58 @@
+/*
+ * NetworkObjectOverlayDataModel.h - overlay model for NetworkObjectModel to provide extra data
+ *
+ * Copyright (c) 2017 Tobias Doerffel <tobydox/at/users.sourceforge.net>
+ *
+ * This file is part of iTALC - http://italc.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#ifndef NETWORK_OBJECT_OVERLAY_DATA_MODEL_H
+#define NETWORK_OBJECT_OVERLAY_DATA_MODEL_H
+
+#include <QIdentityProxyModel>
+
+#include "NetworkObject.h"
+
+class NetworkObjectOverlayDataModel : public QIdentityProxyModel
+{
+	Q_OBJECT
+public:
+	NetworkObjectOverlayDataModel( int overlayDataColumn,
+								   int overlayDataRole,
+								   const QVariant& overlayDataColumnHeaderData,
+								   QObject *parent = 0 );
+
+	int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+
+	QVariant headerData( int section, Qt::Orientation orientation, int role = Qt::DisplayRole ) const override;
+
+	QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+
+	bool setData(const QModelIndex &index, const QVariant &value, int role) override;
+
+
+private:
+	int m_overlayDataColumn;
+	int m_overlayDataRole;
+	QVariant m_overlayDataColumnHeaderData;
+	QHash<NetworkObject::Uid, QVariant> m_overlayData;
+
+};
+
+#endif // NETWORK_OBJECT_OVERLAY_DATA_MODEL_H
