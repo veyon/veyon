1dfb6121ec1195f5b86f24b981109362bab4c2fc
Tobias Doerffel <tobias.doerffel@gmail.com>
2017-02-24 19:06:42 +0100

    Core: AccessControlRule: add ConditionAccessFromAlreadyConnectedUser

--- core/include/AccessControlRule.h
+++ core/include/AccessControlRule.h
@@ -80,6 +80,7 @@ public:
 		ConditionLocatedInSameComputerLab,
 		ConditionAccessFromLocalHost,
 		ConditionAccessFromLocalUser,
+		ConditionAccessFromAlreadyConnectedUser,
 		ConditionCount
 	} ;
 
