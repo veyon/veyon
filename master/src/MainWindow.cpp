2eeb8ea794b8aeeb9641dff9ffe641c60d40fc24
Tobias Doerffel <tobias.doerffel@gmail.com>
2017-02-27 18:29:56 +0100

    Revert "Master: use unmodified QToolBar instead of custom MainToolBar"
    This reverts commit d16ee3c3c32352aea918901b10b5def26625f8c8.

--- master/src/MainWindow.cpp
+++ master/src/MainWindow.cpp
@@ -88,7 +88,9 @@ MainWindow::MainWindow( MasterCore &masterCore ) :
 	// create the action-toolbar
 	ui->toolBar->layout()->setSpacing( 4 );
 	ui->toolBar->setObjectName( "MainToolBar" );
-	ui->toolBar->toggleViewAction()->setEnabled( false );
+	ui->toolBar->toggleViewAction()->setEnabled( FALSE );
+
+	addToolBar( Qt::TopToolBarArea, ui->toolBar );
 
 	ToolButton * scr = new ToolButton(
 			QPixmap( ":/resources/applications-education.png" ),
