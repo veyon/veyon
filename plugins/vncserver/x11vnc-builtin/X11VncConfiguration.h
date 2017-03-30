41f8c3d12a10c551001d2e7d8fdcc49fe8fa4d21
Tobias Doerffel <tobias.doerffel@gmail.com>
2017-03-30 15:46:24 +0200

    Plugins: VncServer: x11vnc: add X11VncConfiguration class

---
+++ plugins/vncserver/x11vnc-builtin/X11VncConfiguration.h
@@ -0,0 +1,48 @@
+/*
+ * X11VncConfiguration.h - x11vnc-specific configuration values
+ *
+ * Copyright (c) 2017 Tobias Doerffel <tobydox/at/users/dot/sf/dot/net>
+ *
+ * This file is part of iTALC - http://italc.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#ifndef X11VNC_CONFIGURATION_H
+#define X11VNC_CONFIGURATION_H
+
+#include "Configuration/Proxy.h"
+
+#define FOREACH_X11VNC_CONFIG_PROPERTY(OP) \
+	OP( X11VncConfiguration, m_configuration, BOOL, isXDamageDisabled, setXDamageDisabled, "XDamageDisabled", "X11Vnc" );	\
+	OP( X11VncConfiguration, m_configuration, STRING, extraArguments, setExtraArguments, "ExtraArguments", "X11Vnc" );
+
+class X11VncConfiguration : public Configuration::Proxy
+{
+	Q_OBJECT
+public:
+	X11VncConfiguration();
+
+	FOREACH_X11VNC_CONFIG_PROPERTY(DECLARE_CONFIG_PROPERTY)
+
+public slots:
+	void setXDamageDisabled( bool );
+	void setExtraArguments( const QString& );
+
+} ;
+
+#endif
