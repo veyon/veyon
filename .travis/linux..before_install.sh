f0050714b8f398a538715500daffe53a7a667dee
Tobias Doerffel <tobias.doerffel@gmail.com>
2017-05-05 15:29:06 +0200

    Travis: build with Ubuntu 16.04 to ensure Qt 5.5 compatibility

--- .travis/linux..before_install.sh
+++ .travis/linux..before_install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo add-apt-repository -y "deb http://archive.ubuntu.com/ubuntu yakkety main universe multiverse"
+sudo add-apt-repository -y "deb http://archive.ubuntu.com/ubuntu xenial main universe multiverse"
 sudo apt-get update -qq
 sudo apt-get install -y --force-yes dpkg
 
