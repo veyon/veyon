eff487a667c2fbecc0040cb964bb4e1a4852f32e
Tobias Doerffel <tobias.doerffel@gmail.com>
2017-05-11 14:47:22 +0200

    Core: VeyonCore: add backport of QOverload for Qt < 5.7

--- core/include/VeyonCore.h
+++ core/include/VeyonCore.h
@@ -58,6 +58,47 @@ static inline bool intersects( const QSet<A>& a, const QSet<B>& b )
 template <typename T> struct QAddConst { typedef const T Type; };
 template <typename T> constexpr typename QAddConst<T>::Type &qAsConst(T &t) { return t; }
 template <typename T> void qAsConst(const T &&) = delete;
+
+template <typename... Args>
+struct QNonConstOverload
+{
+	template <typename R, typename T>
+	Q_DECL_CONSTEXPR auto operator()(R (T::*ptr)(Args...)) const Q_DECL_NOTHROW -> decltype(ptr)
+	{ return ptr; }
+
+	template <typename R, typename T>
+	static Q_DECL_CONSTEXPR auto of(R (T::*ptr)(Args...)) Q_DECL_NOTHROW -> decltype(ptr)
+	{ return ptr; }
+};
+
+template <typename... Args>
+struct QConstOverload
+{
+	template <typename R, typename T>
+	Q_DECL_CONSTEXPR auto operator()(R (T::*ptr)(Args...) const) const Q_DECL_NOTHROW -> decltype(ptr)
+	{ return ptr; }
+
+	template <typename R, typename T>
+	static Q_DECL_CONSTEXPR auto of(R (T::*ptr)(Args...) const) Q_DECL_NOTHROW -> decltype(ptr)
+	{ return ptr; }
+};
+
+template <typename... Args>
+struct QOverload : QConstOverload<Args...>, QNonConstOverload<Args...>
+{
+	using QConstOverload<Args...>::of;
+	using QConstOverload<Args...>::operator();
+	using QNonConstOverload<Args...>::of;
+	using QNonConstOverload<Args...>::operator();
+
+	template <typename R>
+	Q_DECL_CONSTEXPR auto operator()(R (*ptr)(Args...)) const Q_DECL_NOTHROW -> decltype(ptr)
+	{ return ptr; }
+
+	template <typename R>
+	static Q_DECL_CONSTEXPR auto of(R (*ptr)(Args...)) Q_DECL_NOTHROW -> decltype(ptr)
+	{ return ptr; }
+};
 #endif
 
 class QCoreApplication;
