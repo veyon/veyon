0876eed3f2fb62be6eb1abcd8b641d6597293bcc
Tobias Doerffel <tobias.doerffel@gmail.com>
2017-05-18 08:17:01 +0200

    Core: add PlatformPluginManager

---
+++ core/include/PlatformPluginManager.h
@@ -0,0 +1,46 @@
+/*
+ * PlatformPluginManager.h - header file for PlatformPluginManager
+ *
+ * Copyright (c) 2017 Tobias Doerffel <tobydox/at/users.sourceforge.net>
+ *
+ * This file is part of Veyon - http://veyon.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#ifndef PLATFORM_PLUGIN_MANAGER_H
+#define PLATFORM_PLUGIN_MANAGER_H
+
+#include "PlatformPluginInterface.h"
+
+class VEYON_CORE_EXPORT PlatformPluginManager : public QObject
+{
+	Q_OBJECT
+public:
+	PlatformPluginManager( PluginManager& pluginManager );
+
+	PlatformPluginInterface* platformPlugin()
+	{
+		return m_platformPlugin;
+	}
+
+private:
+	PlatformPluginInterface* m_platformPlugin;
+
+};
+
+#endif // PLATFORM_PLUGIN_MANAGER_H
