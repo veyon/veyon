a6d8a0aba5142d689f1193ca1c7b96754fabd8d7
Tobias Doerffel <tobias.doerffel@gmail.com>
2017-05-24 08:06:58 +0200

    Master: add RecursiveFilterProxyModel
    RecursiveFilterProxyModel is an extended QSortFilterProxyModel which
    filters items recursively, i.e. parent items are not filtered if a
    child item matches.

---
+++ master/src/RecursiveFilterProxyModel.cpp
@@ -0,0 +1,52 @@
+/*
+ * RecursiveFilterProxyModel.cpp - proxy model for recursive filtering 
+ *
+ * Copyright (c) 2017 Tobias Doerffel <tobydox/at/users.sourceforge.net>
+ *
+ * This file is part of Veyon - http://veyon.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#include "RecursiveFilterProxyModel.h"
+
+RecursiveFilterProxyModel::RecursiveFilterProxyModel( QObject* parent ) :
+	QSortFilterProxyModel( parent )
+{
+}
+
+
+
+bool RecursiveFilterProxyModel::filterAcceptsRow( int sourceRow, const QModelIndex& sourceParent ) const
+{
+	if( sourceParent.isValid() )
+	{
+		return QSortFilterProxyModel::filterAcceptsRow( sourceRow, sourceParent );
+	}
+
+	const auto rowIndex = sourceModel()->index( sourceRow, 0, sourceParent );
+
+	for( int i = 0; i < sourceModel()->rowCount( rowIndex ); ++i )
+	{
+		if( filterAcceptsRow( i, rowIndex ) )
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
---
+++ master/src/RecursiveFilterProxyModel.h
@@ -0,0 +1,40 @@
+/*
+ * RecursiveFilterProxyModel.h - proxy model for recursive filtering
+ *
+ * Copyright (c) 2017 Tobias Doerffel <tobydox/at/users.sourceforge.net>
+ *
+ * This file is part of Veyon - http://veyon.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#ifndef RECURSIVE_FILTER_PROXY_MODEL_H
+#define RECURSIVE_FILTER_PROXY_MODEL_H
+
+#include <QSortFilterProxyModel>
+
+class RecursiveFilterProxyModel : public QSortFilterProxyModel
+{
+	Q_OBJECT
+public:
+	RecursiveFilterProxyModel( QObject* parent = nullptr );
+
+	bool filterAcceptsRow( int sourceRow, const QModelIndex& sourceParent ) const override;
+
+};
+
+#endif // RECURSIVE_FILTER_PROXY_MODEL_H
