ac9780f54c80a594025790acf3c0babcbb0f8165
Tobias Doerffel <tobias.doerffel@gmail.com>
2017-07-26 08:29:30 +0200

    Core: FeatureControl: add initial implementation

---
+++ core/include/FeatureControl.h
@@ -0,0 +1,116 @@
+/*
+ * FeatureControl.h - declaration of FeatureControl class
+ *
+ * Copyright (c) 2017 Tobias Doerffel <tobydox/at/users/dot/sf/dot/net>
+ *
+ * This file is part of Veyon - http://veyon.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#ifndef FEATURE_CONTROL_H
+#define FEATURE_CONTROL_H
+
+#include <QReadWriteLock>
+
+#include "FeaturePluginInterface.h"
+
+class QThread;
+class QTimer;
+
+class VEYON_CORE_EXPORT FeatureControl : public QObject, public FeaturePluginInterface, public PluginInterface
+{
+	Q_OBJECT
+	Q_INTERFACES(FeaturePluginInterface PluginInterface)
+public:
+	FeatureControl( QObject* parent = nullptr );
+	~FeatureControl() override;
+
+	bool queryActiveFeatures( const ComputerControlInterfaceList& computerControlInterfaces );
+
+	Plugin::Uid uid() const override
+	{
+		return "f5ec79e0-186c-4af0-89a2-7e5687cc32b2";
+	}
+
+	QString version() const override
+	{
+		return QStringLiteral( "1.0" );
+	}
+
+	QString name() const override
+	{
+		return QStringLiteral( "FeatureControl" );
+	}
+
+	QString description() const override
+	{
+		return tr( "Feature control" );
+	}
+
+	QString vendor() const override
+	{
+		return QStringLiteral( "Veyon Community" );
+	}
+
+	QString copyright() const override
+	{
+		return QStringLiteral( "Tobias Doerffel" );
+	}
+
+	const FeatureList& featureList() const override
+	{
+		return m_features;
+	}
+
+	bool startMasterFeature( const Feature& feature,
+							 const ComputerControlInterfaceList& computerControlInterfaces,
+							 ComputerControlInterface& localComputerControlInterface,
+							 QWidget* parent ) override;
+
+	bool stopMasterFeature( const Feature& feature,
+							const ComputerControlInterfaceList& computerControlInterfaces,
+							ComputerControlInterface& localComputerControlInterface,
+							QWidget* parent ) override;
+
+	bool handleMasterFeatureMessage( const FeatureMessage& message,
+									 ComputerControlInterface& computerControlInterface ) override;
+
+	bool handleServiceFeatureMessage( const FeatureMessage& message,
+									  FeatureWorkerManager& featureWorkerManager ) override;
+
+	bool handleWorkerFeatureMessage( const FeatureMessage& message ) override;
+
+private:
+	enum Commands
+	{
+		QueryActiveFeatures,
+	};
+
+	enum Arguments
+	{
+		ActiveFeatureList,
+	};
+
+	Feature m_featureControlFeature;
+	FeatureList m_features;
+
+	FeatureUidList m_activeFeatures;
+
+};
+
+#endif // FEATURE_CONTROL_H
---
+++ core/src/FeatureControl.cpp
@@ -0,0 +1,128 @@
+/*
+ * FeatureControl.cpp - implementation of FeatureControl class
+ *
+ * Copyright (c) 2017 Tobias Doerffel <tobydox/at/users/dot/sf/dot/net>
+ *
+ * This file is part of Veyon - http://veyon.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#include "FeatureControl.h"
+#include "FeatureWorkerManager.h"
+#include "VeyonCore.h"
+#include "VeyonRfbExt.h"
+
+
+FeatureControl::FeatureControl( QObject* parent ) :
+	QObject( parent ),
+	m_featureControlFeature( Feature( Feature::Service | Feature::Worker | Feature::Builtin,
+									  Feature::Uid( "a0a96fba-425d-414a-aaf4-352b76d7c4f3" ),
+									  tr( "Feature control" ), QString(), QString() ) ),
+	m_features( { m_featureControlFeature } )
+{
+}
+
+
+
+FeatureControl::~FeatureControl()
+{
+}
+
+
+
+bool FeatureControl::queryActiveFeatures( const ComputerControlInterfaceList& computerControlInterfaces )
+{
+	return sendFeatureMessage( FeatureMessage( m_featureControlFeature.uid(), QueryActiveFeatures ),
+							   computerControlInterfaces );
+}
+
+
+
+bool FeatureControl::startMasterFeature( const Feature& feature,
+										 const ComputerControlInterfaceList& computerControlInterfaces,
+										 ComputerControlInterface& localComputerControlInterface,
+										 QWidget* parent )
+{
+	Q_UNUSED(feature);
+	Q_UNUSED(computerControlInterfaces);
+	Q_UNUSED(localComputerControlInterface);
+	Q_UNUSED(parent);
+
+	return false;
+}
+
+
+
+bool FeatureControl::stopMasterFeature( const Feature& feature,
+										const ComputerControlInterfaceList& computerControlInterfaces,
+										ComputerControlInterface& localComputerControlInterface,
+										QWidget* parent )
+{
+	Q_UNUSED(feature);
+	Q_UNUSED(localComputerControlInterface);
+	Q_UNUSED(computerControlInterfaces);
+	Q_UNUSED(parent);
+
+	return false;
+}
+
+
+
+bool FeatureControl::handleMasterFeatureMessage( const FeatureMessage& message,
+												 ComputerControlInterface& computerControlInterface )
+{
+	if( message.featureUid() == m_featureControlFeature.uid() )
+	{
+		computerControlInterface.setActiveFeatures( message.argument( ActiveFeatureList ).toStringList() );
+
+		return true;
+	}
+
+	return false;
+}
+
+
+
+bool FeatureControl::handleServiceFeatureMessage( const FeatureMessage& message,
+												  FeatureWorkerManager& featureWorkerManager )
+{
+	Q_UNUSED(featureWorkerManager);
+
+	if( m_featureControlFeature.uid() == message.featureUid() )
+	{
+		FeatureMessage reply( message.featureUid(), message.command() );
+		reply.addArgument( ActiveFeatureList, featureWorkerManager.runningWorkers() );
+
+		char rfbMessageType = rfbVeyonFeatureMessage;
+		message.ioDevice()->write( &rfbMessageType, sizeof(rfbMessageType) );
+		reply.send( message.ioDevice() );
+
+		return true;
+	}
+
+	return false;
+}
+
+
+
+bool FeatureControl::handleWorkerFeatureMessage( const FeatureMessage& message )
+{
+	Q_UNUSED(message);
+
+	return false;
+}
