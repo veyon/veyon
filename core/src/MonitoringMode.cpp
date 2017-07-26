0e4b4b4c21bb9a5fe5e40ba4cfe6f4e985897c25
Tobias Doerffel <tobias.doerffel@gmail.com>
2017-07-26 08:54:52 +0200

    Core: MonitoringMode: set active display name

--- core/src/MonitoringMode.cpp
+++ core/src/MonitoringMode.cpp
@@ -28,7 +28,7 @@ MonitoringMode::MonitoringMode( QObject* parent ) :
 	QObject( parent ),
 	m_monitoringModeFeature( Feature::Mode | Feature::Master | Feature::Builtin,
 							 Feature::Uid( "edad8259-b4ef-4ca5-90e6-f238d0fda694" ),
-							 tr( "Monitoring" ), QString(),
+							 tr( "Monitoring" ), tr( "Monitoring" ),
 							 tr( "This is the default mode and allows you to monitor all computers in one or more rooms." ),
 							 QStringLiteral( ":/resources/presentation-none.png" ) ),
 	m_features()
