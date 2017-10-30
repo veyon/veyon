0d768dadb974a8d445b2a284261f3143decdd1b9
Tobias Junghans <totimaju@gmail.com>
2017-10-30 17:07:27 +0100

    Core: VncView: show actually used host address in progress widget

--- core/src/VncView.cpp
+++ core/src/VncView.cpp
@@ -61,7 +61,7 @@ VncView::VncView( const QString &host, int port, QWidget *parent, Mode mode ) :
 		m_vncConn->setQuality( VeyonVncConnection::DefaultQuality );
 		m_vncConn->setVeyonAuthType( RfbVeyonAuth::HostWhiteList );
 		m_establishingConnectionWidget = new ProgressWidget(
-			tr( "Establishing connection to %1 ..." ).arg( host ),
+			tr( "Establishing connection to %1 ..." ).arg( m_vncConn->host() ),
 					QStringLiteral( ":/resources/watch%1.png" ), 16, this );
 		connect( m_vncConn, &VeyonVncConnection::stateChanged,
 				 this, &VncView::updateConnectionState );
