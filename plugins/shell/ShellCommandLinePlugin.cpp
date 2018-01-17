75c1ae1842ecea55527282eb24178397d4529e57
Tobias Junghans <totimaju@gmail.com>
2018-01-17 08:09:24 +0100

    Plugins: Shell: add initial implementation

---
+++ plugins/shell/ShellCommandLinePlugin.cpp
@@ -0,0 +1,109 @@
+/*
+ * ShellCommandLinePlugin.cpp - implementation of ShellCommandLinePlugin class
+ *
+ * Copyright (c) 2018 Tobias Junghans <tobydox@users.sf.net>
+ *
+ * This file is part of Veyon - http://veyon.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#include <QCoreApplication>
+#include <QFile>
+#include <QProcess>
+
+#include "ShellCommandLinePlugin.h"
+
+
+ShellCommandLinePlugin::ShellCommandLinePlugin( QObject* parent ) :
+	QObject( parent ),
+	m_commands( {
+{ "run", tr( "Run command file" ) },
+				} )
+{
+}
+
+
+
+ShellCommandLinePlugin::~ShellCommandLinePlugin()
+{
+}
+
+
+
+QStringList ShellCommandLinePlugin::commands() const
+{
+	return m_commands.keys();
+}
+
+
+
+QString ShellCommandLinePlugin::commandHelp( const QString& command ) const
+{
+	return m_commands.value( command );
+}
+
+
+
+CommandLinePluginInterface::RunResult ShellCommandLinePlugin::handle_main()
+{
+	QTextStream stream( stdin );
+
+	while( 1 )
+	{
+		printf("VEYON> ");
+
+		QString line;
+		if( stream.readLineInto( &line ) && line != QStringLiteral("exit") )
+		{
+			runCommand( line );
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	return NoResult;
+}
+
+
+
+CommandLinePluginInterface::RunResult ShellCommandLinePlugin::handle_run( const QStringList& arguments )
+{
+	QFile scriptFile( arguments.value( 0 ) );
+	if( scriptFile.exists() == false )
+	{
+		fprintf( stderr, "%s\n", qPrintable( tr( "File \"%1\" does not exist!" ).arg( scriptFile.fileName() ) ) );
+		return Failed;
+	}
+
+	while( scriptFile.canReadLine() )
+	{
+		runCommand( scriptFile.readLine() );
+	}
+
+	return Successful;
+}
+
+
+
+void ShellCommandLinePlugin::runCommand( const QString& command )
+{
+	// TODO: properly split arguments containing spaces
+	QProcess::execute( QCoreApplication::applicationFilePath(), command.split( ' ' ) );
+}
---
+++ plugins/shell/ShellCommandLinePlugin.h
@@ -0,0 +1,93 @@
+/*
+ * ShellCommandLinePlugin.h - declaration of ShellCommandLinePlugin class
+ *
+ * Copyright (c) 2018 Tobias Junghans <tobydox@users.sf.net>
+ *
+ * This file is part of Veyon - http://veyon.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#ifndef SHELL_COMMAND_LINE_PLUGIN_H
+#define SHELL_COMMAND_LINE_PLUGIN_H
+
+#include "CommandLinePluginInterface.h"
+
+class ShellCommandLinePlugin : public QObject, CommandLinePluginInterface, PluginInterface
+{
+	Q_OBJECT
+	Q_PLUGIN_METADATA(IID "org.veyon.Veyon.Plugins.ShellCommandLineInterface")
+	Q_INTERFACES(PluginInterface CommandLinePluginInterface)
+public:
+	ShellCommandLinePlugin( QObject* parent = nullptr );
+	~ShellCommandLinePlugin() override;
+
+	Plugin::Uid uid() const override
+	{
+		return "85f6c631-e75a-4c78-8cb2-a7f3f502015a";
+	}
+
+	QVersionNumber version() const override
+	{
+		return QVersionNumber( 1, 0 );
+	}
+
+	QString name() const override
+	{
+		return QStringLiteral( "Shell" );
+	}
+
+	QString description() const override
+	{
+		return tr( "Provide interactive shell and script execution for Veyon Control" );
+	}
+
+	QString vendor() const override
+	{
+		return QStringLiteral( "Veyon Community" );
+	}
+
+	QString copyright() const override
+	{
+		return QStringLiteral( "Tobias Junghans" );
+	}
+
+	QString commandLineModuleName() const override
+	{
+		return QStringLiteral( "shell" );
+	}
+
+	QString commandLineModuleHelp() const override
+	{
+		return tr( "Commands for shell functionalities" );
+	}
+
+	QStringList commands() const override;
+	QString commandHelp( const QString& command ) const override;
+
+public slots:
+	CommandLinePluginInterface::RunResult handle_main();
+	CommandLinePluginInterface::RunResult handle_run( const QStringList& arguments );
+
+private:
+	void runCommand( const QString& command );
+
+	QMap<QString, QString> m_commands;
+
+};
+
+#endif // SHELL_COMMAND_LINE_PLUGIN_H
