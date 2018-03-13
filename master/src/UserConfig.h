5544484a77d02a7523bdbd7e1d3a77224e296a50
Tobias Junghans <totimaju@gmail.com>
2018-03-13 08:32:02 +0100

    Master: UserConfig: drop BackgroundColor property

--- master/src/UserConfig.h
+++ master/src/UserConfig.h
@@ -43,7 +43,6 @@ public:
 	OP( UserConfig, MasterCore::userConfig, BOOL, noToolTips, setNoToolTips, "NoToolTips", "UI" );	\
 	OP( UserConfig, MasterCore::userConfig, STRING, windowState, setWindowState, "WindowState", "UI" );	\
 	OP( UserConfig, MasterCore::userConfig, STRING, windowGeometry, setWindowGeometry, "WindowGeometry", "UI" );	\
-	OP( UserConfig, MasterCore::userConfig, STRING, backgroundColor, setBackgroundColor, "BackgroundColor", "UI" );	\
 
 	FOREACH_PERSONAL_CONFIG_PROPERTY(DECLARE_CONFIG_PROPERTY)
 
@@ -56,7 +55,6 @@ public slots:
 	void setNoToolTips( bool );
 	void setWindowState( const QString& );
 	void setWindowGeometry( const QString& );
-	void setBackgroundColor( const QString& );
 
 } ;
 
