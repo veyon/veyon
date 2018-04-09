2b0522df23f275092d3a6f93fbf62553d07cd4d0
Tobias Junghans <totimaju@gmail.com>
2018-04-09 20:02:15 +0200

    Master: add FlexibleListView

---
+++ master/src/FlexibleListView.cpp
@@ -0,0 +1,181 @@
+/*
+ * FlexibleListView.cpp - list view with flexible icon positions
+ *
+ * Copyright (c) 2018 Tobias Junghans <tobydox@users.sf.net>
+ *
+ * This file is part of Veyon - http://veyon.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#include <QJsonArray>
+#include <QJsonObject>
+#include <QUuid>
+
+#include "FlexibleListView.h"
+
+
+FlexibleListView::FlexibleListView( QWidget* parent ) :
+	QListView( parent ),
+	m_uidRole( Qt::UserRole )
+{
+	connect( this, &QListView::indexesMoved, this, &FlexibleListView::updatePositions );
+}
+
+
+
+FlexibleListView::~FlexibleListView()
+{
+}
+
+
+
+void FlexibleListView::setUidRole( int role )
+{
+	m_uidRole = role;
+}
+
+
+
+void FlexibleListView::setFlexible( bool enabled )
+{
+	if( enabled )
+	{
+		setMovement( QListView::Free );
+	}
+	else
+	{
+		setMovement( QListView::Static );
+	}
+
+	doItemsLayout();
+}
+
+
+
+bool FlexibleListView::flexible() const
+{
+	return movement() == QListView::Free;
+}
+
+
+
+void FlexibleListView::alignToGrid()
+{
+	auto m = model();
+
+	const QSizeF gridSize( rectForIndex( m->index( 0, 0 ) ).size() );
+
+	for( int i = 0, count = m->rowCount(); i < count; ++i )
+	{
+		const auto index = m->index( i, 0 );
+		const auto uid = m->data( index, m_uidRole ).toUuid();
+
+		if( uid.isNull() == false && m_positions.contains( uid ) )
+		{
+			m_positions[uid] = QPoint( static_cast<int>( qRound( m_positions[uid].x() / gridSize.width() ) * gridSize.width() ),
+									   static_cast<int>( qRound( m_positions[uid].y() / gridSize.height() ) * gridSize.height() ) );
+			setPositionForIndex( m_positions[uid], index );
+		}
+	}
+}
+
+
+
+QJsonArray FlexibleListView::savePositions()
+{
+	QJsonArray data;
+
+	for( auto it = m_positions.constBegin(), end = m_positions.constEnd(); it != end; ++it )
+	{
+		QJsonObject object;
+		object[QStringLiteral("uid")] = it.key().toString();
+		object[QStringLiteral("x")] = it.value().x();
+		object[QStringLiteral("y")] = it.value().y();
+		data += object;
+	}
+
+	return data;
+}
+
+
+
+void FlexibleListView::loadPositions( const QJsonArray& data )
+{
+	m_positions.clear();
+
+	for( const auto& item : data )
+	{
+		const auto object = item.toObject();
+		const QUuid uid( object["uid"].toString() );
+		if( uid.isNull() == false )
+		{
+			m_positions[uid] = QPoint( object["x"].toInt(), object["y"].toInt() );
+		}
+	}
+}
+
+
+
+void FlexibleListView::doItemsLayout()
+{
+	QListView::doItemsLayout();
+
+	if( movement() == QListView::Free )
+	{
+		restorePositions();
+	}
+}
+
+
+
+void FlexibleListView::restorePositions()
+{
+	auto m = model();
+
+	for( int i = 0, count = m->rowCount(); i < count; ++i )
+	{
+		const auto index = m->index( i, 0 );
+		const auto uid = m->data( index, m_uidRole ).toUuid();
+
+		if( uid.isNull() == false && m_positions.contains( uid ) )
+		{
+			setPositionForIndex( m_positions[uid], index );
+		}
+	}
+}
+
+
+
+void FlexibleListView::updatePositions()
+{
+	if( movement() == QListView::Free )
+	{
+		auto m = model();
+
+		for( int i = 0, count = m->rowCount(); i < count; ++i )
+		{
+			const auto index = m->index( i, 0 );
+			const auto uid = m->data( index, m_uidRole ).toUuid();
+
+			if( uid.isNull() == false )
+			{
+				m_positions[uid] = rectForIndex( index ).topLeft();
+			}
+		}
+	}
+}
---
+++ master/src/FlexibleListView.h
@@ -0,0 +1,60 @@
+/*
+ * FlexibleListView.h - list view with flexible icon positions
+ *
+ * Copyright (c) 2018 Tobias Junghans <tobydox@users.sf.net>
+ *
+ * This file is part of Veyon - http://veyon.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#ifndef FLEXIBLE_LIST_VIEW_H
+#define FLEXIBLE_LIST_VIEW_H
+
+#include <QListView>
+
+class FlexibleListView : public QListView
+{
+	Q_OBJECT
+public:
+	FlexibleListView( QWidget *parent = nullptr );
+	~FlexibleListView() override;
+
+	void setUidRole( int role );
+
+	void setFlexible( bool enabled );
+	bool flexible() const;
+
+	void alignToGrid();
+
+	QJsonArray savePositions();
+	void loadPositions( const QJsonArray& data );
+
+public:
+	void doItemsLayout() override;
+
+private:
+	void restorePositions();
+	void updatePositions();
+
+private:
+	int m_uidRole;
+	QHash<QUuid, QPoint> m_positions;
+
+};
+
+#endif // FLEXIBLE_LIST_VIEW_H
