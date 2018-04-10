ef5b5b2af1c94f13a5a6c24063fd585d8d41ff2f
Tobias Junghans <totimaju@gmail.com>
2018-04-10 17:10:15 +0200

    AuthKeys: TableModel: add default arguments

--- plugins/authkeys/AuthKeysTableModel.h
+++ plugins/authkeys/AuthKeysTableModel.h
@@ -51,8 +51,8 @@ public:
 		return m_keys[row];
 	}
 
-	int columnCount( const QModelIndex& parent ) const override;
-	int rowCount( const QModelIndex& parent ) const override;
+	int columnCount( const QModelIndex& parent = QModelIndex() ) const override;
+	int rowCount( const QModelIndex& parent = QModelIndex() ) const override;
 	QVariant data( const QModelIndex& index, int role ) const override;
 	QVariant headerData( int section, Qt::Orientation orientation, int role ) const override;
 
