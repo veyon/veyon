8d4fb472d06b0985ecc1725cf797f529146ecfdc
Tobias Junghans <tobydox@veyon.io>
2018-05-31 16:59:00 +0200

    Core: VeyonVncConnection: cleanups

--- core/src/VeyonVncConnection.cpp
+++ core/src/VeyonVncConnection.cpp
@@ -250,7 +250,7 @@ void VeyonVncConnection::hookCutText( rfbClient *cl, const char *text,
 
 
 
-void VeyonVncConnection::hookOutputHandler( const char *format, ... )
+void VeyonVncConnection::hookOutputHandler( const char* format, ... )
 {
 	va_list args;
 	va_start( args, format );
@@ -260,35 +260,7 @@ void VeyonVncConnection::hookOutputHandler( const char *format, ... )
 
 	va_end(args);
 
-	message = message.trimmed();
-	qDebug() << "VeyonVncConnection: VNC message:" << message;
-
-#if 0
-	if( ( message.contains( "Couldn't convert " ) ) ||
-		( message.contains( "Unable to connect to VNC server" ) ) )
-	{
-		outputErrorMessageString = "Server not found.";
-	}
-
-	if( ( message.contains( "VNC connection failed: Authentication failed, "
-							"too many tries")) ||
-		( message.contains( "VNC connection failed: Too many "
-						"authentication failures" ) ) )
-	{
-		outputErrorMessageString = tr( "VNC authentication failed "
-				"because of too many authentication tries." );
-	}
-
-	if (message.contains("VNC connection failed: Authentication failed"))
-		outputErrorMessageString = tr("VNC authentication failed.");
-
-	if (message.contains("VNC server closed connection"))
-		outputErrorMessageString = tr("VNC server closed connection.");
-
-	// internal messages, not displayed to user
-	if (message.contains("VNC server supports protocol version 3.889")) // see http://bugs.kde.org/162640
-		outputErrorMessageString = "INTERNAL:APPLE_VNC_COMPATIBILTY";
-#endif
+	qDebug() << "VeyonVncConnection: VNC message:" << message.trimmed();
 }
 
 
