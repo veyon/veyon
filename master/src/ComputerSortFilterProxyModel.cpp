403c4742684d810e0e8745e80a616cb135ab6c08
Tobias Junghans <tobydox@veyon.io>
2018-07-20 10:45:17 +0200

    Master: add ComputerSortFilterProxyModel
    This is an extended QSortFilterProxyModel which can also filter
    additional roles of ComputerControlInterface properties such as its
    state.

---
+++ master/src/ComputerSortFilterProxyModel.cpp
@@ -0,0 +1,65 @@
+/*
+ * ComputerSortFilterProxyModel.cpp - implementation of ComputerSortFilterProxyModel
+ *
+ * Copyright (c) 2018 Tobias Junghans <tobydox@veyon.io>
+ *
+ * This file is part of Veyon - http://veyon.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#include "ComputerSortFilterProxyModel.h"
+
+
+ComputerSortFilterProxyModel::ComputerSortFilterProxyModel( QObject* parent ) :
+	QSortFilterProxyModel( parent ),
+	m_stateRole( -1 ),
+	m_stateFilter( ComputerControlInterface::None )
+{
+}
+
+
+
+void ComputerSortFilterProxyModel::setStateRole( int role )
+{
+	beginResetModel();
+	m_stateRole = role;
+	endResetModel();
+}
+
+
+
+void ComputerSortFilterProxyModel::setStateFilter( ComputerControlInterface::State state )
+{
+	beginResetModel();
+	m_stateFilter = state;
+	endResetModel();
+}
+
+
+
+bool ComputerSortFilterProxyModel::filterAcceptsRow( int sourceRow, const QModelIndex& sourceParent ) const
+{
+	if( m_stateFilter != ComputerControlInterface::None &&
+			m_stateRole >= 0 &&
+			sourceModel()->data( sourceModel()->index( sourceRow, 0, sourceParent ), m_stateRole ).toInt() != m_stateFilter )
+	{
+		return false;
+	}
+
+	return QSortFilterProxyModel::filterAcceptsRow( sourceRow, sourceParent );
+}
---
+++ master/src/ComputerSortFilterProxyModel.h
@@ -0,0 +1,61 @@
+/*
+ * ComputerSortFilterProxyModel.h - header file for ComputerSortFilterProxyModel
+ *
+ * Copyright (c) 2018 Tobias Junghans <tobydox@veyon.io>
+ *
+ * This file is part of Veyon - http://veyon.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#ifndef COMPUTER_SORT_FILTER_PROXY_MODEL_H
+#define COMPUTER_SORT_FILTER_PROXY_MODEL_H
+
+#include <QSortFilterProxyModel>
+
+#include "ComputerControlInterface.h"
+
+class ComputerSortFilterProxyModel : public QSortFilterProxyModel
+{
+	Q_OBJECT
+public:
+	ComputerSortFilterProxyModel( QObject* parent );
+
+	int stateRole() const
+	{
+		return m_stateRole;
+	}
+
+	void setStateRole( int role );
+
+	ComputerControlInterface::State stateFilter() const
+	{
+		return m_stateFilter;
+	}
+
+	void setStateFilter( ComputerControlInterface::State state );
+
+protected:
+	bool filterAcceptsRow( int sourceRow, const QModelIndex& sourceParent ) const override;
+
+private:
+	int m_stateRole;
+	ComputerControlInterface::State m_stateFilter;
+
+};
+
+#endif // COMPUTER_SORT_FILTER_PROXY_MODEL_H
