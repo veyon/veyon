291cb7d79b216882f5fe7c50d3f382141351057a
Tobias Junghans <tobydox@veyon.io>
2018-10-01 11:00:17 +0200

    Core: Computer: fix default value for UID

--- core/include/Computer.h
+++ core/include/Computer.h
@@ -35,7 +35,7 @@
 class VEYON_CORE_EXPORT Computer
 {
 public:
-	Computer( NetworkObject::Uid networkObjectUid = 0,
+	Computer( NetworkObject::Uid networkObjectUid = NetworkObject::Uid(),
 			  const QString& name = QString(),
 			  const QString& hostAddress = QString(),
 			  const QString& macAddress = QString(),
