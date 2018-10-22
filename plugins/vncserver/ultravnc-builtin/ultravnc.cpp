753f27c50ac0dc925426d24950c748c77a6fbd9e
Tobias Junghans <tobydox@veyon.io>
2018-10-22 13:30:50 +0200

    VncServer: UltraVNC: drop unused variables
    VNC menu code is not used any longer.

--- plugins/vncserver/ultravnc-builtin/ultravnc.cpp
+++ plugins/vncserver/ultravnc-builtin/ultravnc.cpp
@@ -12,27 +12,8 @@
 // adzm 2010-09 - rfbUltraVNC or other auths may send this to restart authentication (perhaps over a now-secure channel)
 #define rfbVncAuthContinue 0xFFFFFFFF
 
-
-
 #include "vncclient.cpp"
 
-
-#ifdef IPV6V4
-const UINT MENU_ADD_CLIENT6_MSG_INIT = RegisterWindowMessage("WinVNC.AddClient6.Message.Init");
-const UINT MENU_ADD_CLIENT6_MSG = RegisterWindowMessage("WinVNC.AddClient6.Message");
-#endif
-
-const UINT MENU_ADD_CLIENT_MSG_INIT = RegisterWindowMessage("WinVNC.AddClient.Message.Init");
-const UINT MENU_ADD_CLIENT_MSG = RegisterWindowMessage("WinVNC.AddClient.Message");
-const UINT MENU_AUTO_RECONNECT_MSG = RegisterWindowMessage("WinVNC.AddAutoClient.Message");
-const UINT MENU_STOP_RECONNECT_MSG = RegisterWindowMessage("WinVNC.AddStopClient.Message");
-const UINT MENU_STOP_ALL_RECONNECT_MSG = RegisterWindowMessage("WinVNC.AddStopAllClient.Message");
-const UINT MENU_REPEATER_ID_MSG = RegisterWindowMessage("WinVNC.AddRepeaterID.Message");
-
-
-const char *MENU_CLASS_NAME = "WinVNC Tray Icon";
-
-
-HWND G_MENU_HWND = NULL;
+extern bool G_USE_PIXEL;
 
 bool G_USE_PIXEL = false;
