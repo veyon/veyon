0e748209e89424c28972fee32dc9f12a1868b813
Tobias Junghans <tobydox@veyon.io>
2018-11-16 15:08:45 +0100

    Core: Cotire: add more basic classes

--- core/include/Cotire.h
+++ core/include/Cotire.h
@@ -1,10 +1,16 @@
-#ifndef COTIRE_H
-#define COTIRE_H
+#pragma once
+
 #include <QApplication>
 #include <QDialog>
+#include <QDir>
+#include <QFile>
+#include <QFileInfo>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonValue>
 #include <QMainWindow>
 #include <QMessageBox>
+#include <QMutex>
 #include <QProcess>
 #include <QThread>
 #include <QTcpSocket>
-#endif
