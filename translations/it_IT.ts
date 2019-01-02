96390e0044e2262e14b3d65ceac7778bd238365e
albanobattistella <34811668+albanobattistella@users.noreply.github.com>
2019-01-02 18:01:18 +0100

    Update it_IT.ts

--- translations/it_IT.ts
+++ translations/it_IT.ts
@@ -1,4 +1,4 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="it_IT" version="4.1">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="it_IT" version="2.1">
 <context>
     <name>AboutDialog</name>
     <message>
