2401f04564b6cc1ac0a61751c45f5a7397f603e9
Tobias Junghans <tobydox@veyon.io>
2019-02-18 08:18:38 +0100

    Configuration: UiMapping: clean up old code

--- core/include/Configuration/UiMapping.h
+++ core/include/Configuration/UiMapping.h
@@ -234,6 +234,5 @@ inline void connectWidgetToProperty( Config* config, void (Config::*setter)( con
 #define CONNECT_WIDGET_TO_PROPERTY(className, config, type, get, set, key, parentKey, defaultValue, flags)	\
 	connectWidgetToProperty( &config, &className::set, ui->get );
 
-//inline void mapPropertyFlagsToWidget( QWidget* widget, bool
 #define MAP_PROPERTY_FLAGS_TO_WIDGET(className, config, type, get, set, key, parentKey, defaultValue, flags)	\
 	ui->get->setVisible( flags & Object::AdvancedSettingProperty );
