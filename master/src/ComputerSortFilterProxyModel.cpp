veyon/veyon

commit f83d63cb176831e21c06e1381acc86b7af4aaef1

ComputerSortFilterProxyModel: use QVariantHelper
Fixes broken filtering of powered on computers when using Qt 5.5.

master/src/ComputerSortFilterProxyModel.cpp
@@ -65,8 +65,8 @@ bool ComputerSortFilterProxyModel::filterAcceptsRow( int sourceRow, const QModel
 {
 	if( m_stateFilter != ComputerControlInterface::State::None &&
 		m_stateRole >= 0 &&
-		sourceModel()->data( sourceModel()->index( sourceRow, 0, sourceParent ),
-							 m_stateRole ).value<ComputerControlInterface::State>() != m_stateFilter )
+		QVariantHelper<ComputerControlInterface::State>::value(
+			sourceModel()->data( sourceModel()->index( sourceRow, 0, sourceParent ) ) ) != m_stateFilter )
 	{
 		return false;
 	}

commit d3b2f1eb56cd89d980feedb4945d37921c096d89

ComputerSortFilterProxyModel: fetch correct role
Commit f83d63cb176831e21c06e1381acc86b7af4aaef1 introduced a bug which
made the wrong data role being fetched (i.e. not the state but the
display role). This broke the powered on computers filter.

master/src/ComputerSortFilterProxyModel.cpp
@@ -66,7 +66,7 @@ bool ComputerSortFilterProxyModel::filterAcceptsRow( int sourceRow, const QModel
 	if( m_stateFilter != ComputerControlInterface::State::None &&
 		m_stateRole >= 0 &&
 		QVariantHelper<ComputerControlInterface::State>::value(
-			sourceModel()->data( sourceModel()->index( sourceRow, 0, sourceParent ) ) ) != m_stateFilter )
+			sourceModel()->data( sourceModel()->index( sourceRow, 0, sourceParent ), m_stateRole ) ) != m_stateFilter )
 	{
 		return false;
 	}