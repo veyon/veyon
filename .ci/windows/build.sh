4e1706aa6bcf0fe9ea6dd5af6621bdf5bbc6a622
Tobias Junghans <tobydox@veyon.io>
2019-05-21 08:51:53 +0200

    CI: update windows build script

--- .ci/windows/build.sh
+++ .ci/windows/build.sh
@@ -4,13 +4,13 @@ set -e
 
 CPUS=$(nproc)
 BASEDIR=$(pwd)
+BUILDDIR=/tmp/build-$1
 
-apt-get update && apt-get -y dist-upgrade
+$BASEDIR/.ci/common/strip-ultravnc-sources.sh
 
-.ci/common/strip-ultravnc-sources.sh
-
-mkdir /tmp/build
-cd /tmp/build
+rm -rf $BUILDDIR
+mkdir $BUILDDIR
+cd $BUILDDIR
 
 cmake $BASEDIR -DCMAKE_TOOLCHAIN_FILE=$BASEDIR/cmake/modules/Win${1}Toolchain.cmake -DCMAKE_MODULE_PATH=$BASEDIR/cmake/modules/
 
@@ -18,10 +18,10 @@ echo Building on $CPUS CPUs
 
 if [ -z "$2" ] ; then
 	make -j$CPUS
-
 	make win-nsi
-	mv -v veyon*setup.exe $BASEDIR
 else
 	make ${@:2} -j$CPUS
 fi
+
+mv -v veyon*setup.exe $BASEDIR
 
