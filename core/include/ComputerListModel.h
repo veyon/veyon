de0c2d191d5a67779b4fb2470e83fa812b239282
Tobias Junghans <tobydox@veyon.io>
2019-08-05 10:58:54 +0200

    ComputerListModel: add new base class
    This base class provides common enum classes for computer-related
    list models.
    
    (cherry picked from commit 6ffc156c542fca8ac7e1f834218953445279bfc7)

---
+++ core/include/ComputerListModel.h
@@ -0,0 +1,79 @@
+/*
+ * ComputerListModel.h - data model base class for computer objects
+ *
+ * Copyright (c) 2017-2020 Tobias Junghans <tobydox@veyon.io>
+ *
+ * This file is part of Veyon - https://veyon.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#pragma once
+
+#include <QAbstractListModel>
+
+#include "VeyonCore.h"
+
+class VEYON_CORE_EXPORT ComputerListModel : public QAbstractListModel
+{
+	Q_OBJECT
+public:
+	enum {
+		UidRole = Qt::UserRole,
+		StateRole,
+		ImageIdRole,
+		GroupsRole,
+		ScreenRole,
+		ControlInterfaceRole
+	};
+
+	enum class DisplayRoleContent {
+		UserAndComputerName,
+		UserName,
+		ComputerName,
+	};
+	Q_ENUM(DisplayRoleContent)
+
+	enum class SortOrder {
+		ComputerAndUserName,
+		UserName,
+		ComputerName,
+	};
+	Q_ENUM(SortOrder)
+
+	explicit ComputerListModel( QObject* parent = nullptr );
+
+	Qt::ItemFlags flags( const QModelIndex& index ) const override;
+
+	Qt::DropActions supportedDragActions() const override;
+	Qt::DropActions supportedDropActions() const override;
+
+	DisplayRoleContent displayRoleContent() const
+	{
+		return m_displayRoleContent;
+	}
+
+	SortOrder sortOrder() const
+	{
+		return m_sortOrder;
+	}
+
+private:
+	DisplayRoleContent m_displayRoleContent;
+	SortOrder m_sortOrder;
+
+};
---
+++ core/src/ComputerListModel.cpp
@@ -0,0 +1,62 @@
+/*
+ * ComputerListModel.cpp - data model base class for computer objects
+ *
+ * Copyright (c) 2017-2020 Tobias Junghans <tobydox@veyon.io>
+ *
+ * This file is part of Veyon - https://veyon.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#include "ComputerListModel.h"
+#include "VeyonConfiguration.h"
+
+
+ComputerListModel::ComputerListModel( QObject* parent ) :
+	QAbstractListModel( parent ),
+	m_displayRoleContent( VeyonCore::config().computerDisplayRoleContent() ),
+	m_sortOrder( VeyonCore::config().computerMonitoringSortOrder() )
+{
+}
+
+
+
+Qt::ItemFlags ComputerListModel::flags( const QModelIndex& index ) const
+{
+	auto defaultFlags = QAbstractListModel::flags( index );
+
+	if( index.isValid() )
+	{
+		 return Qt::ItemIsDragEnabled | defaultFlags;
+	}
+
+	return Qt::ItemIsDropEnabled | defaultFlags;
+}
+
+
+
+Qt::DropActions ComputerListModel::supportedDragActions() const
+{
+	return Qt::MoveAction;
+}
+
+
+
+Qt::DropActions ComputerListModel::supportedDropActions() const
+{
+	return Qt::MoveAction;
+}
