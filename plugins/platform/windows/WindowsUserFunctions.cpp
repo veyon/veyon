61287fcf1e0e62fc9ef1a51218022636abd9e294
Tobias Junghans <tobydox@veyon.io>
2019-09-06 10:49:48 +0200

    WindowsUserFunctions: implement logon via key input

--- plugins/platform/windows/WindowsUserFunctions.cpp
+++ plugins/platform/windows/WindowsUserFunctions.cpp
@@ -22,189 +22,30 @@
  *
  */
 
-#include <winsock2.h>
-#include <windows.h>
-#include <lm.h>
-#include <ntsecapi.h>
+#include <QBuffer>
 
+#include "DesktopInputController.h"
+#include "VariantStream.h"
 #include "VeyonConfiguration.h"
+#include "VeyonServerInterface.h"
 #include "WindowsCoreFunctions.h"
+#include "WindowsInputDeviceFunctions.h"
 #include "WindowsPlatformConfiguration.h"
+#include "WindowsServiceCore.h"
 #include "WindowsUserFunctions.h"
 #include "WtsSessionManager.h"
 
 #include "authSSP.h"
 
+#define XK_MISCELLANY
 
-static bool storePassword( const QString& password )
-{
-	LSA_OBJECT_ATTRIBUTES loa;
-	loa.Length = sizeof(loa);
-	loa.RootDirectory = nullptr;
-	loa.ObjectName = nullptr;
-	loa.Attributes = 0;
-	loa.SecurityDescriptor = nullptr;
-	loa.SecurityQualityOfService = nullptr;
-
-	LSA_HANDLE lh;
-
-	auto status = LsaOpenPolicy( nullptr, &loa, POLICY_CREATE_SECRET, &lh );
-
-	if( status )
-	{
-		vCritical() << "Error opening LSA policy:" << LsaNtStatusToWinError( status );
-		return false;
-	}
-
-	const auto nameWide = WindowsCoreFunctions::toWCharArray( QStringLiteral("DefaultPassword") );
-
-	LSA_UNICODE_STRING name;
-	name.Buffer = nameWide.data();
-	name.MaximumLength = name.Length = static_cast<USHORT>( wcslen(name.Buffer) - 2 );
-
-	if( password.isEmpty() )
-	{
-		status = LsaStorePrivateData( lh, &name, nullptr );
-		if( status )
-		{
-			vCritical() << "Error clearing stored password:" << LsaNtStatusToWinError( status );
-			return false;
-		}
-		return true;
-	}
-
-	const auto passwordWide = WindowsCoreFunctions::toWCharArray( password );
-
-	LSA_UNICODE_STRING data;
-	data.Buffer = passwordWide.data();
-	data.MaximumLength = data.Length = static_cast<USHORT>( wcslen(data.Buffer) * sizeof(*data.Buffer) );
-
-	status = LsaStorePrivateData( lh, &name, &data );
-
-	LsaClose(lh);
-
-	if( status )
-	{
-		vCritical() << "Error storing password:" << LsaNtStatusToWinError( status );
-		return false;
-	}
-
-	return true;
-}
-
-
-
-static bool setAutologon( const QString& username, const QString& password, const QString& domainName )
-{
-	HKEY h;
-
-	auto err = RegOpenKeyEx( HKEY_LOCAL_MACHINE, L"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon",
-							 0, KEY_WOW64_64KEY | KEY_SET_VALUE, &h );
-	if( err != ERROR_SUCCESS )
-	{
-		vCritical() << "Unable to open Winlogon subkey:" << err;
-		return false;
-	}
-
-	if( storePassword( password ) == false )
-	{
-		return false;
-	}
-
-	const auto usernameWide = WindowsCoreFunctions::toWCharArray( username );
-	const auto domainNameWide = WindowsCoreFunctions::toWCharArray( domainName );
-
-	err = RegSetValueEx( h, L"DefaultUserName", 0, REG_SZ, reinterpret_cast<CONST BYTE *>( usernameWide.data() ),
-						 static_cast<DWORD>( ( username.length() + 1 ) * 2 ) );
-	if( err != ERROR_SUCCESS )
-	{
-		vCritical() << "Unable to set default logon user name:" << err;
-		return false;
-	}
-
-	err = RegSetValueEx( h, L"DefaultDomainName", 0, REG_SZ, reinterpret_cast<CONST BYTE *>( domainNameWide.data() ),
-						 static_cast<DWORD>( ( domainName.length() + 1 ) * 2 ) );
-	if( err != ERROR_SUCCESS )
-	{
-		vCritical() << "Unable to set default domain name:" << err;
-		return false;
-	}
-
-	err = RegSetValueEx( h, L"AutoAdminLogon", 0, REG_SZ, reinterpret_cast<CONST BYTE *>( L"1" ), 2 );
-	if( err != ERROR_SUCCESS )
-	{
-		vCritical() << "Unable to set automatic logon flag" << err;
-		return false;
-	}
-
-	err = RegSetValueEx( h, L"ForceAutoLogon", 0, REG_SZ, reinterpret_cast<CONST BYTE *>( L"1" ), 2 );
-	if( err != ERROR_SUCCESS )
-	{
-		vCritical() << "Unable to set forced automatic logon flag:" << err;
-		return false;
-	}
-
-	return true;
-}
-
-
-
-static bool clearAutologon()
-{
-	HKEY h;
-
-	auto i = RegOpenKeyEx( HKEY_LOCAL_MACHINE, L"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon",
-						   0, KEY_WOW64_64KEY | KEY_SET_VALUE, &h );
-	if( i != ERROR_SUCCESS )
-	{
-		vCritical() << "Unable to open Winlogon subkey" << i;
-		return false;
-	}
-
-	i = RegSetValueEx(h, L"DefaultUserName", 0, REG_SZ, reinterpret_cast<const BYTE *>( L"" ), 1 );
-	if( i != ERROR_SUCCESS )
-	{
-		vCritical() << "Unable to set default logon user name" << i;
-		return false;
-	}
-
-	if( storePassword( {} ) == false )
-	{
-		return false;
-	}
-
-	i = RegDeleteValue( h, L"DefaultPassword" );
-	if( i != ERROR_SUCCESS && i != ERROR_FILE_NOT_FOUND )
-	{
-		vCritical() << "Unable to remove default logon password" << i;
-		return false;
-	}
-
-	i = RegDeleteValue( h, L"ForceAutoLogon" );
-	if( i != ERROR_SUCCESS && i != ERROR_FILE_NOT_FOUND )
-	{
-		vCritical() << "Unable to remove force logon flag" << i;
-		return false;
-	}
-
-	i = RegSetValueEx( h, L"AutoAdminLogon", 0, REG_SZ, reinterpret_cast<CONST BYTE *>( L"0" ), 2 );
-	if( i != ERROR_SUCCESS )
-	{
-		vCritical() << "Unable to clear automatic logon flag" << i;
-		return false;
-	}
-
-	return true;
-}
-
+#include "keysymdef.h"
 
 
 WindowsUserFunctions::WindowsUserFunctions()
 {
-	if( isAnyUserLoggedOn() )
-	{
-		clearAutologon();
-	}
+	connect( VeyonCore::instance(), &VeyonCore::applicationLoaded,
+			 this, &WindowsUserFunctions::checkPendingLogonTasks );
 }
 
 
@@ -332,29 +173,15 @@ bool WindowsUserFunctions::logon( const QString& username, const Password& passw
 		return false;
 	}
 
-	QString domain;
-	QString user;
-
-	const auto userNameParts = username.split( QLatin1Char('\\') );
-	if( userNameParts.count() == 2 )
+	if( writePersistentLogonCredentials( username, password ) )
 	{
-		domain = userNameParts[0];
-		user = userNameParts[1];
-	}
-	else
-	{
-		user = username;
+		logoff();
+		return true;
 	}
 
-	if( setAutologon( user, domain, password ) == false )
-	{
-		vCritical() << "Failed to logon user";
-		return false;
-	}
+	vCritical() << "failed";
 
-	VeyonCore::platform().coreFunctions().reboot();
-
-	return true;
+	return false;
 }
 
 
@@ -406,6 +233,127 @@ bool WindowsUserFunctions::authenticate( const QString& username, const Password
 	}
 
 	return result;
+}
+
+
+
+void WindowsUserFunctions::checkPendingLogonTasks()
+{
+	// running inside a Veyon Server component?
+	if( WindowsServiceCore::serviceDataTokenFromEnvironment().isEmpty() == false &&
+		isAnyUserLoggedOn() == false )
+	{
+		vDebug() << "Reading logon credentials";
+		QString username;
+		Password password;
+		if( readPersistentLogonCredentials( &username, &password ) )
+		{
+			clearPersistentLogonCredentials();
+
+			performFakeInputLogon( username, password );
+		}
+	}
+}
+
+
+
+bool WindowsUserFunctions::readPersistentLogonCredentials( QString* username, Password* password )
+{
+	if( username == nullptr || password == nullptr )
+	{
+		vCritical() << "Invalid input pointers";
+		return false;
+	}
+
+	auto logonData = ServiceDataManager::read( WindowsServiceCore::serviceDataTokenFromEnvironment() );
+	if( logonData.isEmpty() )
+	{
+		vCritical() << "Empty data";
+		return false;
+	}
+
+	QBuffer logonDataBuffer( &logonData );
+	if( logonDataBuffer.open( QBuffer::ReadOnly ) == false )
+	{
+		vCritical() << "Failed to open buffer";
+		return false;
+	}
+
+	VariantStream stream( &logonDataBuffer );
+	*username = stream.read().toString();
+	*password = VeyonCore::cryptoCore().decryptPassword( stream.read().toString() );
+
+	return username->isEmpty() == false && password->isEmpty() == false;
+}
+
+
+
+bool WindowsUserFunctions::writePersistentLogonCredentials( const QString& username, const Password& password )
+{
+	QBuffer logonDataBuffer;
+	if( logonDataBuffer.open( QBuffer::WriteOnly ) == false )
+	{
+		vCritical() << "Failed to open buffer";
+		return false;
+	}
+
+	VariantStream stream( &logonDataBuffer );
+	stream.write( username );
+	stream.write( VeyonCore::cryptoCore().encryptPassword( password ) );
+
+	if( ServiceDataManager::write( WindowsServiceCore::serviceDataTokenFromEnvironment(),
+								   logonDataBuffer.data() ) == false )
+	{
+		vCritical() << "Failed to write persistent service data";
+		return false;
+	}
+
+	return true;
+}
+
+
+
+bool WindowsUserFunctions::clearPersistentLogonCredentials()
+{
+	return ServiceDataManager::write( WindowsServiceCore::serviceDataTokenFromEnvironment(), {} );
+}
+
+
+
+bool WindowsUserFunctions::performFakeInputLogon( const QString& username, const Password& password )
+{
+	WindowsPlatformConfiguration config( &VeyonCore::config() );
+
+	DesktopInputController input( config.logonKeyPressInterval() );
+
+	const auto ctrlAltDel = []() {
+		auto sasEvent = OpenEvent( EVENT_MODIFY_STATE, false, L"Global\\VeyonServiceSasEvent" );
+		SetEvent( sasEvent );
+		CloseHandle( sasEvent );
+	};
+
+	const auto sendString = [&input]( const QString& string ) {
+		for( const auto& character : string )
+		{
+			input.pressAndReleaseKey( character );
+		}
+	};
+
+	ctrlAltDel();
+
+	QThread::msleep( static_cast<unsigned long>( config.logonInputStartDelay() ) );
+
+	input.pressAndReleaseKey( XK_Delete );
+
+	sendString( username );
+
+	input.pressAndReleaseKey( XK_Tab );
+
+	sendString( QString::fromUtf8( password.toByteArray() ) );
+
+	input.pressAndReleaseKey( XK_Return );
+
+	return true;
 }
 
 
