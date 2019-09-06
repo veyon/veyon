3c464682b55ae9f74f111d6ed07a603798f23ee4
Tobias Junghans <tobydox@veyon.io>
2019-09-06 10:48:35 +0200

    Platform: Windows: add ServiceDataManager

---
+++ plugins/platform/windows/ServiceDataManager.cpp
@@ -0,0 +1,197 @@
+/*
+ * ServiceDataManager.cpp - implementation of ServiceDataManager class
+ *
+ * Copyright (c) 2019 Tobias Junghans <tobydox@veyon.io>
+ *
+ * This file is part of Veyon - https://veyon.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#include <QElapsedTimer>
+#include <QLocalServer>
+#include <QLocalSocket>
+
+#include "ServiceDataManager.h"
+
+
+ServiceDataManager::ServiceDataManager( QObject* parent ) :
+	QThread( parent ),
+	m_token( CryptoCore::generateChallenge().toHex() )
+{
+	vDebug();
+
+	start();
+}
+
+
+
+ServiceDataManager::~ServiceDataManager()
+{
+	vDebug();
+
+	quit();
+	wait();
+}
+
+
+
+QByteArray ServiceDataManager::read( const Token& token )
+{
+	QLocalSocket socket;
+	socket.connectToServer( serverName() );
+	if( socket.waitForConnected() == false )
+	{
+		vCritical() << "connection timed out";
+		return {};
+	}
+
+	VariantArrayMessage outMessage( &socket );
+	outMessage.write( token.toByteArray() );
+	outMessage.write( static_cast<int>( Command::ReadData ) );
+
+	sendMessage( &socket, outMessage );
+
+	if( waitForMessage( &socket ) == false )
+	{
+		vCritical() << "no response";
+		return {};
+	}
+
+	VariantArrayMessage response( &socket );
+	response.receive();
+
+	return response.read().toByteArray();
+}
+
+
+
+bool ServiceDataManager::write( const Token& token, const Data& data )
+{
+	QLocalSocket socket;
+	socket.connectToServer( serverName() );
+
+	if( socket.waitForConnected() == false )
+	{
+		vCritical() << "connection timed out";
+		return false;
+	}
+
+	VariantArrayMessage outMessage( &socket );
+	outMessage.write( token.toByteArray() );
+	outMessage.write( static_cast<int>( Command::WriteData ) );
+	outMessage.write( data.toByteArray() );
+
+	sendMessage( &socket, outMessage );
+
+	return waitForMessage( &socket );
+}
+
+
+
+void ServiceDataManager::run()
+{
+	m_server = new QLocalServer;
+	m_server->setSocketOptions( QLocalServer::UserAccessOption );
+
+	if( m_server->listen( serverName() ) == false )
+	{
+		vCritical() << "can't listen" << m_server->errorString();
+		return;
+	}
+
+	connect( m_server, &QLocalServer::newConnection, m_server, [this]() { acceptConnection(); } );
+
+	QThread::run();
+}
+
+
+
+bool ServiceDataManager::waitForMessage( QLocalSocket* socket )
+{
+	// wait for acknowledge
+	QElapsedTimer messageTimeoutTimer;
+	messageTimeoutTimer.start();
+
+	VariantArrayMessage inMessage( socket );
+	while( messageTimeoutTimer.elapsed() < MessageReadTimeout &&
+		   inMessage.isReadyForReceive() == false )
+	{
+		socket->waitForReadyRead( SocketWaitTimeout );
+	}
+
+	return inMessage.isReadyForReceive();
+}
+
+
+
+void ServiceDataManager::sendMessage( QLocalSocket* socket, VariantArrayMessage& message )
+{
+	message.send();
+	socket->flush();
+	socket->waitForBytesWritten();
+}
+
+
+
+void ServiceDataManager::acceptConnection()
+{
+	auto socket = m_server->nextPendingConnection();
+
+	connect( socket, &QLocalSocket::readyRead,
+			 socket, [this, socket]() { handleConnection( socket ); } );
+}
+
+
+
+void ServiceDataManager::handleConnection( QLocalSocket* socket )
+{
+	VariantArrayMessage inMessage( socket );
+
+	if( inMessage.isReadyForReceive() && inMessage.receive() )
+	{
+		const auto token = inMessage.read().toByteArray();
+		if( token != m_token.toByteArray() )
+		{
+			vCritical() << "Invalid token";
+			socket->close();
+			return;
+		}
+
+		const auto command = static_cast<Command>( inMessage.read().toInt() );
+
+		if( command == Command::WriteData )
+		{
+			m_data = inMessage.read().toByteArray();
+
+			VariantArrayMessage acknowledge( socket );
+			acknowledge.send();
+		}
+		else if( command == Command::ReadData )
+		{
+			VariantArrayMessage response( socket );
+			response.write( m_data.toByteArray() );
+			response.send();
+		}
+		else
+		{
+			vCritical() << "unknown command";
+			socket->close();
+		}
+		socket->flush();
+	}
+}
---
+++ plugins/platform/windows/ServiceDataManager.h
@@ -0,0 +1,83 @@
+/*
+ * ServiceDataManager.h - header file for ServiceDataManager class
+ *
+ * Copyright (c) 2019 Tobias Junghans <tobydox@veyon.io>
+ *
+ * This file is part of Veyon - https://veyon.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#pragma once
+
+#include <QThread>
+
+#include "CryptoCore.h"
+#include "VariantArrayMessage.h"
+
+class QLocalServer;
+class QLocalSocket;
+
+class ServiceDataManager : public QThread
+{
+	Q_OBJECT
+public:
+	enum class Command {
+		NoCommand,
+		ReadData,
+		WriteData,
+	};
+	Q_ENUM(Command)
+
+	using Data = CryptoCore::PlaintextPassword;
+	using Token = CryptoCore::PlaintextPassword;
+
+	ServiceDataManager( QObject* parent = nullptr );
+	~ServiceDataManager() override;
+
+	const Token& token() const
+	{
+		return m_token;
+	}
+
+	static QByteArray read( const Token& token );
+	static bool write( const Token& token, const Data& data );
+
+protected:
+	void run() override;
+
+private:
+	static constexpr auto SocketWaitTimeout = 1000;
+	static constexpr auto MessageReadTimeout = 10000;
+
+	static bool waitForMessage( QLocalSocket* socket );
+	static void sendMessage( QLocalSocket* socket, VariantArrayMessage& message );
+
+	void acceptConnection();
+	void handleConnection( QLocalSocket* socket );
+
+	static QString serverName()
+	{
+		return QStringLiteral("VeyonServiceDataManager");
+	}
+
+	QLocalServer* m_server{nullptr};
+
+	Token m_token;
+	Data m_data;
+
+};
