074f4ef94a0d406cabb8ade7c14aa83f399ac733
Tobias Junghans <tobydox@veyon.io>
2019-10-07 07:54:04 +0200

    PlatformUserFunctions: split logon()
    While the prepareLogon() function writes the given credentials to the
    ServiceDataManager and forces a restart of the logon screen, the
    performLogon() method is being called after the server has been
    restarted within the cleanly initialized logon screen.
    
    (cherry picked from commit 7f9f0576b7984c7a0344bfda660731d0deecdfdd)

--- plugins/platform/linux/LinuxUserFunctions.h
+++ plugins/platform/linux/LinuxUserFunctions.h
@@ -42,7 +42,8 @@ public:
 	bool isAnyUserLoggedOn() override;
 	QString currentUser() override;
 
-	bool logon( const QString& username, const Password& password ) override;
+	bool prepareLogon( const QString& username, const Password& password ) override;
+	bool performLogon( const QString& username, const Password& password ) override;
 	void logoff() override;
 
 	bool authenticate( const QString& username, const Password& password ) override;
