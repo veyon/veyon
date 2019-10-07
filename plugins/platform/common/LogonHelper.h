72236020843034a7c6913d0b1c3062648490798f
Tobias Junghans <tobydox@veyon.io>
2019-10-07 07:40:42 +0200

    Platform: add LogonHelper

---
+++ plugins/platform/common/LogonHelper.h
@@ -0,0 +1,44 @@
+/*
+ * LogonHelper.h - declaration of LogonHelper class
+ *
+ * Copyright (c) 2019 Tobias Junghans <tobydox@veyon.io>
+ *
+ * This file is part of Veyon - https://veyon.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#pragma once
+
+#include "CryptoCore.h"
+
+// clazy:exclude=copyable-polymorphic
+
+class LogonHelper : public QObject
+{
+	Q_OBJECT
+public:
+	using Password = CryptoCore::SecureArray;
+
+	LogonHelper( QObject* parent = nullptr );
+
+	bool prepare( const QString& username, const Password& password );
+
+private:
+	void checkPendingLogonTasks();
+
+};
