956aaae4505bff98400e7e181b326d7795aa2beb
Tobias Junghans <tobydox@veyon.io>
2019-11-27 15:33:12 +0100

    Configurator: use VeyonCore::versionString()

--- configurator/src/MainWindow.cpp
+++ configurator/src/MainWindow.cpp
@@ -49,7 +49,7 @@ MainWindow::MainWindow( QWidget* parent ) :
 {
 	ui->setupUi( this );
 
-	setWindowTitle( tr( "%1 Configurator %2" ).arg( VeyonCore::applicationName(), VeyonCore::version() ) );
+	setWindowTitle( tr( "%1 Configurator %2" ).arg( VeyonCore::applicationName(), VeyonCore::versionString() ) );
 
 	loadConfigurationPagePlugins();
 
