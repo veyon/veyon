veyon/veyon

commit 1401906da5a98e8d064411bd05d64c47bed013ea

Allow remote log in when a logon message is displayed
Add feature to allow login when there is a login message displayed.

plugins/platform/windows/WindowsPlatformConfiguration.h
@@ -34,5 +34,6 @@
 	OP( WindowsPlatformConfiguration, m_configuration, bool, hideStartMenuForScreenLock, setHideStartMenuForScreenLock, "HideStartMenuForScreenLock", "Windows", true, Configuration::Property::Flag::Advanced ) \
 	OP( WindowsPlatformConfiguration, m_configuration, int, logonInputStartDelay, setLogonInputStartDelay, "LogonInputStartDelay", "Windows", 1000, Configuration::Property::Flag::Advanced ) \
 	OP( WindowsPlatformConfiguration, m_configuration, int, logonKeyPressInterval, setLogonKeyPressInterval, "LogonKeyPressInterval", "Windows", 10, Configuration::Property::Flag::Advanced ) \
+	OP( WindowsPlatformConfiguration, m_configuration, bool, logonMessageUsed, setLogonMessageUsed, "LogonMessageUsed", "Windows", false, Configuration::Property::Flag::Advanced ) \
 
 DECLARE_CONFIG_PROXY(WindowsPlatformConfiguration, FOREACH_WINDOWS_PLATFORM_CONFIG_PROPERTY)

plugins/platform/windows/WindowsPlatformConfigurationPage.ui
@@ -100,6 +100,13 @@
         </property>
        </widget>
       </item>
+      <item row="2">
+       <widget class="QCheckBox" name="logonMessageUsed">
+        <property name="text">
+         <string>Message is displayed before user logs in</string>
+        </property>
+       </widget>
+      </item>
      </layout>
     </widget>
    </item>
@@ -153,6 +160,7 @@
   <tabstop>disableSSPIBasedUserAuthentication</tabstop>
   <tabstop>logonInputStartDelay</tabstop>
   <tabstop>logonKeyPressInterval</tabstop>
+  <tabstop>logonMessageUsed</tabstop>
   <tabstop>hideTaskbarForScreenLock</tabstop>
   <tabstop>hideStartMenuForScreenLock</tabstop>
   <tabstop>hideDesktopForScreenLock</tabstop>

plugins/platform/windows/WindowsUserFunctions.cpp
@@ -197,6 +197,11 @@ bool WindowsUserFunctions::performLogon( const QString& username, const Password
 	QThread::msleep( static_cast<unsigned long>( config.logonInputStartDelay() ) );
 
 	input.pressAndReleaseKey( XK_Delete );
+	
+	if (config.logonMessageUsed()) {
+		input.pressAndReleaseKey( XK_Return );
+		QThread::msleep( static_cast<unsigned long>( config.logonInputStartDelay() ) );
+	}
 
 	sendString( username );
 

commit 4e92730ff0e85eeb58108bbad35d64df5952b5e4

Windows: improve legal notice confirmation
This improves the new legal notice confirmation code introduced in
1401906da5a98e8d064411bd05d64c47bed013ea.

(cherry picked from commit 3c00de3de5498f50b9de14f5d57c98e11265135f)

plugins/platform/windows/WindowsPlatformConfiguration.h
@@ -34,6 +34,6 @@
 	OP( WindowsPlatformConfiguration, m_configuration, bool, hideStartMenuForScreenLock, setHideStartMenuForScreenLock, "HideStartMenuForScreenLock", "Windows", true, Configuration::Property::Flag::Advanced ) \
 	OP( WindowsPlatformConfiguration, m_configuration, int, logonInputStartDelay, setLogonInputStartDelay, "LogonInputStartDelay", "Windows", 1000, Configuration::Property::Flag::Advanced ) \
 	OP( WindowsPlatformConfiguration, m_configuration, int, logonKeyPressInterval, setLogonKeyPressInterval, "LogonKeyPressInterval", "Windows", 10, Configuration::Property::Flag::Advanced ) \
-	OP( WindowsPlatformConfiguration, m_configuration, bool, logonMessageUsed, setLogonMessageUsed, "LogonMessageUsed", "Windows", false, Configuration::Property::Flag::Advanced ) \
+	OP( WindowsPlatformConfiguration, m_configuration, bool, logonConfirmLegalNotice, setLogonConfirmLegalNotice, "LogonConfirmLegalNotice", "Windows", false, Configuration::Property::Flag::Advanced ) \
 
 DECLARE_CONFIG_PROXY(WindowsPlatformConfiguration, FOREACH_WINDOWS_PLATFORM_CONFIG_PROPERTY)

plugins/platform/windows/WindowsPlatformConfigurationPage.ui
@@ -100,10 +100,10 @@
         </property>
        </widget>
       </item>
-      <item row="2">
-       <widget class="QCheckBox" name="logonMessageUsed">
+      <item row="2" column="0" colspan="2">
+       <widget class="QCheckBox" name="logonConfirmLegalNotice">
         <property name="text">
-         <string>Message is displayed before user logs in</string>
+         <string>Confirm legal notice (message displayed before user logs in)</string>
         </property>
        </widget>
       </item>
@@ -160,7 +160,7 @@
   <tabstop>disableSSPIBasedUserAuthentication</tabstop>
   <tabstop>logonInputStartDelay</tabstop>
   <tabstop>logonKeyPressInterval</tabstop>
-  <tabstop>logonMessageUsed</tabstop>
+  <tabstop>logonConfirmLegalNotice</tabstop>
   <tabstop>hideTaskbarForScreenLock</tabstop>
   <tabstop>hideStartMenuForScreenLock</tabstop>
   <tabstop>hideDesktopForScreenLock</tabstop>

plugins/platform/windows/WindowsUserFunctions.cpp
@@ -196,12 +196,13 @@ bool WindowsUserFunctions::performLogon( const QString& username, const Password
 
 	QThread::msleep( static_cast<unsigned long>( config.logonInputStartDelay() ) );
 
-	input.pressAndReleaseKey( XK_Delete );
-	
-	if (config.logonMessageUsed()) {
+	if( config.logonConfirmLegalNotice() )
+	{
 		input.pressAndReleaseKey( XK_Return );
 		QThread::msleep( static_cast<unsigned long>( config.logonInputStartDelay() ) );
 	}
+
+	input.pressAndReleaseKey( XK_Delete );
 
 	sendString( username );
 