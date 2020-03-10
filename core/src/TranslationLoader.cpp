60de4eac5962e51c1ce0bb62aff2b7de24e535d2
Tobias Junghans <tobydox@veyon.io>
2020-03-10 14:21:02 +0100

    Core: add TranslationLoader
    (cherry picked from commit e96f9baf53a4508aef4aa00c39f9b32a63b53867)

---
+++ core/src/TranslationLoader.cpp
@@ -0,0 +1,76 @@
+/*
+ * TranslationLoader.cpp - implementation of TranslationLoader class
+ *
+ * Copyright (c) 2020 Tobias Junghans <tobydox@veyon.io>
+ *
+ * This file is part of Veyon - https://veyon.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#include <QCoreApplication>
+#include <QTranslator>
+
+#include "TranslationLoader.h"
+#include "VeyonConfiguration.h"
+#include "VeyonCore.h"
+
+
+TranslationLoader::TranslationLoader( const QString& resourceName )
+{
+	load( resourceName );
+}
+
+
+
+QLocale TranslationLoader::load( const QString& resourceName )
+{
+	QLocale configuredLocale( QLocale::C );
+
+	QRegExp localeRegEx( QStringLiteral( "[^(]*\\(([^)]*)\\)") );
+	if( localeRegEx.indexIn( VeyonCore::config().uiLanguage() ) == 0 )
+	{
+		configuredLocale = QLocale( localeRegEx.cap( 1 ) );
+	}
+
+	if( configuredLocale.language() != QLocale::English &&
+		VeyonCore::instance()->findChild<QTranslator *>( resourceName ) == nullptr )
+	{
+		auto translator = new QTranslator( VeyonCore::instance() );
+		translator->setObjectName( resourceName );
+
+		if( configuredLocale == QLocale::C ||
+			translator->load( QStringLiteral( "%1_%2.qm" ).arg( resourceName, configuredLocale.name() ),
+							  VeyonCore::translationsDirectory() ) == false )
+		{
+			configuredLocale = QLocale::system(); // Flawfinder: ignore
+
+			if( translator->load( QStringLiteral( "%1_%2.qm" ).arg( resourceName, configuredLocale.name() ),
+								  VeyonCore::translationsDirectory() ) == false )
+			{
+				translator->load( QStringLiteral( "%1_%2.qm" ).arg( resourceName, configuredLocale.language() ),
+								  VeyonCore::translationsDirectory() );
+			}
+		}
+
+		QLocale::setDefault( configuredLocale );
+
+		QCoreApplication::installTranslator( translator );
+	}
+
+	return configuredLocale;
+}
