29b67e91c3172c1c371531bece22afd7b6681dd3
Tobias Junghans <tobydox@veyon.io>
2020-03-10 13:29:38 +0100

    cmake: rename BuildApplication to BuildVeyonApplication
    (cherry picked from commit 5b693dd56c19909a07c34d3a0e529a07d5f3194a)

---
+++ cmake/modules/BuildVeyonApplication.cmake
@@ -0,0 +1,14 @@
+# BuildVeyonApplication.cmake - Copyright (c) 2019-2020 Tobias Junghans
+#
+# description: build Veyon application
+# usage: build_veyon_application(<NAME> <SOURCES>)
+
+MACRO(build_veyon_application APPLICATION_NAME)
+	IF(VEYON_BUILD_ANDROID)
+		ADD_LIBRARY(${APPLICATION_NAME} SHARED ${ARGN})
+	ELSE()
+		ADD_EXECUTABLE(${APPLICATION_NAME} ${ARGN})
+		INSTALL(TARGETS ${APPLICATION_NAME} RUNTIME DESTINATION bin)
+	ENDIF()
+ENDMACRO()
+
