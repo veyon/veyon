5d9a382587adb4b50c9a2a266ed31a7cb117c95d
Tobias Junghans <tobydox@veyon.io>
2020-04-22 15:38:21 +0200

    3rdparty: ultravnc: update submodule
    (cherry picked from commit c3aeeda75eb9b35ee866d689a3f590e04d4da7ee)

--- .ci/common/strip-ultravnc-sources.sh
+++ .ci/common/strip-ultravnc-sources.sh
@@ -2,7 +2,7 @@
 
 cd 3rdparty/ultravnc
 find -name "*.vcproj" -o -name "*.vcxproj" -o -name "*.vdproj" -o -name "*.sln" | xargs rm
-rm -rf avilog translations old vncviewer zipunzip_src libjpeg-turbo-win winvnc/winvnc/res setcad setpasswd lzo* xz* zlib* JavaViewer repeater uvnc* *.iss *.zip
+rm -rf avilog translations old vncviewer zipunzip_src libjpeg-turbo-win winvnc/winvnc/res setcad setpasswd lzo* xz* zlib* zstd* JavaViewer repeater uvnc* *.iss *.zip
 cd DSMPlugin
 rm -rf MSRC4Plugin TestPlugin
 cd ..
