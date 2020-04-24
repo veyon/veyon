321adbe10fbad9d118f23606bb0acc191134189b
Tobias Junghans <tobydox@veyon.io>
2020-04-24 15:57:59 +0200

    CMake: add FindLibVNCClient/FindLibVNCServer
    (cherry picked from commit 354e56222002cdc972e1fe7dfa2db2773b2e3484)

---
+++ cmake/modules/FindLibVNCClient.cmake
@@ -0,0 +1,61 @@
+#.rst:
+# FindLibVNCClient
+# --------
+#
+# Try to find the LibVNCClient library, once done this will define:
+#
+# ``LibVNCClient_FOUND``
+#	 System has LibVNCClient.
+#
+# ``LibVNCClient_INCLUDE_DIRS``
+#	 The LibVNCClient include directory.
+#
+# ``LibVNCClient_LIBRARIES``
+#	 The LibVNCClient libraries.
+#
+# ``LibVNCClient_VERSION``
+#	 The LibVNCClient version.
+#
+# If ``LibVNCClient_FOUND`` is TRUE, the following imported target
+# will be available:
+#
+# ``LibVNC::LibVNCClient``
+#	 The LibVNCClient library
+
+#=============================================================================
+# SPDX-FileCopyrightText: 2020 Tobias Junghans <tobydox@veyon.io>
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#=============================================================================
+
+find_package(PkgConfig QUIET)
+pkg_check_modules(PC_LIBVNCCLIENT QUIET libvncclient)
+
+find_path(LibVNCClient_INCLUDE_DIRS NAMES rfb/rfbclient.h HINTS ${PC_LIBVNCCLIENT_INCLUDE_DIRS})
+find_library(LibVNCClient_LIBRARIES NAMES vncclient HINTS ${PC_LIBVNCCLIENT_LIBRARY_DIRS})
+
+set(LibVNCClient_VERSION ${PC_LIBVNCCLIENT_VERSION})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(LibVNCClient
+	FOUND_VAR LibVNCClient_FOUND
+	REQUIRED_VARS LibVNCClient_INCLUDE_DIRS LibVNCClient_LIBRARIES
+	VERSION_VAR LibVNCClient_VERSION
+)
+
+mark_as_advanced(LibVNCClient_INCLUDE_DIRS LibVNCClient_LIBRARIES)
+
+if(LibVNCClient_FOUND AND NOT TARGET LibVNCClient::LibVNCClient)
+	add_library(LibVNC::LibVNCClient UNKNOWN IMPORTED)
+	set_target_properties(LibVNC::LibVNCClient PROPERTIES
+		IMPORTED_LOCATION "${LibVNCClient_LIBRARIES}"
+		INTERFACE_INCLUDE_DIRECTORIES "${LibVNCClient_INCLUDE_DIRS}"
+		INTERFACE_COMPILE_DEFINITIONS "${PC_LIBVNCCLIENT_CFLAGS_OTHER}"
+	)
+endif()
+
+include(FeatureSummary)
+set_package_properties(LibVNCClient PROPERTIES
+	DESCRIPTION "cross-platform C library which allows you to easily implement VNC client functionality"
+	URL "https://libvnc.github.io/"
+)
---
+++ cmake/modules/FindLibVNCServer.cmake
@@ -0,0 +1,61 @@
+#.rst:
+# FindLibVNCServer
+# --------
+#
+# Try to find the LibVNCServer library, once done this will define:
+#
+# ``LibVNCServer_FOUND``
+#	 System has LibVNCServer.
+#
+# ``LibVNCServer_INCLUDE_DIRS``
+#	 The LibVNCServer include directory.
+#
+# ``LibVNCServer_LIBRARIES``
+#	 The LibVNCServer libraries.
+#
+# ``LibVNCServer_VERSION``
+#	 The LibVNCServer version.
+#
+# If ``LibVNCServer_FOUND`` is TRUE, the following imported target
+# will be available:
+#
+# ``LibVNC::LibVNCServer``
+#	 The LibVNCServer library
+
+#=============================================================================
+# SPDX-FileCopyrightText: 2020 Tobias Junghans <tobydox@veyon.io>
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#=============================================================================
+
+find_package(PkgConfig QUIET)
+pkg_check_modules(PC_LIBVNCCLIENT QUIET libvncserver)
+
+find_path(LibVNCServer_INCLUDE_DIRS NAMES rfb/rfb.h HINTS ${PC_LIBVNCCLIENT_INCLUDE_DIRS})
+find_library(LibVNCServer_LIBRARIES NAMES vncserver HINTS ${PC_LIBVNCCLIENT_LIBRARY_DIRS})
+
+set(LibVNCServer_VERSION ${PC_LIBVNCCLIENT_VERSION})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(LibVNCServer
+	FOUND_VAR LibVNCServer_FOUND
+	REQUIRED_VARS LibVNCServer_INCLUDE_DIRS LibVNCServer_LIBRARIES
+	VERSION_VAR LibVNCServer_VERSION
+)
+
+mark_as_advanced(LibVNCServer_INCLUDE_DIRS LibVNCServer_LIBRARIES)
+
+if(LibVNCServer_FOUND AND NOT TARGET LibVNCServer::LibVNCServer)
+	add_library(LibVNC::LibVNCServer UNKNOWN IMPORTED)
+	set_target_properties(LibVNC::LibVNCServer PROPERTIES
+		IMPORTED_LOCATION "${LibVNCServer_LIBRARIES}"
+		INTERFACE_INCLUDE_DIRECTORIES "${LibVNCServer_INCLUDE_DIRS}"
+		INTERFACE_COMPILE_DEFINITIONS "${PC_LIBVNCCLIENT_CFLAGS_OTHER}"
+	)
+endif()
+
+include(FeatureSummary)
+set_package_properties(LibVNCServer PROPERTIES
+	DESCRIPTION "cross-platform C library which allows you to easily implement VNC server functionality"
+	URL "https://libvnc.github.io/"
+)
