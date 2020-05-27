veyon/veyon

commit f3aedf42b47922a3ed83440718e28dcbce9b9c01

Core: various code modernizations

core/src/Configuration/Object.cpp
@@ -30,20 +30,8 @@
 namespace Configuration
 {
 
-
-Object::Object() :
-	m_store( nullptr ),
-	m_customStore( false ),
-	m_data()
-{
-}
-
-
-
 Object::Object( Store::Backend backend, Store::Scope scope, const QString& storeName ) :
-	m_store( createStore( backend, scope ) ),
-	m_customStore( false ),
-	m_data()
+	m_store( createStore( backend, scope ) )
 {
 	m_store->setName( storeName );
 
@@ -53,9 +41,7 @@ Object::Object( Store::Backend backend, Store::Scope scope, const QString& store
 
 
 Object::Object( Store* store ) :
-	m_store( store ),
-	m_customStore( true ),
-	m_data()
+	m_store( store )
 {
 	reloadFromStore();
 }
@@ -63,8 +49,7 @@ Object::Object( Store* store ) :
 
 
 Object::Object( const Object& obj ) :
-	m_store( nullptr ),
-	m_customStore( false )
+	m_store( nullptr )
 {
 	*this = obj;
 }

core/src/AccessControlProvider.cpp
@@ -37,7 +37,6 @@
 
 
 AccessControlProvider::AccessControlProvider() :
-	m_accessControlRules(),
 	m_userGroupsBackend( VeyonCore::userGroupsBackendManager().accessControlBackend() ),
 	m_networkObjectDirectory( VeyonCore::networkObjectDirectoryManager().configuredDirectory() ),
 	m_queryDomainGroups( VeyonCore::config().domainGroupsForAccessControlEnabled() )

core/src/AccessControlProvider.h
@@ -78,7 +78,7 @@ private:
 
 	static QStringList objectNames( const NetworkObjectList& objects );
 
-	QList<AccessControlRule> m_accessControlRules;
+	QList<AccessControlRule> m_accessControlRules{};
 	UserGroupsBackendInterface* m_userGroupsBackend;
 	NetworkObjectDirectory* m_networkObjectDirectory;
 	bool m_queryDomainGroups;

core/src/AccessControlRule.cpp
@@ -30,7 +30,6 @@ AccessControlRule::AccessControlRule() :
 	m_name(),
 	m_description(),
 	m_action( Action::None ),
-	m_parameters(),
 	m_invertConditions( false ),
 	m_ignoreConditions( false )
 {
@@ -54,7 +53,6 @@ AccessControlRule::AccessControlRule(const QJsonValue &jsonValue) :
 	m_name(),
 	m_description(),
 	m_action( Action::None ),
-	m_parameters(),
 	m_invertConditions( false ),
 	m_ignoreConditions( false )
 {

core/src/AuthenticationManager.cpp
@@ -27,9 +27,7 @@
 #include "VeyonConfiguration.h"
 
 AuthenticationManager::AuthenticationManager( QObject* parent ) :
-	QObject( parent ),
-	m_configuredPlugin( nullptr ),
-	m_dummyAuthentication()
+	QObject( parent )
 {
 	for( auto pluginObject : qAsConst( VeyonCore::pluginManager().pluginObjects() ) )
 	{

core/src/AuthenticationManager.h
@@ -51,8 +51,8 @@ public:
 	void reloadConfiguration();
 
 private:
-	Plugins m_plugins;
-	AuthenticationPluginInterface* m_configuredPlugin;
-	DummyAuthentication m_dummyAuthentication;
+	Plugins m_plugins{};
+	AuthenticationPluginInterface* m_configuredPlugin{nullptr};
+	DummyAuthentication m_dummyAuthentication{};
 
 };

core/src/Configuration/Object.h
@@ -38,7 +38,7 @@ class VEYON_CORE_EXPORT Object : public QObject
 public:
 	using DataMap = QMap<QString, QVariant>;
 
-	Object();
+	Object() = default;
 	Object( Store::Backend backend, Store::Scope scope, const QString& storeName = {} );
 	explicit Object( Store* store );
 	Object( const Object& );
@@ -96,9 +96,9 @@ signals:
 private:
 	static Store* createStore( Store::Backend backend, Store::Scope scope );
 
-	Configuration::Store *m_store;
-	bool m_customStore;
-	DataMap m_data;
+	Configuration::Store* m_store{nullptr};
+	bool m_customStore{false};
+	DataMap m_data{};
 
 } ;
 

core/src/Configuration/Property.cpp
@@ -76,7 +76,8 @@ QVariant Property::variantValue() const
 	{
 		return m_object->value( m_key, m_parentKey, m_defaultValue );
 	}
-	else if( m_proxy )
+
+	if( m_proxy )
 	{
 		return m_proxy->value( m_key, m_parentKey, m_defaultValue );
 	}

commit 2ce6f769b73ecccbfb73d9419056bdaf6b03540d

Core: Configuration: fix customStore flag init
Previously broken by f3aedf42b47922a3ed83440718e28dcbce9b9c01.

core/src/Configuration/Object.cpp
@@ -41,7 +41,8 @@ Object::Object( Store::Backend backend, Store::Scope scope, const QString& store
 
 
 Object::Object( Store* store ) :
-	m_store( store )
+	m_store( store ),
+	m_customStore( true )
 {
 	reloadFromStore();
 }