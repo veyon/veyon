63a6d6a13b5a3251eb7b1f6de7bdec6d80baf4dc
Tobias Junghans <tobydox@veyon.io>
2020-06-16 15:20:25 +0200

    Core: add PlatformSessionFunctions
    (cherry picked from commit b9637614cb5fd52b0c81023c3c2119ff785c46c9)

---
+++ core/include/PlatformSessionFunctions.h
@@ -0,0 +1,42 @@
+/*
+ * PlatformSessionsFunctions.h - interface class for platform session functions
+ *
+ * Copyright (c) 2020 Tobias Junghans <tobydox@veyon.io>
+ *
+ * This file is part of Veyon - https://veyon.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#pragma once
+
+#include "PlatformPluginInterface.h"
+
+// clazy:excludeall=copyable-polymorphic
+
+class PlatformSessionFunctions
+{
+public:
+	using SessionId = int;
+
+	static constexpr SessionId DefaultSession = 0;
+
+	virtual ~PlatformSessionFunctions() = default;
+
+	virtual SessionId currentSessionId() const = 0;
+
+};
