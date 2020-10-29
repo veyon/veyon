8d1cc0bf9eeebef499462a59cb702c219a67739e
Tobias Junghans <tobydox@veyon.io>
2020-10-29 11:39:24 +0100

    VeyonMaster: add subFeaturesUids()

--- master/src/VeyonMaster.h
+++ master/src/VeyonMaster.h
@@ -97,6 +97,7 @@ public:
 	}
 
 	FeatureList subFeatures( Feature::Uid parentFeatureUid ) const;
+	FeatureUidList subFeaturesUids( Feature::Uid parentFeatureUid ) const;
 
 	FeatureList modeFeatures() const;
 
