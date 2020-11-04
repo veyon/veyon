2ac45d6ca31b9fe3cc5a8fd4fbefc9cc9620d70e
Tobias Junghans <tobydox@veyon.io>
2020-11-04 09:32:41 +0100

    Master: add SlideshowModel

---
+++ master/src/SlideshowModel.cpp
@@ -0,0 +1,158 @@
+/*
+ * SlideshowModel.cpp - implementation of SlideshowModel
+ *
+ * Copyright (c) 2020 Tobias Junghans <tobydox@veyon.io>
+ *
+ * This file is part of Veyon - https://veyon.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#include "ComputerListModel.h"
+#include "SlideshowModel.h"
+
+
+SlideshowModel::SlideshowModel( QAbstractItemModel* sourceModel, QObject* parent ) :
+	QSortFilterProxyModel( parent )
+{
+	setSourceModel( sourceModel );
+
+	connect( sourceModel, &QAbstractItemModel::rowsInserted, this,
+			 [this]( const QModelIndex &parent, int start, int end )
+			 {
+				 Q_UNUSED(parent)
+				 Q_UNUSED(end)
+				 if( start <= m_currentRow )
+				 {
+					 setCurrentRow( m_currentRow + 1 );
+				 }
+			 } );
+	connect( sourceModel, &QAbstractItemModel::rowsRemoved, this,
+			 [this]( const QModelIndex &parent, int start, int end )
+			 {
+				 Q_UNUSED(parent)
+				 Q_UNUSED(end)
+				 if( start <= m_currentRow )
+				 {
+					 setCurrentRow( m_currentRow - 1 );
+				 }
+			 } );
+
+	connect( &m_timer, &QTimer::timeout, this, &SlideshowModel::showNext );
+}
+
+
+
+void SlideshowModel::setIconSize( QSize size )
+{
+	m_iconSize = size;
+
+	Q_EMIT dataChanged( index( 0, 0 ), index( rowCount() - 1, 0 ), { Qt::DisplayRole, Qt::DecorationRole } );
+}
+
+
+
+QVariant SlideshowModel::data( const QModelIndex& index, int role ) const
+{
+	const auto sourceIndex = mapToSource( index );
+	if( sourceIndex.isValid() == false )
+	{
+		return {};
+	}
+
+	if( role == Qt::DecorationRole )
+	{
+		auto screen = sourceModel()->data( sourceIndex, ComputerListModel::ScreenRole ).value<QImage>();
+		if( screen.isNull() )
+		{
+			screen = sourceModel()->data( sourceIndex, Qt::DecorationRole ).value<QImage>();
+		}
+
+		return screen.scaled( m_iconSize, Qt::KeepAspectRatio, Qt::SmoothTransformation );
+	}
+
+	return QSortFilterProxyModel::data( index, role );
+}
+
+
+
+void SlideshowModel::setRunning( bool running, int duration )
+{
+	m_timer.stop();
+	m_timer.setInterval( duration );
+
+	if( running )
+	{
+		m_timer.start();
+	}
+}
+
+
+
+void SlideshowModel::showPrevious()
+{
+	setCurrentRow( m_currentRow - 1 );
+
+	if( m_timer.isActive() )
+	{
+		m_timer.stop();
+		m_timer.start();
+	}
+}
+
+
+
+void SlideshowModel::showNext()
+{
+	setCurrentRow( m_currentRow + 1 );
+
+	if( m_timer.isActive() )
+	{
+		m_timer.stop();
+		m_timer.start();
+	}
+}
+
+
+
+bool SlideshowModel::filterAcceptsRow( int sourceRow, const QModelIndex& sourceParent ) const
+{
+	return m_currentControlInterface == sourceModel()->data( sourceModel()->index( sourceRow, 0, sourceParent ),
+															 ComputerListModel::ControlInterfaceRole )
+											.value<ComputerControlInterface::Pointer>();
+}
+
+
+
+void SlideshowModel::setCurrentRow( int row )
+{
+	if( sourceModel()->rowCount() > 0 )
+	{
+		m_currentRow = qMax( 0, row ) % qMax( 1, sourceModel()->rowCount() );
+
+		m_currentControlInterface = sourceModel()->data( sourceModel()->index( m_currentRow, 0 ),
+														 ComputerListModel::ControlInterfaceRole )
+										.value<ComputerControlInterface::Pointer>();
+	}
+	else
+	{
+		m_currentRow = 0;
+		m_currentControlInterface.clear();
+	}
+
+	invalidateFilter();
+}
---
+++ master/src/SlideshowModel.h
@@ -0,0 +1,60 @@
+/*
+ * SlideshowModel.h - header file for SlideshowModel
+ *
+ * Copyright (c) 2020 Tobias Junghans <tobydox@veyon.io>
+ *
+ * This file is part of Veyon - https://veyon.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#pragma once
+
+#include <QSortFilterProxyModel>
+#include <QTimer>
+
+#include "ComputerControlInterface.h"
+
+class SlideshowModel : public QSortFilterProxyModel
+{
+	Q_OBJECT
+public:
+	SlideshowModel( QAbstractItemModel* sourceModel, QObject* parent = nullptr );
+
+	void setIconSize( QSize size );
+
+	QVariant data( const QModelIndex &index, int role = Qt::DisplayRole ) const override;
+
+public:
+	void setRunning( bool running, int duration );
+	void showPrevious();
+	void showNext();
+
+protected:
+	bool filterAcceptsRow( int sourceRow, const QModelIndex& sourceParent ) const override;
+
+private:
+	void setCurrentRow( int row );
+
+	QSize m_iconSize;
+
+	QTimer m_timer;
+
+	int m_currentRow{0};
+	ComputerControlInterface::Pointer m_currentControlInterface;
+
+};
