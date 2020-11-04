47010aa8aa397cfaa7a42e2f0ee92f7ebd652dd8
Tobias Junghans <tobydox@veyon.io>
2020-11-04 10:13:45 +0100

    Master: add SpotlightModel
    (cherry picked from commit 341ed53a06aedef3cd9757b766ab42d6e8b44f30)

---
+++ master/src/SpotlightModel.cpp
@@ -0,0 +1,93 @@
+/*
+ * SpotlightModel.cpp - implementation of SpotlightModel
+ *
+ * Copyright (c) 2020 Tobias Junghans <tobydox@veyon.io>
+ *
+ * This file is part of Veyon - https://veyon.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#include "SpotlightModel.h"
+
+
+SpotlightModel::SpotlightModel( QAbstractItemModel* sourceModel, QObject* parent ) :
+	QSortFilterProxyModel( parent )
+{
+	setSourceModel( sourceModel );
+}
+
+
+
+void SpotlightModel::setIconSize( QSize size )
+{
+	m_iconSize = size;
+
+	Q_EMIT dataChanged( index( 0, 0 ), index( rowCount() - 1, 0 ), { Qt::DisplayRole, Qt::DecorationRole } );
+}
+
+
+
+void SpotlightModel::add( const ComputerControlInterface::Pointer& controlInterface )
+{
+	m_controlInterfaces.append( controlInterface );
+
+	invalidateFilter();
+}
+
+
+
+void SpotlightModel::remove( const ComputerControlInterface::Pointer& controlInterface )
+{
+	m_controlInterfaces.removeAll( controlInterface );
+
+	invalidateFilter();
+}
+
+
+
+
+QVariant SpotlightModel::data( const QModelIndex& index, int role ) const
+{
+	const auto sourceIndex = mapToSource( index );
+	if( sourceIndex.isValid() == false )
+	{
+		return {};
+	}
+
+	if( role == Qt::DecorationRole )
+	{
+		auto screen = sourceModel()->data( sourceIndex, ComputerControlListModel::ScreenRole ).value<QImage>();
+		if( screen.isNull() )
+		{
+			screen = sourceModel()->data( sourceIndex, Qt::DecorationRole ).value<QImage>();
+		}
+
+		return screen.scaled( m_iconSize, Qt::KeepAspectRatio, Qt::SmoothTransformation );
+	}
+
+	return QSortFilterProxyModel::data( index, role );
+}
+
+
+
+bool SpotlightModel::filterAcceptsRow( int sourceRow, const QModelIndex& sourceParent ) const
+{
+	return m_controlInterfaces.contains( sourceModel()->data( sourceModel()->index( sourceRow, 0, sourceParent ),
+													   ComputerControlListModel::ControlInterfaceRole )
+									  .value<ComputerControlInterface::Pointer>() );
+}
---
+++ master/src/SpotlightModel.h
@@ -0,0 +1,54 @@
+/*
+ * SpotlightModel.h - header file for SpotlightModel
+ *
+ * Copyright (c) 2020 Tobias Junghans <tobydox@veyon.io>
+ *
+ * This file is part of Veyon - https://veyon.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#pragma once
+
+#include <QSortFilterProxyModel>
+
+#include "ComputerControlListModel.h"
+
+class SpotlightModel : public QSortFilterProxyModel
+{
+	Q_OBJECT
+public:
+	static constexpr auto ControlInterfaceRole = ComputerControlListModel::ControlInterfaceRole;
+
+	SpotlightModel( QAbstractItemModel* sourceModel, QObject* parent = nullptr );
+
+	void setIconSize( QSize size );
+
+	void add( const ComputerControlInterface::Pointer& controlInterface );
+	void remove( const ComputerControlInterface::Pointer& controlInterface );
+
+	QVariant data( const QModelIndex &index, int role = Qt::DisplayRole ) const override;
+
+protected:
+	bool filterAcceptsRow( int sourceRow, const QModelIndex& sourceParent ) const override;
+
+private:
+	QSize m_iconSize;
+
+	ComputerControlInterfaceList m_controlInterfaces;
+
+};
