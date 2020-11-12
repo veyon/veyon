d9dee1412e752553af70e7852c083096a294041e
Tobias Junghans <tobydox@veyon.io>
2020-11-12 08:09:17 +0100

    Feature: drop unused flag Dialog
    (cherry picked from commit 82b5f37f12a0529de5cb4c96191804ebd0b6fbfa)

--- core/src/DesktopAccessDialog.cpp
+++ core/src/DesktopAccessDialog.cpp
@@ -38,7 +38,7 @@
 DesktopAccessDialog::DesktopAccessDialog( QObject* parent ) :
 	QObject( parent ),
 	m_desktopAccessDialogFeature( Feature( QLatin1String( staticMetaObject.className() ),
-										   Feature::Dialog | Feature::Service | Feature::Worker | Feature::Builtin,
+										   Feature::Service | Feature::Worker | Feature::Builtin,
 										   Feature::Uid( "3dd8ec3e-7004-4936-8f2a-70699b9819be" ),
 										   Feature::Uid(),
 										   tr( "Desktop access dialog" ), {}, {} ) ),
