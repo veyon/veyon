553171c3ce54fdb5ebb805de33035583224cbcb6
Tobias Junghans <tobydox@veyon.io>
2020-11-25 09:12:36 +0100

    ComputerControlListModel: always supply non-empty display role
    There's a bug in Qt's item views making delegates not updating their
    size properly when supplying an empty display role (caption) first
    and changing it to a non-empty value later. This causes the caption
    being overlapped by the icon.

--- master/src/ComputerControlListModel.cpp
+++ master/src/ComputerControlListModel.cpp
@@ -504,7 +504,7 @@ QString ComputerControlListModel::computerDisplayRole( const ComputerControlInte
 		return controlInterface->computer().name();
 	}
 
-	return {};
+	return tr("[no user]");
 }
 
 
