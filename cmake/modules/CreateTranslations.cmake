1686ee57a6212b05a774b348b30e16d35c7c3660
Tobias Junghans <tobydox@veyon.io>
2020-12-01 12:57:14 +0100

    CMake: fix rules/deps for translation file generation
    This makes the TS files being updated whenever a source file changes
    and fixes ninja-based builds.

--- cmake/modules/CreateTranslations.cmake
+++ cmake/modules/CreateTranslations.cmake
@@ -7,13 +7,20 @@ function(create_translations name ts_files source_files)
 
 	set(qm_targets "")
 	foreach(ts_file ${ts_files})
-		string(REPLACE "${CMAKE_CURRENT_SOURCE_DIR}/" "" ts_target "${ts_file}")
-		string(REPLACE ".ts" ".qm" qm_target "${ts_target}")
-		set(qm_file "${CMAKE_CURRENT_BINARY_DIR}/${qm_target}")
-		add_custom_target(${ts_target} COMMAND ${Qt5_LUPDATE_EXECUTABLE} -locations none -no-obsolete ${source_files} -ts ${ts_file})
+		string(REPLACE "${CMAKE_CURRENT_SOURCE_DIR}/" "" ts_filename "${ts_file}")
+		string(REPLACE ".ts" "" basename "${ts_filename}")
+		set(ts_target "${basename}_ts")
+		set(qm_target "${basename}_qm")
+		set(qm_file "${CMAKE_CURRENT_BINARY_DIR}/${basename}.qm")
+		add_custom_command(OUTPUT ${ts_file}
+			COMMAND ${Qt5_LUPDATE_EXECUTABLE} -locations none -no-obsolete ${source_files} -ts ${ts_file}
+			DEPENDS ${source_files})
+		add_custom_target(${ts_target} DEPENDS ${ts_file})
 		# add command and target for generating/updating QM file if TS file is newer or no QM file exists yet
-		add_custom_command(OUTPUT ${qm_file} COMMAND ${Qt5_LRELEASE_EXECUTABLE} ${ts_file} -qm ${qm_file} DEPENDS ${ts_file})
-		add_custom_target(${qm_target} ALL DEPENDS ${qm_file})
+		add_custom_command(OUTPUT ${qm_file}
+			COMMAND ${Qt5_LRELEASE_EXECUTABLE} ${ts_file} -qm ${qm_file}
+			DEPENDS ${ts_file})
+		add_custom_target(${qm_target} DEPENDS ${qm_file})
 
 		list(APPEND qm_targets "${qm_target}")
 
