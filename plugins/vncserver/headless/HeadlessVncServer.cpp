0ec20dba8bbfe94012e92873a37802505c00d4ac
Tobias Junghans <tobydox@veyon.io>
2020-12-04 14:30:04 +0100

    Add headless VNC server plugin
    This VNC server is mainly used for testing. It can can also be used for
    clients on which only Veyon features should be launched without the
    posssibility to view screen contents.
    
    (cherry picked from commit 0ea3e80a8364959d1a57ac5dfe84bcb6e2ffb6bd)

---
+++ plugins/vncserver/headless/HeadlessVncServer.cpp
@@ -0,0 +1,146 @@
+/*
+ * HeadlessVncServer.cpp - implementation of HeadlessVncServer class
+ *
+ * Copyright (c) 2020 Tobias Junghans <tobydox@veyon.io>
+ *
+ * This file is part of Veyon - https://veyon.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+extern "C" {
+#include "rfb/rfb.h"
+}
+
+#include "HeadlessVncServer.h"
+#include "VeyonConfiguration.h"
+
+
+struct HeadlessVncScreen
+{
+	~HeadlessVncScreen()
+	{
+		delete[] passwords[0];
+	}
+
+	rfbScreenInfoPtr rfbScreen{nullptr};
+	std::array<char *, 2> passwords{};
+	QImage framebuffer;
+
+};
+
+
+HeadlessVncServer::HeadlessVncServer( QObject* parent ) :
+	QObject( parent ),
+	m_configuration( &VeyonCore::config() )
+{
+}
+
+
+
+void HeadlessVncServer::prepareServer()
+{
+}
+
+
+
+bool HeadlessVncServer::runServer( int serverPort, const Password& password )
+{
+	HeadlessVncScreen screen;
+
+	if( initScreen( &screen ) == false ||
+		initVncServer( serverPort, password, &screen ) == false )
+	{
+		return false;
+	}
+
+	while( true )
+	{
+		QThread::msleep( DefaultSleepTime );
+
+		rfbProcessEvents( screen.rfbScreen, 0 );
+	}
+
+	rfbShutdownServer( screen.rfbScreen, true );
+	rfbScreenCleanup( screen.rfbScreen );
+
+	return true;
+}
+
+
+
+bool HeadlessVncServer::initScreen( HeadlessVncScreen* screen )
+{
+	screen->framebuffer = QImage( DefaultFramebufferWidth, DefaultFramebufferHeight, QImage::Format_RGB32 );
+	screen->framebuffer.fill( m_configuration.backgroundColor() );
+
+	return true;
+}
+
+
+
+bool HeadlessVncServer::initVncServer( int serverPort, const VncServerPluginInterface::Password& password,
+									  HeadlessVncScreen* screen )
+{
+	auto rfbScreen = rfbGetScreen( nullptr, nullptr,
+								   screen->framebuffer.width(), screen->framebuffer.height(),
+								   8, 3, 4 );
+
+	if( rfbScreen == nullptr )
+	{
+		return false;
+	}
+
+	screen->passwords[0] = qstrdup( password.toByteArray().constData() );
+
+	rfbScreen->desktopName = "VeyonVNC";
+	rfbScreen->frameBuffer = reinterpret_cast<char *>( screen->framebuffer.bits() );
+	rfbScreen->port = serverPort;
+
+	rfbScreen->authPasswdData = screen->passwords.data();
+	rfbScreen->passwordCheck = rfbCheckPasswordByList;
+
+	rfbScreen->serverFormat.redShift = 16;
+	rfbScreen->serverFormat.greenShift = 8;
+	rfbScreen->serverFormat.blueShift = 0;
+
+	rfbScreen->serverFormat.redMax = 255;
+	rfbScreen->serverFormat.greenMax = 255;
+	rfbScreen->serverFormat.blueMax = 255;
+
+	rfbScreen->serverFormat.trueColour = true;
+	rfbScreen->serverFormat.bitsPerPixel = 32;
+
+	rfbScreen->alwaysShared = true;
+	rfbScreen->handleEventsEagerly = true;
+	rfbScreen->deferUpdateTime = 5;
+
+	rfbScreen->screenData = screen;
+
+	rfbScreen->cursor = nullptr;
+
+	rfbInitServer( rfbScreen );
+
+	rfbMarkRectAsModified( rfbScreen, 0, 0, rfbScreen->width, rfbScreen->height );
+
+	screen->rfbScreen = rfbScreen;
+
+	return true;
+}
+
+
+IMPLEMENT_CONFIG_PROXY(HeadlessVncConfiguration)
---
+++ plugins/vncserver/headless/HeadlessVncServer.h
@@ -0,0 +1,113 @@
+/*
+ * HeadlessVncServer.h - declaration of HeadlessVncServer class
+ *
+ * Copyright (c) 2020 Tobias Junghans <tobydox@veyon.io>
+ *
+ * This file is part of Veyon - https://veyon.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#pragma once
+
+#include "PluginInterface.h"
+#include "VncServerPluginInterface.h"
+#include "HeadlessVncConfiguration.h"
+
+struct HeadlessVncScreen;
+
+class HeadlessVncServer : public QObject, VncServerPluginInterface, PluginInterface
+{
+	Q_OBJECT
+	Q_PLUGIN_METADATA(IID "io.veyon.Veyon.Plugins.HeadlessVncServer")
+	Q_INTERFACES(PluginInterface VncServerPluginInterface)
+public:
+	explicit HeadlessVncServer( QObject* parent = nullptr );
+
+	Plugin::Uid uid() const override
+	{
+		return QStringLiteral("f626f759-7691-45c0-bd4a-37171d98d219");
+	}
+
+	QVersionNumber version() const override
+	{
+		return QVersionNumber( 1, 0 );
+	}
+
+	QString name() const override
+	{
+		return QStringLiteral( "HeadlessVncServer" );
+	}
+
+	QString description() const override
+	{
+		return tr( "Headless VNC server" );
+	}
+
+	QString vendor() const override
+	{
+		return QStringLiteral( "Veyon Community" );
+	}
+
+	QString copyright() const override
+	{
+		return QStringLiteral( "Tobias Junghans" );
+	}
+
+	Plugin::Flags flags() const override
+	{
+		return Plugin::NoFlags;
+	}
+
+	virtual QStringList supportedSessionTypes() const override
+	{
+		return {};
+	}
+
+	QWidget* configurationWidget() override
+	{
+		return nullptr;
+	}
+
+	void prepareServer() override;
+
+	bool runServer( int serverPort, const Password& password ) override;
+
+	int configuredServerPort() override
+	{
+		return -1;
+	}
+
+	Password configuredPassword() override
+	{
+		return {};
+	}
+
+private:
+	static constexpr auto DefaultFramebufferWidth = 640;
+	static constexpr auto DefaultFramebufferHeight = 480;
+	static constexpr auto DefaultSleepTime = 25;
+
+	bool initScreen( HeadlessVncScreen* screen );
+	bool initVncServer( int serverPort, const VncServerPluginInterface::Password& password,
+						HeadlessVncScreen* screen );
+
+	bool handleScreenChanges( HeadlessVncScreen* screen );
+
+	HeadlessVncConfiguration m_configuration;
+
+};
