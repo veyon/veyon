a706429efe0a3be1e8a038c3272dd121a1f62cd2
Tobias Junghans <tobydox@veyon.io>
2021-02-16 09:18:56 +0100

    FeatureManager: add updateActiveFeatures()
    (cherry picked from commit 4ccfb28918bce51b2c5f14eee81001d3df5909ba)

--- core/src/FeatureManager.cpp
+++ core/src/FeatureManager.cpp
@@ -172,6 +172,16 @@ void FeatureManager::stopFeature( VeyonMasterInterface& master,
 
 
 
+void FeatureManager::updateActiveFeatures( const ComputerControlInterfaceList& computerControlInterfaces ) const
+{
+	for( const auto& controlInterface : computerControlInterfaces )
+	{
+		controlInterface->updateActiveFeatures();
+	}
+}
+
+
+
 bool FeatureManager::handleFeatureMessage( ComputerControlInterface::Pointer computerControlInterface,
 										  const FeatureMessage& message ) const
 {
