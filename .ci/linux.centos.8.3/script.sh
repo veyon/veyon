e7d19bde6a4a5871455c1633aa920cf3e00fc0c5
Tobias Junghans <tobydox@veyon.io>
2021-02-26 19:16:02 +0100

    CI: update package name suffixes
    (cherry picked from commit ddcea2d5467e1a7ba41166bb6963e86d66ab2ef9)

--- .ci/linux.centos.8.3/script.sh
+++ .ci/linux.centos.8.3/script.sh
@@ -3,4 +3,4 @@
 set -e
 
 $1/.ci/common/linux-build.sh $@
-$1/.ci/common/finalize-rpm.sh $1 $2 "centos-8.2"
+$1/.ci/common/finalize-rpm.sh $1 $2 "centos-8.3"
--- .ci/linux.fedora.32/script.sh
+++ .ci/linux.fedora.32/script.sh
@@ -3,4 +3,4 @@
 set -e
 
 $1/.ci/common/linux-build.sh $@
-$1/.ci/common/finalize-rpm.sh $1 $2 "fc31"
+$1/.ci/common/finalize-rpm.sh $1 $2 "fc32"
