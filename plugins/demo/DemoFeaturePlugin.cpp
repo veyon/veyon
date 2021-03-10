veyon/veyon

commit a684c7fab94f757e547f5f6223b5a3f907bb796b

Demo: explicitely stop client when stopping demo feature
Fixes demo client not being stopped when stopping demo via context menu.

plugins/demo/DemoFeaturePlugin.cpp
@@ -228,7 +228,7 @@ bool DemoFeaturePlugin::stopFeature( VeyonMasterInterface& master, const Feature
 		feature == m_shareOwnScreenWindowFeature || feature == m_shareOwnScreenFullScreenFeature ||
 		feature == m_shareUserScreenWindowFeature || feature == m_shareUserScreenFullScreenFeature )
 	{
-		controlFeature( feature.uid(), Operation::Stop, {}, computerControlInterfaces );
+		controlFeature( m_demoClientWindowFeature.uid(), Operation::Stop, {}, computerControlInterfaces );
 
 		controlFeature( m_demoClientWindowFeature.uid(), Operation::Stop, {},
 						{ master.localSessionControlInterface().weakPointer() } );

commit f705d5cebd6cedc8b509b63bbb73c3008986a7e8

Demo: also terminate fullscreen demo client
This fixes a regression introduced with a684c7fab94f757e547f5f6223b5a3f.

plugins/demo/DemoFeaturePlugin.cpp
@@ -228,6 +228,7 @@ bool DemoFeaturePlugin::stopFeature( VeyonMasterInterface& master, const Feature
 		feature == m_shareOwnScreenWindowFeature || feature == m_shareOwnScreenFullScreenFeature ||
 		feature == m_shareUserScreenWindowFeature || feature == m_shareUserScreenFullScreenFeature )
 	{
+		controlFeature( m_demoClientFullScreenFeature.uid(), Operation::Stop, {}, computerControlInterfaces );
 		controlFeature( m_demoClientWindowFeature.uid(), Operation::Stop, {}, computerControlInterfaces );
 
 		controlFeature( m_demoClientWindowFeature.uid(), Operation::Stop, {},