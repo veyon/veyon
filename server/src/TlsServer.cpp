429ce87003e768c00837d12c401404863148763c
Tobias Junghans <tobydox@veyon.io>
2021-06-23 15:46:38 +0200

    server: add TlsServer

---
+++ server/src/TlsServer.cpp
@@ -0,0 +1,80 @@
+/*
+ * TlsServer.cpp - header file for TlsServer
+ *
+ * Copyright (c) 2021 Tobias Junghans <tobydox@veyon.io>
+ *
+ * This file is part of Veyon - https://veyon.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#include <QSslSocket>
+
+#include "TlsServer.h"
+
+
+TlsServer::TlsServer( const VeyonCore::TlsConfiguration& tlsConfig, QObject* parent ) :
+	QTcpServer( parent ),
+	m_tlsConfig( tlsConfig )
+{
+}
+
+
+
+void TlsServer::incomingConnection( qintptr socketDescriptor )
+{
+	if( m_tlsConfig.isNull() )
+	{
+		auto socket = new QTcpSocket;
+		if( socket->setSocketDescriptor(socketDescriptor) )
+		{
+			addPendingConnection( socket );
+		}
+		else
+		{
+			delete socket;
+		}
+	}
+	else
+	{
+		auto socket = new QSslSocket;
+		if( socket->setSocketDescriptor(socketDescriptor) )
+		{
+			connect(socket, &QSslSocket::encrypted, this, []() {
+				qCritical() << "connected"; } );
+
+			connect(socket, QOverload<const QList<QSslError>&>::of(&QSslSocket::sslErrors),
+					 [this, socket]( const QList<QSslError> &errors) {
+						 for( const auto& err : errors )
+						 {
+							 vCritical() << err;
+						 }
+
+						 Q_EMIT tlsErrors( socket, errors );
+					 } );
+
+			socket->setSslConfiguration( m_tlsConfig );
+			socket->startServerEncryption();
+
+			addPendingConnection( socket );
+		}
+		else
+		{
+			delete socket;
+		}
+	}
+}
