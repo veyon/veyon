b7a44d498bf6b3b5f5b2e17730f117697080b9ca
Tobias Junghans <tobydox@veyon.io>
2021-07-23 14:12:16 +0200

    LinuxSessionFunctions: add xdgSessionPathEnvVarName()

--- plugins/platform/linux/LinuxSessionFunctions.h
+++ plugins/platform/linux/LinuxSessionFunctions.h
@@ -106,6 +106,11 @@ public:
 		return QStringLiteral("XDG_SESSION_ID");
 	}
 
+	static QString xdgSessionPathEnvVarName()
+	{
+		return QStringLiteral("XDG_SESSION_PATH");
+	}
+
 	static bool isOpen( const QString& session );
 	static bool isGraphical( const QString& session );
 	static bool isRemote( const QString& session );
