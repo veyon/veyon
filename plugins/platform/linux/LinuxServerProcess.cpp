a633a33e58a03dce4983117ed433e664446929d1
Tobias Junghans <tobydox@veyon.io>
2021-09-09 20:09:48 +0200

    LinuxServiceCore: refactor to LinuxServerProcess
    This improves the server termination, i.e. it avoids zombie processes by
    not relying on QProcess::terminate()/kill()/state() which seem to have
    some issues when not being used event-driven.

---
+++ plugins/platform/linux/LinuxServerProcess.cpp
@@ -0,0 +1,122 @@
+/*
+ * LinuxServiceFunctions.cpp - implementation of LinuxServerProcess class
+ *
+ * Copyright (c) 2021 Tobias Junghans <tobydox@veyon.io>
+ *
+ * This file is part of Veyon - https://veyon.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#include <QFileInfo>
+
+#include <csignal>
+#include <proc/readproc.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include "Filesystem.h"
+#include "LinuxCoreFunctions.h"
+#include "LinuxServerProcess.h"
+#include "VeyonConfiguration.h"
+
+
+LinuxServerProcess::LinuxServerProcess( const QProcessEnvironment& processEnvironment,
+										const QString& sessionPath, int sessionId, QObject* parent ) :
+	QProcess( parent ),
+	m_sessionPath( sessionPath ),
+	m_sessionId( sessionId )
+{
+	setProcessEnvironment( processEnvironment );
+}
+
+
+
+LinuxServerProcess::~LinuxServerProcess()
+{
+	stop();
+}
+
+
+
+void LinuxServerProcess::start()
+{
+	if( VeyonCore::config().logToSystem() )
+	{
+		setProcessChannelMode( QProcess::ForwardedChannels );
+	}
+
+	const auto catchsegv{ QStringLiteral("/usr/bin/catchsegv") };
+	if( qEnvironmentVariableIsSet("VEYON_VALGRIND_SERVERS") )
+	{
+		QProcess::start( QStringLiteral("/usr/bin/valgrind"),
+			   { QStringLiteral("--error-limit=no"),
+				 QStringLiteral("--log-file=valgrind-veyon-server-%1.log").arg(m_sessionId),
+				 VeyonCore::filesystem().serverFilePath() } );
+	}
+	else if( VeyonCore::isDebugging() && QFileInfo::exists( catchsegv ) )
+	{
+		QProcess::start( catchsegv, { VeyonCore::filesystem().serverFilePath() } );
+	}
+	else
+	{
+		QProcess::start( VeyonCore::filesystem().serverFilePath(), QStringList{} );
+	}
+}
+
+
+
+void LinuxServerProcess::stop()
+{
+	const auto sendSignalRecursively = []( int pid, int sig ) {
+		if( pid > 0 )
+		{
+			LinuxCoreFunctions::forEachChildProcess(
+				[=]( proc_t* procInfo ) {
+					if( procInfo->tid > 0 )
+					{
+						::kill( procInfo->tid, sig );
+					}
+					return true;
+				},
+				pid, 0, true );
+
+			// clean up process
+			waitpid( pid, nullptr, WNOHANG );
+		}
+	};
+
+	const auto pid = processId();
+
+	// manually set process state since we're managing the process termination on our own
+	setProcessState( QProcess::NotRunning );
+
+	// tell x11vnc and child processes (in case spawned via catchsegv) to shutdown
+	sendSignalRecursively( pid, SIGINT );
+
+	if( LinuxCoreFunctions::waitForProcess( pid, ServerShutdownTimeout, ServerWaitSleepInterval ) == false )
+	{
+		sendSignalRecursively( pid, SIGTERM );
+
+		if( LinuxCoreFunctions::waitForProcess( pid, ServerTerminateTimeout, ServerWaitSleepInterval ) == false )
+		{
+			vWarning() << "server for session" << m_sessionPath << "still running - killing now";
+			sendSignalRecursively( pid, SIGKILL );
+			LinuxCoreFunctions::waitForProcess( pid, ServerKillTimeout, ServerWaitSleepInterval );
+		}
+	}
+}
