2af8ab8d9914c59b16c29d5a762f7cc2dcbdca08
Tobias Junghans <tobydox@veyon.io>
2021-09-10 14:04:25 +0200

    QtCompat: drop support for Qt < 5.7

--- core/src/QtCompat.h
+++ core/src/QtCompat.h
@@ -26,59 +26,12 @@
 
 #include <QtGlobal>
 
-#if QT_VERSION < QT_VERSION_CHECK(5, 6, 0)
-#error Qt < 5.6 not supported
+#if QT_VERSION < QT_VERSION_CHECK(5, 7, 0)
+#error Qt < 5.7 not supported
 #endif
 
 #if QT_VERSION < QT_VERSION_CHECK(5, 13, 0)
 #define Q_DISABLE_MOVE(Class) \
 	Class(const Class &&) Q_DECL_EQ_DELETE;\
 	Class &operator=(const Class &&) Q_DECL_EQ_DELETE;
-#endif
-
-#if QT_VERSION < QT_VERSION_CHECK(5, 7, 0)
-template <typename T> struct QAddConst { using Type = const T; };
-template <typename T> constexpr typename QAddConst<T>::Type &qAsConst(T &t) { return t; }
-template <typename T> void qAsConst(const T &&) = delete;
-
-template <typename... Args>
-struct QNonConstOverload
-{
-	template <typename R, typename T>
-	Q_DECL_CONSTEXPR auto operator()(R (T::*ptr)(Args...)) const Q_DECL_NOTHROW -> decltype(ptr)
-	{ return ptr; }
-
-	template <typename R, typename T>
-	static Q_DECL_CONSTEXPR auto of(R (T::*ptr)(Args...)) Q_DECL_NOTHROW -> decltype(ptr)
-	{ return ptr; }
-};
-
-template <typename... Args>
-struct QConstOverload
-{
-	template <typename R, typename T>
-	Q_DECL_CONSTEXPR auto operator()(R (T::*ptr)(Args...) const) const Q_DECL_NOTHROW -> decltype(ptr)
-	{ return ptr; }
-
-	template <typename R, typename T>
-	static Q_DECL_CONSTEXPR auto of(R (T::*ptr)(Args...) const) Q_DECL_NOTHROW -> decltype(ptr)
-	{ return ptr; }
-};
-
-template <typename... Args>
-struct QOverload : QConstOverload<Args...>, QNonConstOverload<Args...>
-{
-	using QConstOverload<Args...>::of;
-	using QConstOverload<Args...>::operator();
-	using QNonConstOverload<Args...>::of;
-	using QNonConstOverload<Args...>::operator();
-
-	template <typename R>
-	Q_DECL_CONSTEXPR auto operator()(R (*ptr)(Args...)) const Q_DECL_NOTHROW -> decltype(ptr)
-	{ return ptr; }
-
-	template <typename R>
-	static Q_DECL_CONSTEXPR auto of(R (*ptr)(Args...)) Q_DECL_NOTHROW -> decltype(ptr)
-	{ return ptr; }
-};
 #endif
