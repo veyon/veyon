161298d5da138052d86a0854a84f14f8abdec6e5
Tobias Junghans <tobydox@veyon.io>
2021-09-17 14:26:46 +0200

    VncConnection: clean up unused signal
    (cherry picked from commit fe32d691ebe7bebac70e275ad6a770437456ef4d)

--- core/include/VncConnection.h
+++ core/include/VncConnection.h
@@ -156,7 +156,6 @@ public:
 
 Q_SIGNALS:
 	void connectionPrepared();
-	void connectionEstablished();
 	void imageUpdated( int x, int y, int w, int h );
 	void framebufferUpdateComplete();
 	void framebufferSizeChanged( int w, int h );
