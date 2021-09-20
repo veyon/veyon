bfb0828ef87a8fac008984554204e7fdf0518e91
Tobias Junghans <tobydox@veyon.io>
2021-09-20 12:32:14 +0200

    CMake: backport build options from master
    This harmonizes the remaining differences in the CMake code to be more
    close to the master branch.

--- cmake/modules/BuildVeyonPlugin.cmake
+++ cmake/modules/BuildVeyonPlugin.cmake
@@ -3,18 +3,19 @@
 # description: build Veyon plugin
 # usage: build_veyon_plugin(<NAME> <SOURCES>)
 
+include(SetDefaultTargetProperties)
+
 macro(build_veyon_plugin PLUGIN_NAME)
 	add_library(${PLUGIN_NAME} MODULE ${ARGN})
 
 	target_include_directories(${PLUGIN_NAME} PRIVATE ${CMAKE_CURRENT_BINARY_DIR} ${CMAKE_CURRENT_SOURCE_DIR})
 	target_link_libraries(${PLUGIN_NAME} veyon-core)
+	target_compile_options(${PLUGIN_NAME} PRIVATE ${VEYON_COMPILE_OPTIONS})
 
+	set_default_target_properties(${PLUGIN_NAME})
 	set_target_properties(${PLUGIN_NAME} PROPERTIES PREFIX "")
 	set_target_properties(${PLUGIN_NAME} PROPERTIES LINK_FLAGS "-Wl,-no-undefined")
 	install(TARGETS ${PLUGIN_NAME} LIBRARY DESTINATION ${VEYON_INSTALL_PLUGIN_DIR})
-
-	set_default_target_properties(${PLUGIN_NAME})
-
 	if(WITH_PCH)
 		target_precompile_headers(${PLUGIN_NAME} REUSE_FROM veyon-pch)
 	endif()
