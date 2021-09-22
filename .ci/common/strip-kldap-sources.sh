df3848c56d044880616ded585800c1e27062e274
Tobias Junghans <tobydox@veyon.io>
2021-09-22 09:31:44 +0200

    CI: strip-kldap-sources: extend for kldap-qt-compat
    (cherry picked from commit 31d24d555d9ee86f4a3bfd8fecc0a7e1aa6abe14)

--- .ci/common/strip-kldap-sources.sh
+++ .ci/common/strip-kldap-sources.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-cd 3rdparty/kldap && \
+(cd 3rdparty/kldap && \
 rm -rf \
 	autotests \
-	kioslave
-
+	kioslave \
+	src/widgets)
+(cd 3rdparty/kldap-qt-compat && \
+rm -rf \
+	autotests \
+	kioslave \
+	src/widgets)
