6bd598db1bbec1b91ffe3acffb3c9dccf86c5125
Tobias Junghans <tobydox@veyon.io>
2021-09-22 20:34:36 +0200

    DemoServerConnection: fix context for socket I/O

--- plugins/demo/DemoServerConnection.cpp
+++ plugins/demo/DemoServerConnection.cpp
@@ -98,7 +98,7 @@ void DemoServerConnection::processClient()
 		// try again later in case we could not proceed because of
 		// external protocol dependencies or in case we're finished
 		// and already have RFB messages in receive queue
-		QTimer::singleShot( ProtocolRetryTime, this, [this]() { processClient(); } );
+		QTimer::singleShot( ProtocolRetryTime, m_socket, [this]() { processClient(); } );
 	}
 	else
 	{
@@ -192,6 +192,6 @@ void DemoServerConnection::sendFramebufferUpdate()
 	if( sentUpdates == false )
 	{
 		// did not send updates but client still waiting for update? then try again soon
-		QTimer::singleShot( m_framebufferUpdateInterval, this, [this]() { sendFramebufferUpdate(); } );
+		QTimer::singleShot( m_framebufferUpdateInterval, m_socket, [this]() { sendFramebufferUpdate(); } );
 	}
 }
