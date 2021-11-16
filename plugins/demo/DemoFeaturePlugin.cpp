0c045ee1f3aa20a0901129cd971d35a013a8b03e
Tobias Junghans <tobydox@veyon.io>
2021-11-16 22:25:00 +0100

    Demo: use PlatformCoreFunctions::queryDisplayDeviceName()
    (cherry picked from commit 832ac44459c55a8889523ea62f9d0349d56fa6c4)

--- plugins/demo/DemoFeaturePlugin.cpp
+++ plugins/demo/DemoFeaturePlugin.cpp
@@ -35,6 +35,7 @@
 #include "FeatureWorkerManager.h"
 #include "HostAddress.h"
 #include "Logger.h"
+#include "PlatformCoreFunctions.h"
 #include "PlatformSessionFunctions.h"
 #include "VeyonConfiguration.h"
 #include "VeyonMasterInterface.h"
@@ -512,15 +513,13 @@ void DemoFeaturePlugin::updateFeatures()
 		{
 			const auto name = QStringLiteral( "DemoScreen%1" ).arg( index );
 
-			auto displayName = tr( "Screen %1 [%2]" ).arg( index ).arg( screen->name() );
+			auto displayName = tr("Display %1").arg(index);
 
-#if QT_VERSION >= QT_VERSION_CHECK(5, 9, 0)
-			if( screen->manufacturer().isEmpty() == false &&
-				screen->model().isEmpty() == false )
+			const auto displayDeviceName = VeyonCore::platform().coreFunctions().queryDisplayDeviceName(*screen);
+			if(displayDeviceName.isEmpty() == false)
 			{
-				displayName += QStringLiteral(" – %1 %2").arg( screen->manufacturer(), screen->model() );
+				displayName.append(QStringLiteral(" (%1)").arg(displayDeviceName));
 			}
-#endif
 
 			auto flags = Feature::Flag::Option | Feature::Flag::Master;
 			if( index == m_screenSelection )
