c299acf536e8a525534a6cbbbbf31b5060037d13
Tobias Junghans <tobydox@veyon.io>
2021-11-16 13:52:07 +0100

    CCI: add locking and connection check in updateDisplays()

--- core/src/ComputerControlInterface.cpp
+++ core/src/ComputerControlInterface.cpp
@@ -207,7 +207,18 @@ void ComputerControlInterface::setUserInformation( const QString& userLoginName,
 
 void ComputerControlInterface::updateDisplays()
 {
-	VeyonCore::builtinFeatures().monitoringMode().queryDisplays( { weakPointer() } );
+	lock();
+
+	if( vncConnection() && state() == State::Connected )
+	{
+		VeyonCore::builtinFeatures().monitoringMode().queryDisplays( { weakPointer() } );
+	}
+	else
+	{
+		setDisplays( {} );
+	}
+
+	unlock();
 }
 
 
