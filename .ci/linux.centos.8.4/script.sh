53d23e13870c0c57e699a580947e8302634ef289
Tobias Junghans <tobydox@veyon.io>
2021-12-03 17:32:16 +0100

    CI: CentOS 8.4: extend CMAKE_FLAGS only
    (cherry picked from commit ccd0908edc8b0aa0ffee1ce8fccfa30479a512ea)

--- .ci/linux.centos.8.4/script.sh
+++ .ci/linux.centos.8.4/script.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-export CMAKE_FLAGS="-DWITH_PCH=OFF"
+export CMAKE_FLAGS="$CMAKE_FLAGS -DWITH_PCH=OFF"
 
 $1/.ci/common/linux-build.sh $@
 $1/.ci/common/finalize-rpm.sh $1 $2 "centos-8.4"
