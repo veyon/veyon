2741b789692fb2d88ae07170349c65c20f4aedc9
Tobias Junghans <tobydox@veyon.io>
2021-12-14 09:31:25 +0100

    Core: Configuration: drop export decl for TypedProperty
    Since the template class is fully defined, only specializations have
    to be exported. This avoids linkage problems when instantiating
    specializations for custom types in components linking against the
    core library.

--- core/src/Configuration/Property.h
+++ core/src/Configuration/Property.h
@@ -128,7 +128,8 @@ private:
 
 
 template<class T>
-class VEYON_CORE_EXPORT TypedProperty : public Property {
+class TypedProperty : public Property
+{
 public:
 	using Type = typename CheapestType<T>::Type;
 
