cde9efb7ab8b8cb81ac8d5ee6be16315b0c7468c
Tobias Junghans <tobydox@veyon.io>
2022-04-25 09:38:08 +0200

    CI: drop Fedora 34 and add Fedora 36

--- .ci/linux.fedora.36/script.sh
+++ .ci/linux.fedora.36/script.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-export CMAKE_FLAGS="$CMAKE_FLAGS -DCPACK_DIST=fedora.34"
+export CMAKE_FLAGS="$CMAKE_FLAGS -DCPACK_DIST=fedora.36"
 
 $1/.ci/common/linux-build.sh $@
 $1/.ci/common/finalize-rpm.sh $1 $2
