72046c951feb148900938e6fd5c9078882045544
Tobias Junghans <tobydox@veyon.io>
2023-01-24 11:33:28 +0100

    CI: drop Fedora 35 and add Fedora 37

--- .ci/linux.fedora.37/script.sh
+++ .ci/linux.fedora.37/script.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-export CMAKE_FLAGS="$CMAKE_FLAGS -DCPACK_DIST=fedora.35"
+export CMAKE_FLAGS="$CMAKE_FLAGS -DCPACK_DIST=fedora.37"
 
 $1/.ci/common/linux-build.sh $@
 $1/.ci/common/finalize-rpm.sh $1 $2
