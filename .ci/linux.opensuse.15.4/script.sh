f086cc1a82644253b35a226cb49ab59474365077
Tobias Junghans <tobydox@veyon.io>
2023-01-24 11:38:05 +0100

    CI: replace openSUSE Leap 15.3 with 15.4
    15.3 is EOL as of Dec 31st 2022.

--- .ci/linux.opensuse.15.4/script.sh
+++ .ci/linux.opensuse.15.4/script.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-export CMAKE_FLAGS="$CMAKE_FLAGS -DCPACK_DIST=opensuse.15.3"
+export CMAKE_FLAGS="$CMAKE_FLAGS -DCPACK_DIST=opensuse.15.4"
 
 $1/.ci/common/linux-build.sh $@
 $1/.ci/common/finalize-rpm.sh $1 $2
