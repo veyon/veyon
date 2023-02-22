68ffb19c16db07ca8366250b6f53d897ef75fd71
Tobias Junghans <tobydox@veyon.io>
2023-02-22 13:34:18 +0100

    CI: set QT_TRANSLATIONS_DIR for Windows builds

--- .ci/windows/build.sh
+++ .ci/windows/build.sh
@@ -11,7 +11,12 @@ rm -rf $BUILDDIR
 mkdir $BUILDDIR
 cd $BUILDDIR
 
-/usr/$1-w64-mingw32/bin/qt-cmake $BASEDIR -G Ninja -DWITH_QT6=ON $CMAKE_FLAGS
+PREFIX=/usr/$1-w64-mingw32
+$PREFIX/bin/qt-cmake $BASEDIR \
+	-G Ninja \
+	-DWITH_QT6=ON \
+	-DQT_TRANSLATIONS_DIR=$PREFIX/translations \
+	$CMAKE_FLAGS
 
 if [ -z "$2" ] ; then
 	ninja windows-binaries
