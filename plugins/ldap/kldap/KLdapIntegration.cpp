35831d607c23c99152e513ed9761d78acd2c95d7
Tobias Junghans <tobydox@veyon.io>
2024-01-19 11:43:37 +0100

    3rdparty: kldap: update submodule
    Due to larger upstream refactorings the integration code needs to be
    adopted as well. Luckily we're still able to integrate kldap-qt-compat.
    
    (cherry picked from commit 6663b23ca260d2ec78c8fc89b8fdcab7514de662)

--- plugins/ldap/kldap/KLdapIntegration.cpp
+++ plugins/ldap/kldap/KLdapIntegration.cpp
@@ -1,7 +1,7 @@
-// Copyright (c) 2016-2023 Tobias Junghans <tobydox@veyon.io>
+// Copyright (c) 2016-2024 Tobias Junghans <tobydox@veyon.io>
 // This file is part of Veyon - https://veyon.io
 // SPDX-License-Identifier: LGPL-2.0-or-later
 
-#include "ldap_debug.h"
+#include "ldap_core_debug.h"
 
 Q_LOGGING_CATEGORY(LDAP_LOG, "KLDAP");
--- plugins/ldap/kldap/ldap_debug.h
+++ plugins/ldap/kldap/ldap_debug.h
@@ -1,11 +1,1 @@
-// Copyright (c) 2019-2023 Tobias Junghans <tobydox@veyon.io>
-// This file is part of Veyon - https://veyon.io
-// SPDX-License-Identifier: LGPL-2.0-or-later
-
-#pragma once
-
-#include <QDebug>
-#include <QLoggingCategory>
-
-Q_DECLARE_LOGGING_CATEGORY(LDAP_LOG);
-
+#include "ldap_core_debug.h"
