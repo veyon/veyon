904c746080b295b12744f2ee1c67776daaa9abd4
Tobias Junghans <tobydox@veyon.io>
2024-05-14 09:29:05 +0200

    CI: replace Fedora 38 with Fedora 40
    Fedora 38 is EOL as of 2024-05-14.
    
    Closes #951.

--- .ci/linux.fedora.40/script.sh
+++ .ci/linux.fedora.40/script.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-export CMAKE_FLAGS="$CMAKE_FLAGS -DWITH_QT6=ON -DCPACK_DIST=fedora.38"
+export CMAKE_FLAGS="$CMAKE_FLAGS -DWITH_QT6=ON -DCPACK_DIST=fedora.40"
 
 $1/.ci/common/linux-build.sh $@
 $1/.ci/common/finalize-rpm.sh $1 $2
