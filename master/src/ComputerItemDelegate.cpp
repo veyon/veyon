a64ebe2ea97c5b17f3694645055685972d8171b4
Tobias Junghans <tobydox@veyon.io>
2025-02-21 15:13:15 +0100

    Master: add ComputerItemDelegate
    Indicate active features by overlaying the thumbnail with the
    corresponding feature icons.

---
+++ master/src/ComputerItemDelegate.cpp
@@ -0,0 +1,104 @@
+/*
+ * ComputerItemDelegate.cpp - implementation of ComputerItemDelegate
+
+ * Copyright (c) 2025 Tobias Junghans <tobydox@veyon.io>
+ *
+ * This file is part of Veyon - https://veyon.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#include <QPainter>
+
+#include "ComputerControlListModel.h"
+#include "ComputerItemDelegate.h"
+#include "FeatureManager.h"
+
+
+ComputerItemDelegate::ComputerItemDelegate(QObject* parent) :
+	QStyledItemDelegate(parent)
+{
+	initFeaturePixmaps();
+}
+
+
+
+void ComputerItemDelegate::paint(QPainter* painter, const QStyleOptionViewItem& option, const QModelIndex& index) const
+{
+	QStyledItemDelegate::paint(painter, option, index);
+
+	if (index.isValid() && index.model())
+	{
+		drawFeatureIcons(painter, index.model()->data(index, ComputerControlListModel::ControlInterfaceRole).value<ComputerControlInterface::Pointer>());
+	}
+}
+
+
+
+void ComputerItemDelegate::initFeaturePixmaps()
+{
+	for (const auto& feature : VeyonCore::featureManager().features() )
+	{
+		if (feature.testFlag(Feature::Flag::Master) && !feature.iconUrl().isEmpty())
+		{
+			m_featurePixmaps[feature.uid()] = QIcon(feature.iconUrl()).pixmap(QSize(OverlayIconSize, OverlayIconSize));
+		}
+	}
+}
+
+
+
+void ComputerItemDelegate::drawFeatureIcons(QPainter* painter, ComputerControlInterface::Pointer controlInterface) const
+{
+	if (painter &&
+		controlInterface &&
+		controlInterface->state() == ComputerControlInterface::State::Connected)
+	{
+		auto count = 0;
+		for (const auto& feature : controlInterface->activeFeatures())
+		{
+			if (m_featurePixmaps.contains(feature))
+			{
+				count++;
+			}
+		}
+
+		if (count == 0)
+		{
+			return;
+		}
+
+		painter->setRenderHint(QPainter::Antialiasing);
+		painter->setBrush(QColor(255, 255, 255, 192));
+		painter->setPen(QColor(25, 140, 179));
+		painter->drawRoundedRect(QRect(OverlayIconsPadding, OverlayIconsPadding,
+									   count * (OverlayIconSize + OverlayIconSpacing), OverlayIconSize),
+								 OverlayIconsRadius, OverlayIconsRadius);
+
+		int x = OverlayIconsPadding;
+
+		for (const auto& feature : controlInterface->activeFeatures())
+		{
+			const auto it = m_featurePixmaps.find(feature);
+			if (it != m_featurePixmaps.constEnd())
+			{
+				painter->drawPixmap(QPoint(x, OverlayIconsPadding), *it);
+				x += OverlayIconSize + OverlayIconSpacing;
+			}
+		}
+	}
+}
