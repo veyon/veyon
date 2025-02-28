dbbd536b1ac354a12f92bbf2ba1506b6747db363
Tobias Junghans <tobydox@veyon.io>
2025-02-28 15:12:00 +0100

    ComputerControlListModel: supply access control message in tooltip
    (cherry picked from commit 4c72767a294b82ef89381ac47db7ea44a0f801eb)

--- master/src/ComputerControlListModel.cpp
+++ master/src/ComputerControlListModel.cpp
@@ -281,6 +281,13 @@ void ComputerControlListModel::updateState( const QModelIndex& index )
 
 
 
+void ComputerControlListModel::updateAccessControlMessage(const QModelIndex& index)
+{
+	Q_EMIT dataChanged(index, index, { Qt::ToolTipRole });
+}
+
+
+
 void ComputerControlListModel::updateScreen( const QModelIndex& index )
 {
 	Q_EMIT dataChanged( index, index, { Qt::DecorationRole, FramebufferRole } );
@@ -343,6 +350,9 @@ void ComputerControlListModel::startComputerControlInterface( ComputerControlInt
 
 	connect(controlInterface, &ComputerControlInterface::sessionInfoChanged,
 			 this, [=]() { updateSessionInfo(interfaceIndex(controlInterface)); });
+
+	connect(controlInterface, &ComputerControlInterface::accessControlMessageChanged,
+			this, [=] () { updateAccessControlMessage(interfaceIndex(controlInterface)); });
 }
 
 
@@ -524,6 +534,11 @@ QString ComputerControlListModel::computerStateDescription( const ComputerContro
 
 	default:
 		break;
+	}
+
+	if (controlInterface->accessControlMessage().isEmpty() == false)
+	{
+		return controlInterface->accessControlMessage();
 	}
 
 	return tr( "Disconnected" );
