2b9f9ffed4cc56dcacfcae15d26accfa7b75d75f
Tobias Junghans <tobydox@veyon.io>
2025-03-12 15:16:13 +0100

    ComputerControlListModel: refactor host icons
    Add icon for state host is online but framebuffer not available yet.
    Indicate DNS, service or access problems with dedicated symbols.
    
    (cherry picked from commit 699c2e741c8f894bfbbb4febe03ea989e6a60cfe)

--- master/src/ComputerControlListModel.h
+++ master/src/ComputerControlListModel.h
@@ -91,9 +91,11 @@ private:
 
 	VeyonMaster* m_master;
 
-	QImage m_iconDefault;
-	QImage m_iconConnectionProblem;
-	QImage m_iconServerNotRunning;
+	QImage m_iconHostOffline;
+	QImage m_iconHostOnline;
+	QImage m_iconHostNameReslutionFailed;
+	QImage m_iconHostAccessDenied;
+	QImage m_iconHostServiceError;
 
 	QSize m_computerScreenSize{};
 
