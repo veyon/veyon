380d527f2a5b38c473f475176561e7feabc6d041
Tobias Junghans <tobydox@veyon.io>
2025-03-14 16:48:35 +0100

    ComputerControlListModel: indicate connected state only after framebuffer is available

--- master/src/ComputerControlListModel.cpp
+++ master/src/ComputerControlListModel.cpp
@@ -583,7 +583,11 @@ QString ComputerControlListModel::computerStateDescription( const ComputerContro
 	switch( controlInterface->state() )
 	{
 	case ComputerControlInterface::State::Connected:
-		return tr( "Online and connected" );
+		if (controlInterface->hasValidFramebuffer())
+		{
+			return tr("Online and connected");
+		}
+		[[fallthrough]];
 
 	case ComputerControlInterface::State::Connecting:
 		return tr( "Establishing connection" );
