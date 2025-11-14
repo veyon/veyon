cb90fe0f522b5f75f9e7f236e34509ba563a2832
Tobias Junghans <tobydox@veyon.io>
2025-11-14 14:41:38 +0100

    CI: replace Fedora 41 with Fedora 43
    Closes #1080.
    
    (cherry picked from commit 5b49a9d8d048bf930471f0988dd1e546f098fba2)

---
+++ .ci/linux.fedora.43/script.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e
+
+export CMAKE_FLAGS="$CMAKE_FLAGS -DWITH_PCH=OFF -DCPACK_DIST=fedora.43 -DCMAKE_CXX_FLAGS=-Wno-template-id-cdtor"
+
+$1/.ci/common/linux-build.sh $@
+$1/.ci/common/finalize-rpm.sh $1 $2
