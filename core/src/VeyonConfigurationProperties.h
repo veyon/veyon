f5ed9d2402b3377f80c13f1f5620fccbb300d608
Tobias Junghans <tobydox@veyon.io>
2025-12-05 15:27:58 +0100

    VeyonConfiguration: add properties for identifying users in guest sessions

--- core/src/VeyonConfigurationProperties.h
+++ core/src/VeyonConfigurationProperties.h
@@ -146,6 +146,8 @@
 	OP( VeyonConfiguration, VeyonCore::config(), bool, confirmUnsafeActions, setConfirmUnsafeActions, "ConfirmUnsafeActions", "Master", false, Configuration::Property::Flag::Standard )	\
 	OP( VeyonConfiguration, VeyonCore::config(), bool, showFeatureWindowsOnSameScreen, setShowFeatureWindowsOnSameScreen, "ShowFeatureWindowsOnSameScreen", "Master", false, Configuration::Property::Flag::Standard )	\
 	OP( VeyonConfiguration, VeyonCore::config(), Computer::NameSource, computerNameSource, setComputerNameSource, "ComputerNameSource", "Master", QVariant::fromValue(Computer::NameSource::Default), Configuration::Property::Flag::Advanced )	\
+	OP( VeyonConfiguration, VeyonCore::config(), bool, identifyUsersInGuestSessions, setIdentifyUsersInGuestSessions, "IdentifyUsersInGuestSessions", "Master", false, Configuration::Property::Flag::Advanced ) \
+	OP( VeyonConfiguration, VeyonCore::config(), QString, guestUserLoginName, setGuestUserLoginName, "GuestUserLoginName", "Master", VeyonCore::tr("Guest"), Configuration::Property::Flag::Advanced ) \
 
 #define FOREACH_VEYON_AUTHENTICATION_CONFIG_PROPERTY(OP) \
 	OP( VeyonConfiguration, VeyonCore::config(), QStringList, enabledAuthenticationPlugins, setEnabledAuthenticationPlugins, "EnabledPlugins", "Authentication", QStringList(), Configuration::Property::Flag::Standard )	\
