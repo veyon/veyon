8687bfcccfe3e2a103a4e275a2685eeb9dbb4980
Tobias Junghans <tobydox@veyon.io>
2025-12-18 09:44:14 +0100

    3rdparty: drop kldap-qt-compat

--- .ci/common/strip-kldap-sources.sh
+++ .ci/common/strip-kldap-sources.sh
@@ -2,11 +2,6 @@
 
 (cd 3rdparty/kldap && \
 rm -rf \
-	autotests \
-	kioslave \
-	src/widgets)
-(cd 3rdparty/kldap-qt-compat && \
-rm -rf \
-	autotests \
-	kioslave \
+	kioworker \
+	src/core/autotests \
 	src/widgets)
