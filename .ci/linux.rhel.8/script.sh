3b9263ce1173881e8fc66104e971fb5d59d031c0
Tobias Junghans <tobydox@veyon.io>
2026-05-22 12:40:33 +0200

    CI: linux.rhel.8: disable WebAPI plugin explictly
    As of 26ea7a2ec86b5bab8dd4a9e2965555076aad5fa3 the plugin has to be
    disabled explicitly if the required headers are not available.

--- .ci/linux.rhel.8/script.sh
+++ .ci/linux.rhel.8/script.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-export CMAKE_FLAGS="$CMAKE_FLAGS -DWITH_QT6=OFF -DCPACK_DIST=rhel.8"
+export CMAKE_FLAGS="$CMAKE_FLAGS -DWITH_QT6=OFF -DWITH_WEBAPI=OFF -DCPACK_DIST=rhel.8"
 
 $1/.ci/common/linux-build.sh $@
 $1/.ci/common/finalize-rpm.sh $1 $2
