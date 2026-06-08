3d64c12ea465c040825201faff01d24c5513dff1
Tobias Junghans <tobydox@veyon.io>
2026-06-08 09:40:26 +0200

    CMake: refactor integration of bundled LibVNCServer
    Include the LibVNCServer source directory as regular subdirectory and
    set up options and target properties accordingly.

--- .ci/linux.rhel.9/script.sh
+++ .ci/linux.rhel.9/script.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-export CMAKE_FLAGS="$CMAKE_FLAGS -DCPACK_DIST=rhel.9"
+export CMAKE_FLAGS="$CMAKE_FLAGS -DWITH_BUNDLED_LIBVNC=ON -DCPACK_DIST=rhel.9"
 
 $1/.ci/common/linux-build.sh $@
 $1/.ci/common/finalize-rpm.sh $1 $2
