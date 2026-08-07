veyon/veyon

commit f224fc7d6387d34224e9b86e0b73404961b0fa82

Modernize code to build with Qt 6.8
(cherry picked from commit 662ee617cc550ab7a275043d9d937acb143d0c86)

core/src/Configuration/Object.cpp
@@ -99,7 +99,7 @@ static Object::DataMap operator+( Object::DataMap dst, Object::DataMap src )
 {
 	for( auto it = src.begin(), end = src.end(); it != end; ++it )
 	{
-		if( it.value().type() == QVariant::Map && dst.contains( it.key() ) )
+		if (it.value().userType() == QMetaType::QVariantMap && dst.contains(it.key()))
 		{
 			dst[it.key()] = dst[it.key()].toMap() + it.value().toMap();
 		}
@@ -138,8 +138,8 @@ bool Object::hasValue( const QString& key, const QString& parentKey ) const
 
 	for( const auto& level : subLevels )
 	{
-		if( currentMap.contains( level ) &&
-				currentMap[level].type() == QVariant::Map )
+		if (currentMap.contains(level) &&
+			currentMap[level].userType() == QMetaType::QVariantMap)
 		{
 			currentMap = currentMap[level].toMap();
 		}
@@ -175,8 +175,8 @@ QVariant Object::value( const QString& key, const QString& parentKey, const QVar
 	DataMap currentMap = m_data;
 	for( const auto& level : subLevels )
 	{
-		if( currentMap.contains( level ) &&
-				currentMap[level].type() == QVariant::Map )
+		if (currentMap.contains(level) &&
+			currentMap[level].userType() == QMetaType::QVariantMap)
 		{
 			currentMap = currentMap[level].toMap();
 		}
@@ -205,7 +205,7 @@ static Object::DataMap setValueRecursive( Object::DataMap data,
 	if( subLevels.isEmpty() )
 	{
 		// search for key in toplevel data map
-		if( !data.contains( key ) || data[key].type() != QVariant::Map )
+		if( !data.contains( key ) || data[key].userType() != QMetaType::QVariantMap)
 		{
 			data[key] = value;
 		}
@@ -220,7 +220,7 @@ static Object::DataMap setValueRecursive( Object::DataMap data,
 	const QString level = subLevels.takeFirst();
 	if( data.contains( level ) )
 	{
-		if( data[level].type() != QVariant::Map )
+		if( data[level].userType() != QMetaType::QVariantMap)
 		{
 			vWarning() << "parent key points doesn't point to a data map!";
 			return data;
@@ -272,7 +272,7 @@ static Object::DataMap removeValueRecursive( Object::DataMap data,
 	}
 
 	const QString level = subLevels.takeFirst();
-	if( data.contains( level ) && data[level].type() == QVariant::Map )
+	if (data.contains(level) && data[level].userType() != QMetaType::QVariantMap)
 	{
 		data[level] = removeValueRecursive( data[level].toMap(), subLevels, key );
 	}
@@ -304,7 +304,7 @@ static void addSubObjectRecursive( const Object::DataMap& dataMap,
 {
 	for( auto it = dataMap.begin(), end = dataMap.end(); it != end; ++it )
 	{
-		if( it.value().type() == QVariant::Map )
+		if (it.value().userType() != QMetaType::QVariantMap)
 		{
 			auto newParentKey = it.key();
 			if( parentKey.isEmpty() == false )

cli/src/ConfigCommands.cpp
@@ -329,16 +329,16 @@ CommandLinePluginInterface::RunResult ConfigCommands::applyConfiguration()
 
 QString ConfigCommands::printableConfigurationValue( const QVariant& value )
 {
-	if( value.type() == QVariant::String ||
-		value.type() == QVariant::Uuid ||
-		value.type() == QVariant::UInt ||
-		value.type() == QVariant::Int ||
-		value.type() == QVariant::Bool )
+	if (value.userType() == QMetaType::QString ||
+		value.userType() == QMetaType::QUuid ||
+		value.userType() == QMetaType::UInt ||
+		value.userType() == QMetaType::Int ||
+		value.userType() == QMetaType::Bool)
 	{
 		return value.toString();
 	}
 
-	if( value.type() == QVariant::StringList )
+	if (value.userType() == QMetaType::QStringList)
 	{
 		return value.toStringList().join( QLatin1Char(';') );
 	}

cli/src/FeatureCommands.cpp
@@ -147,7 +147,7 @@ CommandLinePluginInterface::RunResult FeatureCommands::handle_list( const QStrin
 	Q_UNUSED(arguments)
 
 	FeatureManager featureManager;
-	for( const auto& feature : qAsConst(featureManager.features()) )
+	for( const auto& feature : std::as_const(featureManager.features()) )
 	{
 		print( feature.name() );
 	}

cli/src/main.cpp
@@ -172,7 +172,7 @@ int main( int argc, char **argv )
 			std::sort( commands.begin(), commands.end() );
 
 			CommandLineIO::print( VeyonCore::tr( "Available commands:" ) );
-			for( const auto& command : qAsConst(commands) )
+			for( const auto& command : std::as_const(commands) )
 			{
 				CommandLineIO::print( QStringLiteral("    %1 - %2").arg( command, it.key()->commandHelp( command ) ) );
 			}

configurator/src/MainWindow.cpp
@@ -347,7 +347,7 @@ bool MainWindow::applyConfiguration()
 
 void MainWindow::loadConfigurationPagePlugins()
 {
-	for( auto pluginObject : qAsConst( VeyonCore::pluginManager().pluginObjects() ) )
+	for( auto pluginObject : std::as_const( VeyonCore::pluginManager().pluginObjects() ) )
 	{
 		auto pluginInterface = qobject_cast<PluginInterface *>( pluginObject );
 		auto configurationPagePluginInterface = qobject_cast<ConfigurationPagePluginInterface *>( pluginObject );

configurator/src/MasterConfigurationPage.cpp
@@ -160,7 +160,7 @@ void MasterConfigurationPage::updateFeatureLists()
 	ui->allFeaturesListWidget->clear();
 	ui->disabledFeaturesListWidget->clear();
 
-	for( const auto& feature : qAsConst( VeyonCore::featureManager().features() ) )
+	for( const auto& feature : std::as_const( VeyonCore::featureManager().features() ) )
 	{
 		if( feature.testFlag( Feature::Flag::Master ) == false ||
 			feature.testFlag( Feature::Flag::Meta ) ||

configurator/src/ServiceConfigurationPage.cpp
@@ -154,7 +154,7 @@ void ServiceConfigurationPage::updateVncServerPluginConfigurationWidget()
 
 void ServiceConfigurationPage::populateVncServerPluginComboBox()
 {
-	for( auto pluginObject : qAsConst( VeyonCore::pluginManager().pluginObjects() ) )
+	for( auto pluginObject : std::as_const( VeyonCore::pluginManager().pluginObjects() ) )
 	{
 		auto pluginInterface = qobject_cast<PluginInterface *>( pluginObject );
 		auto vncServerPluginInterface = qobject_cast<VncServerPluginInterface *>( pluginObject );

core/src/AccessControlProvider.cpp
@@ -183,7 +183,7 @@ AccessControlRule::Action AccessControlProvider::processAccessControlRules( cons
 {
 	vDebug() << "processing rules for" << accessingUser << accessingComputer << localUser << localComputer << connectedUsers;
 
-	for( const auto& rule : qAsConst( m_accessControlRules ) )
+	for( const auto& rule : std::as_const( m_accessControlRules ) )
 	{
 		// rule disabled?
 		if( rule.action() == AccessControlRule::Action::None )
@@ -216,7 +216,7 @@ bool AccessControlProvider::isAccessToLocalComputerDenied() const
 		return false;
 	}
 
-	for( const auto& rule : qAsConst( m_accessControlRules ) )
+	for( const auto& rule : std::as_const( m_accessControlRules ) )
 	{
 		if( matchConditions( rule, {}, {},
 							 VeyonCore::platform().userFunctions().currentUser(), HostAddress::localFQDN(), {} ) )

commit 54befcc38ac46c196d22941a53ab86d5567d4919

Configuration: fix broken config merge and key removal
Commit f224fc7d6 introduced a regression where addSubObject() and
removeValue() no longer worked properly due to the mistakenly inverted
condition.

Closes #1155.

core/src/Configuration/Object.cpp
@@ -272,7 +272,7 @@ static Object::DataMap removeValueRecursive( Object::DataMap data,
 	}
 
 	const QString level = subLevels.takeFirst();
-	if (data.contains(level) && data[level].userType() != QMetaType::QVariantMap)
+	if (data.contains(level) && data[level].userType() == QMetaType::QVariantMap)
 	{
 		data[level] = removeValueRecursive( data[level].toMap(), subLevels, key );
 	}
@@ -304,7 +304,7 @@ static void addSubObjectRecursive( const Object::DataMap& dataMap,
 {
 	for( auto it = dataMap.begin(), end = dataMap.end(); it != end; ++it )
 	{
-		if (it.value().userType() != QMetaType::QVariantMap)
+		if (it.value().userType() == QMetaType::QVariantMap)
 		{
 			auto newParentKey = it.key();
 			if( parentKey.isEmpty() == false )